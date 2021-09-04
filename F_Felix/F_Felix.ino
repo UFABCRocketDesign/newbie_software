a706fca2afe1e89de187d3e892a97c05abdb776b
Fernanda Felix <fernanda.felix0@outlook.com>
2021-09-03 22:01:44 -0300

    Update F_Felix.ino
    formatação tabela

--- F_Felix/F_Felix.ino
+++ F_Felix/F_Felix.ino
@@ -11,6 +11,12 @@ void setup() {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
     }
+
+    Serial.print("Temp.(*C) ");
+    Serial.print(" Pres.(Pa) ");
+    Serial.print("Alt.(m)  ");
+    Serial.print("Pres. sealevel(Pa)  ");
+    Serial.println("Real alt.(m)  ");
 }
 
 // the loop function runs over and over again forever
@@ -18,29 +24,21 @@ void loop() {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(500);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-
-    Serial.print("Temperature (*C) ");
-    Serial.print(" Pressure (Pa) ");
-    Serial.print("Altitude (meters)  ");
-    Serial.print("Pressure at sealevel (Pa)  ");
-    Serial.println("Real altitude (meters)  ");
     
     Serial.print(bmp.readTemperature());
-    Serial.print("    ");
+    Serial.print("         ");
     
     Serial.print(bmp.readPressure());
-    Serial.print("    ");
+    Serial.print("         ");
     
     Serial.print(bmp.readAltitude());
-    Serial.print("    ");
+    Serial.print("         ");
    
     Serial.print(bmp.readSealevelPressure());
-    Serial.print("    ");
+    Serial.print("            ");
     
     Serial.print(bmp.readAltitude(101500));
-    Serial.println("    ");
-    
-    
-    Serial.println();
+    Serial.println("        ");
+   
     delay(500);
 }
