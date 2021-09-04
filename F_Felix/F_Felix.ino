4a970c232c46daf4a4358efde01d7224bac7d310
Fernanda Felix <fernanda.felix0@outlook.com>
2021-09-03 22:52:12 -0300

    Update F_Felix.ino
    aumentando Serial.begin

--- F_Felix/F_Felix.ino
+++ F_Felix/F_Felix.ino
@@ -7,7 +7,7 @@ void setup() {
     // initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);
   
-    Serial.begin(9600);
+    Serial.begin(115200);
     if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
