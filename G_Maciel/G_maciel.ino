4690ded0ae9fba5569c7508955b0079d7f9a3b8d
gabimaciels <gabimsilveira02@gmail.com>
2021-09-07 22:07:15 -0300

    update apogeu

--- G_Maciel/G_maciel.ino
+++ G_Maciel/G_maciel.ino
@@ -1,5 +1,7 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
+float velhaAlt=0.0;
+float novaAlt=0.0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -10,26 +12,27 @@ void setup() {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   }
-  Serial.println("Temperature [*C]\tPressure [Pa]\tAltitude [m]\tPressure at sealevel (calculated) [Pa]\tReal altitude [m]"); 
+  
+  Serial.println("Altitude [m]\tApogeu"); 
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  
-  Serial.print(bmp.readTemperature());
-  Serial.print("\t");
-    
-  Serial.print(bmp.readPressure());
-  Serial.print("\t");
+
+  float velhaAlt=novaAlt;
   
   // Calculate altitude assuming 'standard' barometric
   // pressure of 1013.25 millibar = 101325 Pascal
-  Serial.print(bmp.readAltitude());
+  float novaAlt=bmp.readAltitude();
+  Serial.print(novaAlt);
   Serial.print("\t");
-
-  Serial.print(bmp.readSealevelPressure());
-  Serial.print("\t");
-
-  Serial.println(bmp.readAltitude(101500));
+  
+  if (novaAlt < velhaAlt) {
+    Serial.println("caindo");
+  }
+  else {
+    Serial.println("subindo");
+  }
 
 }
