4baf93bdddfe417f59bf80913de89920d48acae6
Heitor Rodrigues <heitor.rodrigues.savegnago@outlook.com>
2021-09-10 22:58:09 -0300

    Heck Media Movel

--- Heitor/Heitor.ino
+++ Heitor/Heitor.ino
@@ -1,10 +1,25 @@
 #include <Adafruit_BMP085.h>
 
+#define len 10
+#define coiso vec[len]
+#define idx vec[index]
+#define av_pp av += idx
+#define av_ll av -= idx
+#define av_tt av *= len
+#define av_dd av /= len
+#define update(X) ((++X)%len)
+#define av_fst av_tt;av_ll
+#define av_lst av_pp;av_dd
+
+float coiso;
+int index = 0;
+float av = 0;
+
 Adafruit_BMP085 bmp;
 float solo = 0.0;
 
 void setup() {
-  Serial.begin(250000);
+  Serial.begin(115200);
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
@@ -22,12 +37,20 @@ void loop() {
 
   float temp = bmp.readTemperature();
   int press = bmp.readPressure();
-  float alt = bmp.readAltitude();
+  float alt = bmp.readAltitude()-solo;
+
+  av_fst;
+  idx = alt;
+  av_lst;
+  index = update(index);
+
 //   Serial.print(temp);
 //   Serial.print("\t");
 //   Serial.print(press);
 //   Serial.print("\t");
-  Serial.print(alt-solo);
+  Serial.print(alt);
+  Serial.print("\t");
+  Serial.print(av);
   Serial.print("\t");
 
   Serial.println();
