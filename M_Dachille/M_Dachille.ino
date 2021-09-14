b07f21a5f7d48babc2c9c6a851e04b90609a0a8a
dachillematheus <matheusdachille@hotmail.com>
2021-09-14 20:13:49 -0300

    nona versão

--- M_Dachille/M_Dachille.ino
+++ M_Dachille/M_Dachille.ino
@@ -1,16 +1,16 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
 
-  float primeira_altitude, segunda_altitude;
+float primeira_altitude, segunda_altitude;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   if (!bmp.begin()) {
-  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-  while (1) {}
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
-    // Criando a variável 
+  // Criando a variável
 
 }
 
@@ -18,12 +18,12 @@ void loop() {
 
   primeira_altitude = bmp.readAltitude();
   // Comparando as variáveis
-  if(primeira_altitude < segunda_altitude);
-  digitalWrite(LED_BUILTIN, HIGH);
-  
-    //Altitude
+  if (primeira_altitude > segunda_altitude){
+    Serial.println("caindo");
+  }
+  //Altitude
   Serial.print(primeira_altitude);
-  Serial.print("\t");                     
+  Serial.print("\t");
   segunda_altitude = primeira_altitude;
   Serial.println();
 }
