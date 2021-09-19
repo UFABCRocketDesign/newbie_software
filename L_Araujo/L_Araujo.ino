93310b82de7084d705481325e8f3fc055755a056
LidiaAraujo <lidia.victoria@aluno.ufabc.edu.br>
2021-09-19 18:20:28 -0300

    Update L_Araujo.ino

--- L_Araujo/L_Araujo.ino
+++ L_Araujo/L_Araujo.ino
@@ -4,6 +4,13 @@
 
 Adafruit_BMP085 bmp;
 
+void printa_linha() {
+  for (int i = 0; i < 70; i++) {
+    Serial.print("_");
+  }
+  Serial.println("");
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -11,13 +18,11 @@ void setup() {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   }
-}
 
-void printa_linha() {
-  for (int i = 0; i < 55; i++) {
-    Serial.print("_");
-  }
-  Serial.println("");
+  printa_linha();
+  Serial.println("| Temperatura (°C) |  Pressão (Pa)  |  Altitude  (meters) |  Pressão a nível do mar (Pa) |  Real Altitude (meters)|");
+  printa_linha();
+  
 }
   
 void loop() {
@@ -28,35 +33,30 @@ void loop() {
       digitalWrite(LED_BUILTIN, LOW);
       delay(500);
     }
-
-    printa_linha();
-    Serial.println("| Temperatura |  Pressão  |  Altitude  |  Pressão a nível do mar |  Real Altitude |");
-    printa_linha();
            
     //Mede temperatura
     Serial.print(bmp.readTemperature());
-    Serial.print(" *C    |");
+    Serial.print("|");
 
     Serial.print(bmp.readPressure());
-    Serial.print(" Pa     |");
+    Serial.print("|");
     
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
     Serial.print(bmp.readAltitude());
-    Serial.print(" meters    |");
+    Serial.print("|");
 
     Serial.print(bmp.readSealevelPressure());
-    Serial.print(" Pa     |");
+    Serial.print("|");
 
   // you can get a more precise measurement of altitude
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
     Serial.print(bmp.readAltitude(101500));
-    Serial.print(" meters     |");
+    Serial.print("|");
     
     Serial.println();
-    printa_linha();
     
     delay(500);
 }
