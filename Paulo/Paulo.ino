35ab97d393cf5d30a95d498716f7cbfccb88b26a
paulhartmann07 <phenriquehsales@gmail.com>
2021-09-21 16:29:31 -0300

    #01-filtro_apogeu

--- Paulo/Paulo.ino
+++ Paulo/Paulo.ino
@@ -1,32 +1,10 @@
 #include <Adafruit_BMP085.h>
 
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
 Adafruit_BMP085 bmp; // Declaração da biblioteca
 float altitudeLeitura, nova_altLeitura, cont_sub, cont_subidas, cont_desc, ult_subida;
-float altura_inicio, media_alt_inicio, i;
+float altura_inicio, media_alt_inicio;
+int j;
+float list_media_movel[10], media_movel;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -41,24 +19,31 @@ void setup() {
   }
 
   //Serial.println("Temperature (ºC)\tPressure (Pa)\tAltitude(m)\tPressure at sealevel (calculated) (Pa)\tReal altitude (m) ");
-
+  Serial.println("Altitude\tMedia Movel(10)");
+  
   // Medicao
-  for (i=0; i<10; i++) {
+  for (j=0; j<10; j++) {
     altura_inicio = bmp.readAltitude();
     media_alt_inicio = media_alt_inicio + altura_inicio;
     delay (100);
+    list_media_movel[j] = altura_inicio;
   }
-  media_alt_inicio = media_alt_inicio / i;
+  media_alt_inicio = media_alt_inicio / 10;
 
+  // Inicialização de variáveis
   nova_altLeitura = bmp.readAltitude() - media_alt_inicio;
   ult_subida = 0;
   cont_subidas = 0;
   cont_sub = 0;
   cont_desc = 0;
+  media_movel = 0;
+  
 }
 
 // the loop function runs over and over again forever
 void loop() {
+
+    /*
     // Zerar contagem de altitude
     if (cont_sub > 0 and cont_desc == 1) {
       cont_sub = 0;
@@ -66,6 +51,7 @@ void loop() {
     if (cont_desc > 0 and cont_sub == 1) {
       cont_desc = 0;
     }
+    */
     
     // Detecção de Apogeu
     altitudeLeitura = nova_altLeitura;
@@ -77,6 +63,17 @@ void loop() {
       cont_desc += 1;
     }
 
+    // Media Movel
+    for (j=0; j<10; j++) {
+      media_movel = media_movel + list_media_movel[j];
+      if (j < 9) {
+        list_media_movel[j] = list_media_movel[j+1];
+      }
+    }
+    media_movel = (media_movel + nova_altLeitura) / 11;
+    list_media_movel[9] = nova_altLeitura;
+
+    /*
     if (cont_sub > 10) {
       Serial.print("Subindo");
       cont_subidas = 1;
@@ -89,33 +86,13 @@ void loop() {
       Serial.print("Apogeu em: ");
       Serial.println(ult_subida);
     }
-    
-    // BMP085 - TESTE
-      // Criação de Colunas - 2º Linha:
-        // Temperatura
-    /*
-    Serial.print(bmp.readTemperature());
-    Serial.print("\t");
-        // Pressão
-    Serial.print(bmp.readPressure());
-    Serial.print("\t");
     */
-        // Altitude
+
+        // Altura
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
     Serial.print(nova_altLeitura);
-    Serial.println(" m");
-    /*
     Serial.print("\t");
-        // Pressão nivel do mar
-    Serial.print(bmp.readSealevelPressure());
-    Serial.print("\t");
-        // Altitude Real
-    // you can get a more precise measurement of altitude
-    // if you know the current sea level pressure which will
-    // vary with weather and such. If it is 1015 millibars
-    // that is equal to 101500 Pascals.
-    Serial.println(bmp.readAltitude(101500));
-    */
+    Serial.print(media_movel);
        
 }
