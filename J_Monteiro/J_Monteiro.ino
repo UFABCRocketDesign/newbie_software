ebb8fba7960c58146c1724ff1901bb7dc3c118bf
Jaqueline Monteiro <90111052+montjaqueline@users.noreply.github.com>
2021-09-27 18:23:40 -0300

    Update J_Monteiro.ino

--- J_Monteiro/J_Monteiro.ino
+++ J_Monteiro/J_Monteiro.ino
@@ -5,10 +5,11 @@ Adafruit_BMP085 bmp;
 void setup() {
   Serial.begin(9600);
   if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   
   }
-  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  
   Serial.print("Temperature = ");
   Serial.print(" Pa");
   Serial.print("  Pressure = ");
@@ -21,21 +22,21 @@ void setup() {
   
 void loop() {
     
-    Serial.print(bmp.readTemperature());
+    Serial.print(bmp.readTemperature()  );
     
-    Serial.print( bmp.readPressure());
+    Serial.print( bmp.readPressure()  );
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal 
-    Serial.print( bmp.readAltitude());
+    Serial.print( bmp.readAltitude()  );
    
-    Serial.print( bmp.readSealevelPressure());
+    Serial.print( bmp.readSealevelPressure()  );
    
 
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
   
-    Serial.println( bmp.readAltitude(101500));
+    Serial.print( bmp.readAltitude(101500));
     
     Serial.println();
-    delay(100);
+ 
 }
