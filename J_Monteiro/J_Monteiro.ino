cbe6e40f5371123c19e81146507cbed1f0adc33f
Jaqueline Monteiro <90111052+montjaqueline@users.noreply.github.com>
2021-09-27 18:36:26 -0300

    Update J_Monteiro.ino

--- J_Monteiro/J_Monteiro.ino
+++ J_Monteiro/J_Monteiro.ino
@@ -10,37 +10,37 @@ void setup() {
   
   }
   
-  Serial.print("Temperature = ");
-  Serial.print(" Pa");
-  Serial.print("  Pressure = ");
-  Serial.print(" Pa");
-  Serial.print("  Pressure at sealevel (calculated) = ");
-  Serial.print(" Pa");
-  Serial.print("  Real altitude = ");
-  Serial.println(" meters");
+  Serial.print("Temperature =  \t");
+  Serial.print(" Pa \t");
+  Serial.print("  Pressure = \t");
+  Serial.print(" Pa \t");
+  Serial.print("  Pressure at sealevel (calculated) = \t");
+  Serial.print(" Pa \t");
+  Serial.print("  Real altitude = \t");
+  Serial.println(" meters \t");
 }
   
 void loop() {
     
     Serial.print(bmp.readTemperature() );
-    Serial.print("  ");
+    Serial.print('\t');
     
     Serial.print( bmp.readPressure()  );
-    Serial.print("  ");
+    Serial.print('\t');
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal 
     Serial.print( bmp.readAltitude());
-    Serial.print("  ");
+    Serial.print('\t');
    
     Serial.print( bmp.readSealevelPressure() );
-    Serial.print("  ");
+    Serial.print('\t');
    
 
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
   
     Serial.print( bmp.readAltitude(101500));
-    Serial.print("  ");
+    Serial.print('\t');
     
     Serial.println();
  
