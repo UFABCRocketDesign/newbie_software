359745e3f2b04d7e26bdf3848b0c74d67f99057c
BrunoCarvalho0304 <bruno.felipe@aluno.ufabc.edu.br>
2021-10-02 16:48:08 -0300

    Update B_Carvalho.ino

--- B_Carvalho/B_Carvalho.ino
+++ B_Carvalho/B_Carvalho.ino
@@ -5,15 +5,15 @@ Adafruit_BMP085 bmp;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-  Serial.println("Temperature(°C ) = ");
-  Serial.println(" ");
-  Serial.println("Pressure(Pa) = ");
-  Serial.println(" ");
-  Serial.println("Altitude(m) = ");
+  Serial.print("Temperature(°C ) = ");
+  Serial.print(" ");
+  Serial.print("Pressure(Pa) = ");
+  Serial.print(" ");
+  Serial.print("Altitude(m) = ");
   Serial.print("Pressure at sealevel (calculated as Pa) = ");
-  Serial.println(" ");
+  Serial.print(" ");
   Serial.print("Real altitude(m) = ");
-  Serial.println(" ");
+  Serial.print(" ");
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
@@ -24,28 +24,28 @@ void setup() {
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.print(bmp.readTemperature());
-  Serial.println(" ");
+  Serial.print(" ");
   digitalWrite(LED_BUILTIN, LOW);
 
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.print(bmp.readPressure());
-  Serial.println(" ");
+  Serial.print(" ");
   digitalWrite(LED_BUILTIN, LOW);
 
   // Calculate altitude assuming 'standard' barometric
   // pressure of 1013.25 millibar = 101325 Pascal
   Serial.print(bmp.readAltitude());
-  Serial.println(" ");
+  Serial.print(" ");
 
   Serial.print(bmp.readSealevelPressure());
-  Serial.println(" ");
+  Serial.print(" ");
 
   // you can get a more precise measurement of altitude
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
   Serial.print(bmp.readAltitude(101500));
-  Serial.println(" ");
+  Serial.print(" ");
 
   Serial.println();
   delay(500);
