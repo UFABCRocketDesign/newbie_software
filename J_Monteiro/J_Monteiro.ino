1d2e4b998861c29b199ce9979621122df678c7bc
Jaqueline Monteiro <90111052+montjaqueline@users.noreply.github.com>
2021-10-04 18:15:13 -0300

    Update J_Monteiro.ino

--- J_Monteiro/J_Monteiro.ino
+++ J_Monteiro/J_Monteiro.ino
@@ -1,18 +1,15 @@
 #include <Adafruit_BMP085.h>
-  
+
 Adafruit_BMP085 bmp;
 
-  float relative_average;
-  float current_Altitude = bmp.readAltitude();
-  float previous_Altitude;
- 
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   if (!bmp.begin()) {
- Serial.println("Could not find a valid BMP085 sensor");
-  while (1) {}
-   pinMode(LED_BUILTIN, OUTPUT);
+    Serial.println("Could not find a valid BMP085 sensor");
+    while (1) {}
+    pinMode(LED_BUILTIN, OUTPUT);
   }
   Serial.print("Temperature =  \t");
   Serial.print(" Pa \t");
@@ -23,56 +20,59 @@ void setup() {
   Serial.print("  Real altitude = \t");
   Serial.println(" meters \t");
 }
-  
+
 void loop() {
-  
+
   float altitude = 0;
-  
-   Serial.print(bmp.readTemperature() );
-    Serial.print('\t');
-    
-    Serial.print( bmp.readPressure()  );
-    Serial.print('\t');
-    // Calculate altitude assuming 'standard' barometric
-    // pressure of 1013.25 millibar = 101325 Pascal 
-    Serial.print( bmp.readAltitude());
-    Serial.print('\t');
-   
-    Serial.print( bmp.readSealevelPressure() );
-    Serial.print('\t');
-   
+  float relative_average;
+  float current_Altitude = bmp.readAltitude();
+  float previous_Altitude;
+
+
+  Serial.print(bmp.readTemperature() );
+  Serial.print('\t');
+
+  Serial.print( bmp.readPressure()  );
+  Serial.print('\t');
+  // Calculate altitude assuming 'standard' barometric
+  // pressure of 1013.25 millibar = 101325 Pascal
+  Serial.print( bmp.readAltitude());
+  Serial.print('\t');
+
+  Serial.print( bmp.readSealevelPressure() );
+  Serial.print('\t');
+
 
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
-  
-    Serial.print( bmp.readAltitude(101500));
-    Serial.print('\t');
-    
-    Serial.println();
-    
 
-      for ( float i =1; i < 15; i ++){
+  Serial.print( bmp.readAltitude(101500));
+  Serial.print('\t');
 
-             ( current_Altitude >  altitude );
-             altitude = altitude + current_Altitude;
-               
-               relative_average = (  altitude )/ i;     
-        
-        
-        if (relative_average  < current_Altitude){
-    
+  Serial.println();
+
+
+  for ( float i = 1; i < 15; i ++) {
+
+    ( current_Altitude >  altitude );
+    altitude = altitude + current_Altitude;
+
+    relative_average = (  altitude ) / i;
+
+
+    if (relative_average  < current_Altitude) {
+
       Serial.print("Subida \t");
       digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-     
+
     }
-     else{
+    else {
 
       Serial.print("Descida \t");
       digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level);
-      
-        }
-      }
-      
-     
+
+    }
+  }
+
+
 }
-     
