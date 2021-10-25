4fbfbd94f5566a7a60fa94d6fe5a3bffde7e2425
gabimaciels <gabimsilveira02@gmail.com>
2021-10-25 18:47:32 -0300

    apogeu #01

--- G_Maciel/G_maciel.ino
+++ G_Maciel/G_maciel.ino
@@ -3,9 +3,15 @@
 #include <SD.h>
 
 #define l 20 // tamanho
+#define IGN_1 36  /*act1*/
+#define IGN_2 61  /*act2*/
+#define IGN_3 46  /*act3*/
+#define IGN_4 55  /*act4*/
+
 Adafruit_BMP085 bmp;
 
 float novaAlt=0.0;
+float velhaAlt=0.0;
 float media=0.0;
 float h = 0.0;
 float lista[l];
@@ -13,13 +19,19 @@ float lista2[l];
 float media_mov = 0;
 float media_mov2 = 0;
 const int chipSelect = 53;
-String cabecalho = "Altitude [m]\tAltura [m]\tFiltro1 (h)\tFiltro2 (h)\tTemperatura [*C]\tPressao [Pa]\tPressao no nivel do mar [Pa]";
-// float velhaAlt=0.0;
+String cabecalho = "Altitude [m]\tAltura [m]\tFiltro1 (h)\tFiltro2 (h)\tTemperatura [*C]\tPressao [Pa]\tPressao no nivel do mar [Pa]\tApogeu";
 String nomeArquivo = "";
+
+const int ledPin =  LED_BUILTIN;
+int ledState = LOW;    // ledState used to set the LED
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;        // will store last time LED was updated
+const long intervalo = 10000;      // interval at which to blink (milliseconds)
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  // pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
   if (!bmp.begin()) {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
@@ -70,6 +82,8 @@ void setup() {
 void loop() {
   // make a string for assembling the data to log:
   String dataString = "";
+
+  unsigned long currentMillis = millis();
   
   // Calculate altitude assuming 'standard' barometric
   // pressure of 1013.25 millibar = 101325 Pascal
@@ -104,25 +118,37 @@ void loop() {
   dataString += String(media_mov2);
   dataString += "\t";
 
-  float temp =  bmp.readTemperature(); //temperatura
+  //temperatura
+  float temp =  bmp.readTemperature(); 
   dataString += String(temp);
   dataString += "\t";
-  float pressao = bmp.readPressure(); //pressão
+  //pressão
+  float pressao = bmp.readPressure(); 
   dataString += String(pressao);
   dataString += "\t";
-  float pressaoNivelMar = bmp.readSealevelPressure(); //pressão no nível do mar
+  //pressão no nível do mar
+  float pressaoNivelMar = bmp.readSealevelPressure(); 
   dataString += String(pressaoNivelMar);
+  dataString += "\t";
+    
+  // encontrando apogeu 
   
+  if (h < velhaAlt) {
+    dataString += "Apogeu Detectado!";
+    previousMillis = millis();
+    ledState = HIGH;
+  }
+  else {
+    dataString += " ";   
+  }
+  if (previousMillis != 0) {
+    if (currentMillis - previousMillis >= intervalo) {
+      ledState = LOW;
+    }
+  }
+  digitalWrite(ledPin, ledState);
+
   Serial.println(dataString);
-  // Serial.print("\t");
-  //if (h < velhaAlt) {
-    //Serial.println("caindo");
-    //velhaAlt = h;
-  //}
-  //if (h > velhaAlt) {
-    //Serial.println("subindo");
-    //velhaAlt = h;
-  //}
   
   File dataFile = SD.open(nomeArquivo, FILE_WRITE);
 
@@ -138,4 +164,5 @@ void loop() {
   
   media_mov = 0;
   media_mov2 = 0;
+  velhaAlt = h;
 }
