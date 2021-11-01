ab5de3b66395d18bc540b4ad3eda00cb0779279c
Jaqueline Monteiro <90111052+montjaqueline@users.noreply.github.com>
2021-11-01 19:29:28 -0300

    Update J_Monteiro.ino

--- J_Monteiro/J_Monteiro.ino
+++ J_Monteiro/J_Monteiro.ino
@@ -56,26 +56,26 @@ void loop() {
   Serial.print('\t');
 
 
-
-  Serial.print(bmp.readTemperature() );
-  Serial.print('\t');
-
-  Serial.print( bmp.readPressure()  );
-  Serial.print('\t');
-  // Calculate altitude assuming 'standard' barometric
-  // pressure of 1013.25 millibar = 101325 Pascal
-  Serial.print( current_Altitude);
-  Serial.print('\t');
-
-  Serial.print( bmp.readSealevelPressure() );
-  Serial.print('\t');
-
-
-  // vary with weather and such. If it is 1015 millibars
-  // that is equal to 101500 Pascals.
-
-  Serial.print( bmp.readAltitude(101500));
-  Serial.print('\t');
+//
+//  Serial.print(bmp.readTemperature() );
+//  Serial.print('\t');
+//
+//  Serial.print( bmp.readPressure()  );
+//  Serial.print('\t');
+//  // Calculate altitude assuming 'standard' barometric
+//  // pressure of 1013.25 millibar = 101325 Pascal
+//  Serial.print( current_Altitude);
+//  Serial.print('\t');
+//
+//  Serial.print( bmp.readSealevelPressure() );
+//  Serial.print('\t');
+//
+//
+//  // vary with weather and such. If it is 1015 millibars
+//  // that is equal to 101500 Pascals.
+//
+//  Serial.print( bmp.readAltitude(101500));
+//  Serial.print('\t');
 
 
   Serial.print(current_high);
@@ -89,10 +89,10 @@ void loop() {
   }
   vet[0] = current_high;
 
-    High = 0;
+  High = 0;
 
   for ( int i = 0; i < n ; i ++) {
-  
+
     High = High + vet[i];
   }
   relative_average = High / n;
@@ -100,13 +100,13 @@ void loop() {
   Serial.print('\t');
 
 
-   if (relative_average < current_high ) {
+  if (relative_average < current_high ) {
 
     Serial.print("Subida \t");
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 
   }
-    else {
+  else {
 
     Serial.print("Descida \t");
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (LOW is the voltage level);
