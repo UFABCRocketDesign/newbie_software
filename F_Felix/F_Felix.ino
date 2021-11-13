e983c907c4d0f17e59fe47d8ce535514fc8aebfc
Fernanda Felix <fernanda.felix0@outlook.com>
2021-11-13 14:37:10 -0300

    Update F_Felix.ino
    acelerometro #2

--- F_Felix/F_Felix.ino
+++ F_Felix/F_Felix.ino
@@ -2,7 +2,8 @@
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
-#include <ADXL345.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
 #define IGN_1 36  /*act1*/
 #define IGN_2 61  /*act2*/
@@ -12,7 +13,7 @@
 #define intervalo2 5000
 
 Adafruit_BMP085 bmp;
-ADXL345 adxl;
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 float auxiliar = 0;
 float media = 0;
 float soma = 0;
@@ -44,53 +45,15 @@ void setup() {
 
   Serial.begin(115200);
 
-  adxl.powerOn();
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
 
-  //set activity/ inactivity thresholds (0-255)
-  adxl.setActivityThreshold(75); //62.5mg per increment
-  adxl.setInactivityThreshold(75); //62.5mg per increment
-  adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
- 
-  //look of activity movement on this axes - 1 == on; 0 == off 
-  adxl.setActivityX(1);
-  adxl.setActivityY(1);
-  adxl.setActivityZ(1);
- 
-  //look of inactivity movement on this axes - 1 == on; 0 == off
-  adxl.setInactivityX(1);
-  adxl.setInactivityY(1);
-  adxl.setInactivityZ(1);
- 
-  //look of tap movement on this axes - 1 == on; 0 == off
-  adxl.setTapDetectionOnX(0);
-  adxl.setTapDetectionOnY(0);
-  adxl.setTapDetectionOnZ(1);
- 
-  //set values for what is a tap, and what is a double tap (0-255)
-  adxl.setTapThreshold(50); //62.5mg per increment
-  adxl.setTapDuration(15); //625us per increment
-  adxl.setDoubleTapLatency(80); //1.25ms per increment
-  adxl.setDoubleTapWindow(200); //1.25ms per increment
- 
-  //set values for what is considered freefall (0-255)
-  adxl.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
-  adxl.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
- 
-  //setting all interrupts to take place on int pin 1
-  //I had issues with int pin 2, was unable to reset it
-  adxl.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
-  adxl.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
-  adxl.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
-  adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
-  adxl.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
- 
-  //register interrupt actions - 1 == on; 0 == off  
-  adxl.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
-  adxl.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
-  adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
-  adxl.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
-  adxl.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
-
+  accel.setRange(ADXL345_RANGE_16_G);
+  
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
@@ -175,34 +138,13 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
    
-  int x,y,z;  
-  adxl.readXYZ(&x, &y, &z);
-  
-  Serial.print("values of X , Y , Z: ");
-  Serial.print(x);
-  Serial.print(" , ");
-  Serial.print(y);
-  Serial.print(" , ");
-  Serial.println(z);
-  
-  double xyz[3];
-  double ax,ay,az;
-  
-  adxl.getAcceleration(xyz);
-  
-  ax = xyz[0];
-  ay = xyz[1];
-  az = xyz[2];
-  Serial.print("X=");
-  Serial.print(ax);
-  Serial.println(" g");
-  Serial.print("Y=");
-  Serial.print(ay);
-  Serial.println(" g");
-  Serial.print("Z=");
-  Serial.print(az);
-  Serial.println(" g");
-  Serial.println("**********************");
+  sensors_event_t event; 
+  accel.getEvent(&event);
+ 
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("\t");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("\t");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("\t");Serial.println();
 
   tempoAtual = millis();
 
@@ -341,5 +283,5 @@ void loop() {
 
   auxiliar = alt;
 
-  // delay(100);
+  delay(100);
 }
