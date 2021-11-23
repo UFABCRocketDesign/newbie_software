7950f703be31c39f951b789254bb04f4b11682e7
narumi2358 <anafugita@gmail.com>
2021-11-23 17:20:42 -0300

    filtro1

--- A_Fugita/A_Fugita.ino
+++ A_Fugita/A_Fugita.ino
@@ -1,53 +1,61 @@
 
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
+#define tam 10
+#define zer 10
+float zerar = 0;
+float filtro[tam] = {};
 
-// the setup function runs once when you press reset or power the board
-
-float media = 0;
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  //pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
   }
   
-  for(int i = 0;i<10;i++){
+  for(int i = 0;i<zer;i++){ //deixando na altitude 0
     float valor = bmp.readAltitude();
-    media = media + valor;
+    zerar+= valor;
   }
-  media = media/10;
+  zerar /= zer;
   
   //Serial.println("Temperature\tPressure\tAltitude\tPressure at sealevel (calculated)\tReal Altitude"); 
 }
 
-// the loop function runs over and over again forever
+
 void loop() {
+  float alt_med= 0;
+  float f1 = bmp.readAltitude()- zerar;
   //Serial.print("Temperature = ");
   //Serial.print(bmp.readTemperature());
   //Serial.print("\t");
-
   //Serial.print("Pressure = ");
   //Serial.print(bmp.readPressure());
   //Serial.print("\t");
 
-  // Calculate altitude assuming 'standard' barometric
-  // pressure of 1013.25 millibar = 101325 Pascal
+  for (int i=tam-1; i>0; i--){
+    filtro[i] = filtro[i-1];
+  }
+  filtro[0] = f1;
+  
+  
+  for (int i=0; i<tam; i++){
+    alt_med += filtro[i];
+  }
+  alt_med /= tam;
+
+
   //Serial.print("Altitude = ");
   
-  Serial.print(bmp.readAltitude() - media);
+  Serial.print(f1);
+  Serial.print("\t");
+  Serial.print(alt_med);
   Serial.print("\t");
 
   //Serial.print("Pressure at sealevel (calculated) = ");
   //Serial.print(bmp.readSealevelPressure());
   //Serial.print("\t");
 
-  // you can get a more precise measurement of altitude
-  // if you know the current sea level pressure which will
-  // vary with weather and such. If it is 1015 millibars
-  // that is equal to 101500 Pascals.
   //Serial.print("Real altitude = ");
   //Serial.print(bmp.readAltitude(101500));
   Serial.println();
