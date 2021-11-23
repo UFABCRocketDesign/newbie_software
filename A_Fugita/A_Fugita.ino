46903ce4329f494c9b90a68c85809de986ca2eae
narumi2358 <anafugita@gmail.com>
2021-11-23 17:39:02 -0300

    filtronsei
    teste de 20

--- A_Fugita/A_Fugita.ino
+++ A_Fugita/A_Fugita.ino
@@ -1,8 +1,8 @@
 
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
-#define tam 100
-#define zer 100
+#define tam 20
+#define zer 20
 float zerar = 0;
 float filtro[tam] = {};
 float filtro1[tam] = {};
