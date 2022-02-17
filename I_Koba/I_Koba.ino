4649af904614c3bcfb9199b08998d8d867e660e2
ikobayashidesu <igorkobayashi@hotmail.com>
2022-02-17 20:13:45 -0300

    Led com sensor
    nao sei oque ele faz ainda

--- I_Koba/I_Koba.ino
+++ I_Koba/I_Koba.ino
@@ -1,11 +1,41 @@
-
+#include <Adafruit_BMP085.h>
 #define led 13 
+Adafruit_BMP085 bmp;
+
 void setup() {
  pinMode(led, OUTPUT);
-
+ Serial.begin(9600);
+ if (!bmp.begin()) {
+ Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+ while (1) {}
+  }
 }
 
 void loop() {
+  
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+    
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+    
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.println(" meters");
+    
+  Serial.println();
+  delay(500);
+ 
   digitalWrite(led, HIGH);
   delay(2000);
   digitalWrite(led, LOW);
@@ -22,4 +52,5 @@ void loop() {
   delay(2000);
   digitalWrite(led, LOW);
   delay(2000);
+  
 }
