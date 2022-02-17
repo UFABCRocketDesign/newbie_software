153d7163aa0d02f63a3f8bc84ce6ca58215a91c1
ikobayashidesu <igorkobayashi@hotmail.com>
2022-02-17 19:56:06 -0300

    LED de 4 piscadinha
    O led irá piscar 4 vezes por período.

---
+++ I_Koba/I_Koba.ino
@@ -0,0 +1,25 @@
+
+#define led 13 
+void setup() {
+ pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(2000);
+  digitalWrite(led, LOW);
+  delay(500);
+  digitalWrite(led, HIGH);
+  delay(2000);
+  digitalWrite(led, LOW);
+  delay(500);
+  digitalWrite(led, HIGH);
+  delay(2000);
+  digitalWrite(led, LOW);
+  delay(500);
+  digitalWrite(led, HIGH);
+  delay(2000);
+  digitalWrite(led, LOW);
+  delay(2000);
+}
