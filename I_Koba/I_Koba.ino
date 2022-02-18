03de76518d5602311573bcb097a7c3d755c349e5
ikobayashidesu <igorkobayashi@hotmail.com>
2022-02-17 21:15:56 -0300

    att4

--- I_Koba/I_Koba.ino
+++ I_Koba/I_Koba.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_BMP085.h>
 #define led 13 
 Adafruit_BMP085 bmp;
+float z, resultado, ta;
 
 void setup() {
  pinMode(led, OUTPUT);
@@ -8,19 +9,23 @@ void setup() {
  if (!bmp.begin()) {
  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   }
+  media();
   Serial.print("Temp (*C)\t");
   Serial.print("Pres (Pa)\t");
   Serial.print("Alt (m)\t");
   Serial.println();
+
 }
 
 void loop() {
-  
+
+ta = bmp.readAltitude() - resultado;
+ 
 // Serial.print(bmp.readTemperature());
 // Serial.print("\t");
 // Serial.print(bmp.readPressure());
 // Serial.print("\t");
- Serial.print(bmp.readAltitude());
+ Serial.print(ta);
  Serial.print("\t");
  Serial.println();
  
@@ -42,4 +47,12 @@ void loop() {
 //  digitalWrite(led, LOW);
 //  delay(2000);
   
+}
+
+void media() {
+  z = 0;
+ for(int x = 0; x < 100; x++){
+   z = bmp.readAltitude() + z;
+ }
+ resultado = z/100;
 }
