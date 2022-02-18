4da317b209a80a953d474b949b3ec0cfdb5ac224
gabimaciels <gabimsilveira02@gmail.com>
2022-02-18 14:37:47 -0300

    update

--- G_Maciel/G_maciel.ino
+++ G_Maciel/G_maciel.ino
@@ -18,14 +18,14 @@ L3G gyro;
 #define use_tempo 0
 #define use_sd 0
 #define use_bar 1
-#define use_gyro 0
-#define use_mag 0
-#define use_accel 0
-#define print_serial 1
+#define use_gyro 1
+#define use_mag 1
+#define use_accel 1
+#define print_serial 0
 
-#define use_alt (use_bar && 1)
+#define use_alt (use_bar && 0)
 #define use_pressao (use_bar && 0)
-#define use_temp (use_bar && 0)
+#define use_temp (use_bar && 1)
 #define use_apogeu (use_alt && 0)
 
 #define use_gyro_x (use_gyro && 1)
@@ -48,19 +48,19 @@ L3G gyro;
 #define ledPin LED_BUILTIN
 
 #if (use_alt)
-float velhaAlt=0.0;
-float altura_referencia=0.0;
+float velhaAlt = 0.0;
+float altura_referencia = 0.0;
 float lista[l];
 float lista2[l];
 #endif
 
 #if use_apogeu
-int encontra_apogeu=0;
+int encontra_apogeu = 0;
 bool apogeu_detectado = false;
-bool laco_led_2 = false;      // variavel para entrar no laço liga led 2 
+bool laco_led_2 = false;      // variavel para entrar no laço liga led 2
 bool laco_led_3 = false;      // variavel para entrar no laço liga led 3 (built in)
 bool ledState1 = LOW;    // ledState used to set the LED
-bool ledState2 = LOW; 
+bool ledState2 = LOW;
 bool ledState3 = LOW;
 #define h_paraquedas_2 10  // altura para acionar 2º paraquedas (led2)
 #define interv_desliga_led 7000      // interval at which to blink (milliseconds)
@@ -68,7 +68,7 @@ bool ledState3 = LOW;
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
 unsigned long liga_led2 = 0;        // quando o led tem que acender
-unsigned long desliga_led1 = 0; 
+unsigned long desliga_led1 = 0;
 unsigned long desliga_led2 = 0; // quando o led tem que desligar após o apogeu
 unsigned long desliga_led3 = 0;
 #endif
@@ -76,32 +76,38 @@ unsigned long desliga_led3 = 0;
 #define chipSelect 53
 String nomeArquivo = "";
 
+//////// Função: filtro altura ///////////
+
+#if use_alt
 float filtro_altura(float entrada, int i)
 {
   float media_mov = 0.0;
-  if (i==1){
-   for (int k=0; k<(l-1); k++){
-    lista[k] = lista[k+1];
-   }
-   lista[l-1] = entrada;
-   for (int j=0; j<l; j++){
-    media_mov = media_mov + lista[j];
-   }
-   media_mov = media_mov/l;
+  if (i == 1) {
+    for (int k = 0; k < (l - 1); k++) {
+      lista[k] = lista[k + 1];
+    }
+    lista[l - 1] = entrada;
+    for (int j = 0; j < l; j++) {
+      media_mov = media_mov + lista[j];
+    }
+    media_mov = media_mov / l;
   }
-  
-  else if (i==2){
-   for (int k=0; k<(l-1); k++){
-    lista2[k] = lista2[k+1];
-   }
-   lista2[l-1] = entrada;
-   for (int j=0; j<l; j++){
-    media_mov = media_mov + lista2[j];
-   }
-   media_mov = media_mov/l;
+
+  else if (i == 2) {
+    for (int k = 0; k < (l - 1); k++) {
+      lista2[k] = lista2[k + 1];
+    }
+    lista2[l - 1] = entrada;
+    for (int j = 0; j < l; j++) {
+      media_mov = media_mov + lista2[j];
+    }
+    media_mov = media_mov / l;
   }
-  return media_mov;  
+  return media_mov;
 }
+#endif
+
+////////////////////////////////////////
 
 void setup() {
   String cabecalho = "";
@@ -114,60 +120,60 @@ void setup() {
 
   // TESTANDO SE OS SENSORES/SD ESTÃO FUNCIONANDO
 
-  #if use_bar
+#if use_bar
   if (!bmp.begin()) {
-  #if print_serial
-  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+#if print_serial
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   }
-  #endif // print_serial
-  #endif // bar
+#endif // print_serial
+#endif // bar
 
-  #if use_mag
-    /* Initialise the mag sensor */
-  if(!mag.begin())
+#if use_mag
+  /* Initialise the mag sensor */
+  if (!mag.begin())
   {
-    #if print_serial
+#if print_serial
     /* There was a problem detecting the HMC5883 ... check your connections */
     Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
-    #endif
+#endif
   }
-  #endif  //mag
+#endif  //mag
 
-  #if use_accel
+#if use_accel
   /* Initialise the sensor */
-  if(!accel.begin())
+  if (!accel.begin())
   {
-    #if print_serial
+#if print_serial
     /* There was a problem detecting the ADXL345 ... check your connections */
     Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
-    #endif
+#endif
   }
-  #endif   //accel
+#endif   //accel
 
-  #if use_gyro
+#if use_gyro
   /* gyro */
   if (!gyro.init())
   {
-    #if print_serial
+#if print_serial
     Serial.println("Failed to autodetect gyro type!");
-    #endif
+#endif
   }
   gyro.enableDefault();
-  #endif   // gyro
-  
-  #if use_sd
+#endif   // gyro
+
+#if use_sd
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    #if print_serial
+#if print_serial
     Serial.println("Card failed, or not present");
-    #endif
+#endif
   }
-  #if print_serial
+#if print_serial
   Serial.println("card initialized.");
-  #endif // print_serial
-  #endif // use_sd
+#endif // print_serial
+#endif // use_sd
 
-  #if use_accel
+#if use_accel
   // ACCELEROMETER RANGE
 
   /* Set the range to whatever is appropriate for your project */
@@ -175,73 +181,73 @@ void setup() {
   // accel.setRange(ADXL345_RANGE_8_G);
   // accel.setRange(ADXL345_RANGE_4_G);
   // accel.setRange(ADXL345_RANGE_2_G);
-  #endif
+#endif
 
-  #if (use_alt)
+#if (use_alt)
   // MÉDIA DE ALTITUDE
-  
-  for (int i=0; i<20; i++) {
+
+  for (int i = 0; i < 20; i++) {
     altura_referencia =  altura_referencia + bmp.readAltitude();
   }
   altura_referencia = altura_referencia / 20;
-  #endif
+#endif
 
   // CABEÇALHO
 
-  #if use_tempo
+#if use_tempo
   cabecalho += "Tempo [s]\t";
-  #endif
-  
-  #if (use_alt)
+#endif
+
+#if (use_alt)
   cabecalho += "Altura [m]\tFiltro1 (h)\tFiltro2 (h)\t";
-  #endif
-  
-  #if (use_temp)
+#endif
+
+#if (use_temp)
   cabecalho += "Temperatura [*C]\t";
-  #endif
-  
-  #if (use_pressao)
+#endif
+
+#if (use_pressao)
   cabecalho += "Pressao [Pa]\t";   //Pressao no nivel do mar [Pa]";
-  #endif
-  
-  #if use_mag_x
+#endif
+
+#if use_mag_x
   cabecalho += "Mag eixo X [uT]\t";
-  #endif
-  #if use_mag_y
+#endif
+#if use_mag_y
   cabecalho += "Mag eixo Y [uT]\t";
-  #endif
-  #if use_mag_z
+#endif
+#if use_mag_z
   cabecalho += "Mag eixo Z [uT]\t";
-  #endif
-  
-  #if use_accel_x
+#endif
+
+#if use_accel_x
   cabecalho += "Accel eixo X [m/s^2]\t";
-  #endif
-  #if use_accel_y
+#endif
+#if use_accel_y
   cabecalho += "Accel eixo Y [m/s^2]\t";
-  #endif
-  #if use_accel_z
+#endif
+#if use_accel_z
   cabecalho += "Accel eixo Z [m/s^2]\t";
-  #endif
-  
-  #if use_gyro_x
+#endif
+
+#if use_gyro_x
   cabecalho += "Gyro eixo X\t";
-  #endif
-  #if use_gyro_y
+#endif
+#if use_gyro_y
   cabecalho += "Gyro eixo Y\t";
-  #endif
-  #if use_gyro_z
+#endif
+#if use_gyro_z
   cabecalho += "Gyro eixo Z\t";
-  #endif
-  
-  #if (use_alt)
+#endif
+
+#if (use_alt)
   cabecalho += "Apogeu\t";
-  #endif
+#endif
 
   // CRIANDO UM NOVO ARQUIVO PARA SALVAR NO SD
 
-  #if use_sd
-  String nome = "gabi"; 
+#if use_sd
+  String nome = "gabi";
   int tamNome = nome.length();
   int num = 0;
   String txt = ".txt";
@@ -250,7 +256,7 @@ void setup() {
     String zeros = "";
     int tamNum = String(num).length();
     int tamTotal = tamNome + tamNum;
-    for (int j=0; j < (8-tamTotal); j++) {
+    for (int j = 0; j < (8 - tamTotal); j++) {
       zeros = zeros + "0";
     }
     nomeArquivo = nome + zeros + String(num) + txt;
@@ -265,137 +271,119 @@ void setup() {
       tmp = true;
     }
   }
-  #if print_serial
+#if print_serial
   Serial.println("file: " + nomeArquivo);
-  #endif // print_serial
-  #endif // use_sd
+#endif // print_serial
+#endif // use_sd
 
-  #if print_serial
+#if print_serial
   Serial.println(cabecalho);
-  #endif
+#endif
 }
 
 void loop() {
   unsigned long t_atual = millis();
-  float t_atual_segundos = t_atual/1000.0;
+  float t_atual_segundos = t_atual / 1000.0;
   // make a string for assembling the data to log:
   String dataString = "";
-  
+
   //// LEITURA DE SENSORES
 
-  #if use_alt
+#if use_alt
   float h = 0.0;
   // Calculate altitude assuming 'standard' barometric pressure of 1013.25 millibar = 101325 Pascal
-  float novaAlt=bmp.readAltitude();
-  h = novaAlt - altura_referencia;  //altura 
-  #endif
+  float novaAlt = bmp.readAltitude();
+  h = novaAlt - altura_referencia;  //altura
+#endif
 
-  #if use_temp
+#if use_temp
   //temperatura
-  float temp =  bmp.readTemperature(); 
-  #endif
+  float temp =  bmp.readTemperature();
+#endif
 
-  #if use_pressao
+#if use_pressao
   //pressão
-  float pressao = bmp.readPressure(); 
-     //pressão no nível do mar
-     //float pressaoNivelMar = bmp.readSealevelPressure(); 
-  #endif 
-  
-  #if (use_mag || use_accel)
-  /* Get a new sensor event */ 
-  sensors_event_t event;
-  #endif
+  float pressao = bmp.readPressure();
+  //pressão no nível do mar
+  //float pressaoNivelMar = bmp.readSealevelPressure();
+#endif
 
-  #if use_mag
-  // Magnetometro 
+#if (use_mag || use_accel)
+  /* Get a new sensor event */
+  sensors_event_t event;
+#endif
+
+#if use_mag
+  // Magnetometro
   mag.getEvent(&event);
-  #if use_mag_x
+#if use_mag_x
   float mag_X;
   mag_X = event.magnetic.x;
-  #endif
-  #if use_mag_y
+#endif
+#if use_mag_y
   float mag_Y;
   mag_Y = event.magnetic.y;
-  #endif
-  #if use_mag_z
+#endif
+#if use_mag_z
   float mag_Z;
   mag_Z = event.magnetic.z;
-  #endif
+#endif
 
-  #endif  // use_mag
-  
-  #if use_accel
+#endif  // use_mag
+
+#if use_accel
   // Acelerometro
   accel.getEvent(&event);
-  #if use_accel_x
+#if use_accel_x
   float accel_X;
   accel_X = event.acceleration.x;
-  #endif
-  #if use_accel_y
+#endif
+#if use_accel_y
   float accel_Y;
   accel_Y = event.acceleration.y;
-  #endif
-  #if use_accel_z
+#endif
+#if use_accel_z
   float accel_Z;
   accel_Z = event.acceleration.z;
-  #endif
- 
-  #endif   // use_accel
+#endif
 
-  #if use_gyro
+#endif   // use_accel
+
+#if use_gyro
   // Giroscopio
   gyro.read();
-  #if use_gyro_x
+#if use_gyro_x
   float gyro_X;
   gyro_X = (int)gyro.g.x;
-  #endif
-  #if use_gyro_y
+#endif
+#if use_gyro_y
   float gyro_Y;
   gyro_Y = (int)gyro.g.y;
-  #endif
-  #if use_gyro_z
+#endif
+#if use_gyro_z
   float gyro_Z;
   gyro_Z = (int)gyro.g.z;
-  #endif
-  
-  #endif  // use_gyro
-  
+#endif
+
+#endif  // use_gyro
+
   //// FILTROS DE ALTURA
-  
-  #if use_alt
+
+#if use_alt
   // filtro 1
   float media_mov = filtro_altura(h, 1);
-//  float media_mov = 0;
-//  for (int k=0; k<(l-1); k++) {
-//    lista[k] = lista[k+1];
-//  }
-//  lista[l-1] = h;
-//  for (int j=0; j<l; j++) {
-//    media_mov = media_mov + lista[j];
-//  }
-//  media_mov = media_mov/l;
-  
+
   // filtro 2
   float media_mov2 = filtro_altura(media_mov, 2);
-//  float media_mov2 = 0;
-//  for (int k=0; k<(l-1); k++) {
-//    lista2[k] = lista2[k+1];
-//  }
-//  lista2[l-1] = media_mov;
-//  for (int j=0; j<l; j++) {
-//    media_mov2 = media_mov2 + lista2[j];
-//  }
-//  media_mov2 = media_mov2/l;
-  #endif
-  
+#endif
+
   //// REUNINDO OS DADOS EM UMA STRING
 
-  #if use_tempo
+#if use_tempo
   dataString += String(t_atual_segundos);
   dataString += "\t";
-  #endif
-  #if use_alt
+#endif
+#if use_alt
   // dataString += String(novaAlt);
   // dataString += "\t";
   dataString += String(h);
@@ -404,58 +392,58 @@ void loop() {
   dataString += "\t";
   dataString += String(media_mov2);  // media_mov2 é o filtro 2 de altura
   dataString += "\t";
-  #endif
-  #if use_temp
+#endif
+#if use_temp
   dataString += String(temp);
   dataString += "\t";
-  #endif
-  #if use_pressao
+#endif
+#if use_pressao
   dataString += String(pressao);
   dataString += "\t";
-     //dataString += String(pressaoNivelMar);
-     //dataString += "\t";
-  #endif
-  
-  #if use_mag_x  
+  //dataString += String(pressaoNivelMar);
+  //dataString += "\t";
+#endif
+
+#if use_mag_x
   dataString += String(mag_X);
   dataString += "\t";
-  #endif
-  #if use_mag_y
+#endif
+#if use_mag_y
   dataString += String(mag_Y);
   dataString += "\t";
-  #endif
-  #if use_mag_z
+#endif
+#if use_mag_z
   dataString += String(mag_Z);
   dataString += "\t";
-  #endif
-  #if use_accel_x
+#endif
+#if use_accel_x
   dataString += String(accel_X);
-  dataString += "\t"; 
-  #endif
-  #if use_accel_y
+  dataString += "\t";
+#endif
+#if use_accel_y
   dataString += String(accel_Y);
-  dataString += "\t";  
-  #endif
-  #if use_accel_z
+  dataString += "\t";
+#endif
+#if use_accel_z
   dataString += String(accel_Z);
   dataString += "\t";
-  #endif
-  #if use_gyro_x
+#endif
+#if use_gyro_x
   dataString += String(gyro_X);
-  dataString += "\t";  
-  #endif
-  #if use_gyro_y
+  dataString += "\t";
+#endif
+#if use_gyro_y
   dataString += String(gyro_Y);
-  dataString += "\t";  
-  #endif
-  #if use_gyro_z
+  dataString += "\t";
+#endif
+#if use_gyro_z
   dataString += String(gyro_Z);
   dataString += "\t";
-  #endif
+#endif
 
-  // Encontrando o apogeu 
+  // Encontrando o apogeu
 
-  #if use_apogeu
+#if use_apogeu
   if (media_mov2 < velhaAlt) {
     encontra_apogeu += 1;
   }
@@ -463,7 +451,7 @@ void loop() {
     encontra_apogeu = 0;
   }
 
-  if (encontra_apogeu <= 5) {  
+  if (encontra_apogeu <= 5) {
     dataString += "Apogeu foi detectado! Descendo.";
     dataString += "\t";
     if (apogeu_detectado == false) {
@@ -477,7 +465,7 @@ void loop() {
     }
   }
   // LED 2
-  if (t_atual >= liga_led2 && laco_led_2 == true){
+  if (t_atual >= liga_led2 && laco_led_2 == true) {
     ledState2 = HIGH;
     dataString += "Led 2!";
     dataString += "\t";
@@ -502,18 +490,18 @@ void loop() {
   if (t_atual >= desliga_led3) {
     ledState3 = LOW;
   }
-  
+
   digitalWrite(IGN_1, ledState1);
   digitalWrite(IGN_2, ledState2);
   digitalWrite(ledPin, ledState3);
-  #endif // use_apogeu
-  
-  // Mostrando dataString no Serial
-  #if print_serial
-  Serial.println(dataString);
-  #endif
+#endif // use_apogeu
 
-  #if use_sd
+  // Mostrando dataString no Serial
+#if print_serial
+  Serial.println(dataString);
+#endif
+
+#if use_sd
   // abrir o arquivo do SD e armazenar dados
   File dataFile = SD.open(nomeArquivo, FILE_WRITE);
 
@@ -522,10 +510,10 @@ void loop() {
     dataFile.println(dataString);
     dataFile.close();
   }
-  #endif
+#endif
 
-  #if use_alt
+#if use_alt
   // Reiniciando variável de velha altitude
   velhaAlt = media_mov2;
-  #endif
+#endif
 }
