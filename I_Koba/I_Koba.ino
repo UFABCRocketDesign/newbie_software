d5ac5c9a5587d1bd4dd983ec5fe089b41c873686
ikobayashidesu <igorkobayashi@hotmail.com>
2022-02-19 10:13:51 -0300

    eeeeei

--- I_Koba/I_Koba.ino
+++ I_Koba/I_Koba.ino
@@ -62,6 +62,7 @@ void loop() {
 
   sinal[0] = bmp.readAltitude() - const_chao;
   Serial.print(sinal[0]);
+   Serial.print("\t");
   Filtros();
   for (int y = 0; y < nf; y++) {
     Serial.print(sinal[y]);
