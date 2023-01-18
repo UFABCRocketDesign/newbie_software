735e0134ff26ae1c82ef8055d7686f2af6c79fef
kauakrumpanzer <kauakrumpanzer15@gmail.com>
2023-01-18 17:57:06 -0300

    Testando biblioteca bmp
    Test da biblioteca com BMP085 com pisca-pisca

--- 2023/K_Krumpanzer/K_Krumpanzer.ino
+++ 2023/K_Krumpanzer/K_Krumpanzer.ino
@@ -1,10 +1,44 @@
-void setup() {
-    pinMode(13, OUTPUT);
+#include <Adafruit_BMP085.h>
+Adafruit_BMP085 bmp;
+void setup (){
+  Serial.begin(115200);
+  pinMode(13,OUTPUT);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
+}
+void loop (){
+  digitalWrite(13,HIGH);
+  delay(2000);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(2000);
+  delay(1000);
 }
 
-void loop() {
-    digitalWrite(13, HIGH);
-    delay(3500);
-    digitalWrite(13, LOW);
-    delay(3500);
+void loop (){
+  Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+    Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+    Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.println(" meters");
+
+    Serial.println();
+    delay(1500);
 }
