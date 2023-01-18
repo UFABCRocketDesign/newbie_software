1f130c2265e052d0387766ae2dcd1b0dfb508330
kauakrumpanzer <kauakrumpanzer15@gmail.com>
2023-01-18 18:26:59 -0300

    Mudando os tempos e os blocos
    Atualizando os tempos e mudando de blocos para colunas os dados

--- 2023/K_Krumpanzer/K_Krumpanzer.ino
+++ 2023/K_Krumpanzer/K_Krumpanzer.ino
@@ -7,36 +7,24 @@ void setup (){
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
   }
+  Serial.print("Temperature  =     ");
+  Serial.print("Pressure  =     ");
+  Serial.println("Altitude  =     ");
 }
 void loop (){
   digitalWrite(13,HIGH);
   delay(2000);
-  delay(1000);
   digitalWrite(13,LOW);
   delay(2000);
-  delay(1000);
 
-  Serial.print("Temperature = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
 
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
+  Serial.print(bmp.readTemperature());
+  Serial.println("     ");
 
-    // Calculate altitude assuming 'standard' barometric
-    // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print("Altitude = ");
-    Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println("     ");
 
-    Serial.print("Pressure at sealevel (calculated) = ");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
-    Serial.print("Real altitude = ");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.println(" meters");
+  Serial.print(bmp.readAltitude());
 
-    Serial.println();
-    delay(1500);
+  Serial.println();
 }
