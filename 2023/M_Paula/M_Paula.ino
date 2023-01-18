8c2713c1de8e54a6099b6d0d829fb3bdf6bee03b
Mariprs99 <122938362+Mariprs99@users.noreply.github.com>
2023-01-18 17:37:26 -0300

    Teste 1 - barômetro e pisca LED 13
    Primeiro teste picar led 13 com biblioteca BMP085

--- 2023/M_Paula/M_Paula.ino
+++ 2023/M_Paula/M_Paula.ino
@@ -1,10 +1,39 @@
+#include <Adafruit_BMP085.h>
+Adafruit_BMP085 bmp;
 void setup (){
   Serial.begin(9600);
   pinMode(13,OUTPUT);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
 }
 void loop (){
   digitalWrite(13,HIGH);
-  delay(5000);
+  delay(3000);
   digitalWrite(13,LOW);
-  delay(5000);
+  delay(2000);
+  Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+    
+    Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+    
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+    Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+  Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.println(" meters");
+    
+    Serial.println();
+    delay(500);
 }
