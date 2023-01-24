61ebc8e2dabaced1ec40e789ca3aa6e94df86a50
Giovana <torres.a@aluno.ufabc.edu.br>
2023-01-24 19:19:23 -0300

    Update G_Torres.ino

--- 2023/G_Torres/G_Torres.ino
+++ 2023/G_Torres/G_Torres.ino
@@ -1,8 +1,16 @@
+#include <Adafruit_BMP085.h>
+Adafruit_BMP085 bmp;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
+
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
 
 // the loop function runs over and over again forever
 void loop() {
@@ -11,42 +19,6 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(1700);                      // wait for a second
 }
-
-#include <Adafruit_BMP085.h>
-
-/*************************************************** 
-  This is an example for the BMP085 Barometric Pressure & Temp Sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout 
-  ----> https://www.adafruit.com/products/391
-
-  These pressure and temperature sensors use I2C to communicate, 2 pins
-  are required to interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
-// Connect GND to Ground
-// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
-// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
-// EOC is not used, it signifies an end of conversion
-// XCLR is a reset pin, also not used here
-
-Adafruit_BMP085 bmp;
-  
-void setup() {
-  Serial.begin(9600);
-  if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
-  }
-}
-  
 void loop() {
     Serial.print("Temperature = ");
     Serial.print(bmp.readTemperature());
