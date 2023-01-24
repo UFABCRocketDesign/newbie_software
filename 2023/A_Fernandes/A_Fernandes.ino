51c4a6e301ed5cdbd6cb7e92d3795108740502d0
Amanda <amanda.fernandes225@gmail.com>
2023-01-24 20:31:12 -0300

    Juntar blink com adafruit

--- 2023/A_Fernandes/A_Fernandes.ino
+++ 2023/A_Fernandes/A_Fernandes.ino
@@ -90,5 +90,6 @@ void loop() {
     digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(7000);                      // wait for a second
     digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW 
+    
     delay(500);
 }
