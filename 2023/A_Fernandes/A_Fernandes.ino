844e967ff80388c565f4267b9e7db933e49abadb
Amanda <amanda.fernandes225@gmail.com>
2023-01-24 21:13:39 -0300

    colunas_certo

--- 2023/A_Fernandes/A_Fernandes.ino
+++ 2023/A_Fernandes/A_Fernandes.ino
@@ -92,7 +92,7 @@ void loop() {
     Serial.print("			");
     //Serial.println(" meters");
     
-    //Serial.println();
+    Serial.println();
     digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(7000);                      // wait for a second
     digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW 
