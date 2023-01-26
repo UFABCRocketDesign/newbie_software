be48bb26507fee92c2262143309284daf08bdf6f
Giovana <torres.a@aluno.ufabc.edu.br>
2023-01-26 17:46:03 -0300

    Update G_Torres.ino

--- 2023/G_Torres/G_Torres.ino
+++ 2023/G_Torres/G_Torres.ino
@@ -6,13 +6,13 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
 }
 
-    Serial.println(" Temperature(*C) | Pressure(Pa) | Altitude(meters)");
-    Serial.println();
+Serial.println(" Temperature(*C) | Pressure(Pa) | Altitude(meters)");
+Serial.println();
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
@@ -20,17 +20,17 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(1700);                      // wait for a second
 
-    Serial.print(bmp.readTemperature());
-    Serial.print("  ");
-    Serial.print(bmp.readPressure());
-    Serial.print("  ");
-    Serial.print(bmp.readAltitude());
-    Serial.print("  ");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.print("  ");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.print("  ");
-    
-    Serial.println();
-    delay(500);
+  Serial.print(bmp.readTemperature());
+  Serial.print("  ");
+  Serial.print(bmp.readPressure());
+  Serial.print("  ");
+  Serial.print(bmp.readAltitude());
+  Serial.print("  ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.print("  ");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.print("  ");
+
+  Serial.println();
+  delay(500);
 }
