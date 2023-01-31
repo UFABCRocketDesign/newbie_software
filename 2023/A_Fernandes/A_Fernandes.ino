0afc49cd425c29d7a44be552786a980ab2c47d57
Amanda Fernandes de Lima <86573574+amandaflima@users.noreply.github.com>
2023-01-31 19:01:55 -0300

    zerando altura

--- 2023/A_Fernandes/A_Fernandes.ino
+++ 2023/A_Fernandes/A_Fernandes.ino
@@ -1,37 +1,74 @@
-/*
-  Blink
+#include <Adafruit_BMP085.h>
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+/*************************************************** 
+  This is an example for the BMP085 Barometric Pressure & Temp Sensor
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> https://www.adafruit.com/products/391
+  These pressure and temperature sensors use I2C to communicate, 2 pins
+  are required to interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
+// Connect GND to Ground
+// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
+// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
+// EOC is not used, it signifies an end of conversion
+// XCLR is a reset pin, also not used here
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
+Adafruit_BMP085 bmp;
+float altitude;
 
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(115200);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
   pinMode(13, OUTPUT);
+  Serial.print("Temperature (*C)     Pressure (Pa)      Altitude (m)     Altura(m)     Pressure at sealevel (calculated) (Pa)     Real altitude (m)");
 }
-
-// the loop function runs over and over again forever
+  
 void loop() {
-  digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(7000);                      // wait for a second
-  digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
-  delay(2000);                      // wait for a second
+    
+    Serial.print(bmp.readTemperature());
+    Serial.print("			");
+
+    Serial.print(bmp.readPressure());
+    Serial.print("			");
+
+    
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+    altitude = bmp.readAltitude();
+    Serial.print(altitude);
+    Serial.print("			");
+
+    Serial.print(altitude - int(altitude));
+    Serial.print("			");
+
+    //Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.print("			");
+    //Serial.println(" Pa");
+
+  // you can get a more precise measurement of altitude
+  // if you know the current sea level pressure which will
+  // vary with weather and such. If it is 1015 millibars
+  // that is equal to 101500 Pascals.
+    //Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.print("			");
+    //Serial.println(" meters");
+    
+    Serial.println();
+    digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(7000);                      // wait for a second
+    digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW 
+    
+    delay(500);
 }
