b05f65bb868ffe318db70771fc8ff760b54cdb82
laiscarriello <99299015+Lais0104@users.noreply.github.com>
2023-01-31 20:14:17 -0300

    bmp altura inicial
    código pra zerar a altura

--- 2023/L_Carriello/L_Carriello.ino
+++ 2023/L_Carriello/L_Carriello.ino
@@ -2,36 +2,40 @@
 
 
 Adafruit_BMP085 bmp;
-  
+
+float alt_inicial;
+float soma;
+int i;
+
 void setup() {
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
   Serial.begin(115200);
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
-
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
   Serial.print("Temperature(C) ");
   Serial.print("Altitude(M) ");
   Serial.print("Pressure at sealevel(Pa) ");
   Serial.println();
+
+  for (i = 0; i < 10; i++) {
+    soma += bmp.readAltitude();
+  }
+
+  alt_inicial = soma / 10;
 }
-  
+
+
 void loop() {
-    
-    Serial.print(bmp.readTemperature());
-    Serial.print("         ");    
-    Serial.print(bmp.readAltitude());
-    Serial.print("         ");  
-    Serial.print(bmp.readSealevelPressure());
-   
-   
-    Serial.println();
-    delay(500);
 
-    digitalWrite(13, HIGH);
-    delay(3000); 
-    digitalWrite(13, LOW);
+  Serial.print(bmp.readTemperature());
+  Serial.print("       ");
+  Serial.print(bmp.readAltitude() - alt_inicial);
+  Serial.print("       ");
+  Serial.print(bmp.readSealevelPressure());
 
-  
+
+  Serial.println();
+  delay(10);
 }
