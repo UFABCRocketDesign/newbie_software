68040e170ae1115c51de941eff8e534c8cbd2dab
marcos-vpc <107070597+marcos-vpc@users.noreply.github.com>
2023-01-31 21:05:04 -0300

    Altitude 0
    teste

--- 2023/M_Vinicius/M_Vinicius.ino
+++ 2023/M_Vinicius/M_Vinicius.ino
@@ -7,40 +7,45 @@ void setup() {
   pinMode (LED_BUILTIN, OUTPUT);
   
   Serial.begin (115200);
+
+  Serial.print ("Temperature (*C) \t");
+  Serial.print ("Pressure (Pa) \t");
+  Serial.print("Altitude (meters) \t");
+  Serial.println("Real altitude (meters)");
+  
+  float alt_in = 0;
+
+  for (i = 0, i<6, i++) {
+    alt_in = alt_in + bmp.readAltitude();
+  }
+
+  alt_in = alt_in/6;
+
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
+
+    
   }
 }
 
 void loop() {
 
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(3000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(3000);
-
-
-  Serial.print ("Temperature (*C) =   ");
-  Serial.print ("Pressure (Pa) =   ");
-  Serial.print("Altitude (meters) =  ");
-  Serial.println("Real altitude (meters) =   ");
-
   Serial.print(bmp.readTemperature());
-  Serial.print("    ");
+  Serial.print("\t");
 
 //
   Serial.print(bmp.readPressure());
-  Serial.print("    ");
+  Serial.print("\t");
 
-  Serial.print(bmp.readAltitude());
-  Serial.print("    ");
+  Serial.print(bmp.readAltitude()-alt_in);
+  Serial.print("\t");
 
   Serial.print(bmp.readAltitude(101500));
-  Serial.print("    ");
+  Serial.print("\t");
 
   Serial.println();
-    delay(500); 
+    delay(100); 
 
 
 }
