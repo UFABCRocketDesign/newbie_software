e7f8efc0a11f77406ae2cea4bb5528b684e72780
Mariprs99 <122938362+Mariprs99@users.noreply.github.com>
2023-02-07 20:05:48 -0300

    teste 2- organizando o código - BMP085

--- 2023/M_Paula/M_Paula.ino
+++ 2023/M_Paula/M_Paula.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
-#define n_media  5
-#define num  10
+#define n_media 5
+#define num 10
 float alt_inicial;
 float values_1[num];
 float values_2[num];
@@ -9,47 +9,51 @@ float values_2[num];
 
 
 //filtragem da altura com ruido
-float media_movel_1(float sinal_com_ruido){
+float media_movel_1(float sinal_com_ruido) {
   float acc = 0;
-  for(int i = num-1; i > 0; i--){
-    values_1[i] = values_1[i-1];
+  for (int i = num - 1; i > 0; i--) {
+    values_1[i] = values_1[i - 1];
   }
   values_1[0] = sinal_com_ruido;
-  for(int i = 0; i < num; i++){
-  acc += values_1[i];
+  for (int i = 0; i < num; i++) {
+    acc += values_1[i];
   }
-  return acc/num;
+  return acc / num;
 }
 
 
 //filtragem de altura sem ruido
-float media_movel_2(float sinal_sem_ruido){
+float media_movel_2(float sinal_sem_ruido) {
   float acc = 0;
-  for(int i = num-1; i > 0; i--){
-    values_2[i] = values_2[i-1];
+  for (int i = num - 1; i > 0; i--) {
+    values_2[i] = values_2[i - 1];
   }
   values_2[0] = sinal_sem_ruido;
-  for(int i = 0; i < num; i++){
-  acc += values_2[i];
+  for (int i = 0; i < num; i++) {
+    acc += values_2[i];
   }
-  return acc/num;
+  return acc / num;
 }
 
 
 
-void setup (){
+void setup() {
   float soma = 0;
   Serial.begin(115200);
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
+  
+  //Inicializando o sensor de barômetro
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
-  //cálculo da altura inicial
-  for(int i = 0; i < n_media; ++i){
+  
+  //cálculo da altitude inicial
+  for (int i = 0; i < n_media; ++i) {
     soma = soma + bmp.readAltitude();
-  } 
-   alt_inicial = soma /5;
+  }
+  alt_inicial = soma / 5;
+  
   //print dos rótulos das medicoes
   Serial.print("Temperatura (*C) ");
   Serial.print("Altura com ruido (meters) ");
@@ -59,19 +63,22 @@ void setup (){
 
 
 
-void loop (){
-  //medicoes           
-    Serial.print(bmp.readTemperature());
-    Serial.print("\t");
-    float altura_com_ruido = bmp.readAltitude() - alt_inicial;
-    Serial.print(altura_com_ruido);
-    Serial.print("\t");
-    float altura_sem_ruido_1= media_movel_1(altura_com_ruido);
-    Serial.print(altura_sem_ruido_1);
-    Serial.print("\t");
-    float altura_sem_ruido_2= media_movel_2(altura_sem_ruido_1);
-    Serial.print(altura_sem_ruido_2);
-    Serial.print("\t");
-    Serial.print(bmp.readPressure());
-    Serial.println();
+void loop() {
+  //leitura dos dados
+  float altura_com_ruido = bmp.readAltitude() - alt_inicial;
+  float altura_sem_ruido_1 = media_movel_1(altura_com_ruido);
+  float altura_sem_ruido_2 = media_movel_2(altura_sem_ruido_1);
+  float temperatura = bmp.readTemperature();
+  
+  //impressão dos dados
+  Serial.print(temperatura);
+  Serial.print("\t");
+  Serial.print(altura_com_ruido);
+  Serial.print("\t");
+  Serial.print(altura_sem_ruido_1);
+  Serial.print("\t");
+  Serial.print(altura_sem_ruido_2);
+  Serial.print("\t");
+  Serial.print(bmp.readPressure());
+  Serial.println();
 }
