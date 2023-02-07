845cd5d436b92d4db99226c540db1f9efd7ef9b1
Giovana <torres.a@aluno.ufabc.edu.br>
2023-02-07 18:30:13 -0300

    Update G_Torres.ino

--- 2023/G_Torres/G_Torres.ino
+++ 2023/G_Torres/G_Torres.ino
@@ -1,6 +1,8 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
 float alt_inicial;
+float alt; 
+int i;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -8,21 +10,25 @@ void setup() {
   if (!bmp.begin()) {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
-  alt_inicial = bmp.readAltitude();
-}
-
+  }
+  
   Serial.println(" Temperature(*C) | Pressure(Pa) | Altitude(meters)");
   Serial.println();
-  }
+
+   for(i=0; i<5; i++){
+    alt += bmp.readAltitude();
+  } 
+ alt_inicial = alt/5;
+}
 
 void loop() {
 
     Serial.print(bmp.readTemperature());
-    Serial.print("  ");
+    Serial.print("\t");
     Serial.print(bmp.readPressure());
-    Serial.print("  ");
+    Serial.print("\t");
     Serial.print(bmp.readAltitude() - alt_inicial);
-    Serial.print("  ");
+    Serial.print("\t");
 
 
   Serial.println();
