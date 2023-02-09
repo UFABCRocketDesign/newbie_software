2537ad6d8334b2207a2a80ba213754bb8148adb4
kauakrumpanzer <kauakrumpanzer15@gmail.com>
2023-02-09 15:05:18 -0300

    Tentativa do 2 filtro

--- 2023/K_Krumpanzer/K_Krumpanzer.ino
+++ 2023/K_Krumpanzer/K_Krumpanzer.ino
@@ -1,12 +1,13 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
 
-#define n 30
+#define n 10
 
 float alt_inicial; 
 float soma; 
-int i;
+int i, j;
 float num[n];
+float num2[n];
 
 float filtro(float mediamovel){
   for(i = n-1; i>0; i--) num[i] = num[i-1];
@@ -15,6 +16,15 @@ float filtro(float mediamovel){
   for(i=0; i<n; i++) acc += num[i];
 
   return acc/n;
+}
+
+float filtro2(float mediamovel2){
+  for(i = n-1; i>0; i--) num2[i] = num2[i-1];
+  num2[0] = mediamovel2;
+  float acc2 = 0;
+  for(i=0; i<n; i++) acc2 += num2[i];
+
+  return acc2/n;
 }
 
 void setup (){
@@ -30,6 +40,7 @@ void setup (){
   Serial.print("Pressão (Pa) = \t");
   Serial.print("Altitude (m) = \t");
   Serial.println("Altitude com filtro (m) =");
+  Serial.println("Altitude com filtro 2 (m) =");
 
   for(i=0; i<5; i++){
     soma += bmp.readAltitude();
@@ -49,6 +60,9 @@ void loop (){
   Serial.print("\t");
   float altura_filtrada = filtro(altura);
   Serial.print(altura_filtrada);
+  Serial.print("\t");
+  float altura_filtrada2 = filtro2(altura_filtrada);
+  Serial.print(altura_filtrada2);
 
   Serial.println();
   delay(10);
