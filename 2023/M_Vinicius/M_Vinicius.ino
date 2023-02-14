4e7c8e6a25e3d8667cdcfbcbe18d0d2c3682b805
marcos-vpc <107070597+marcos-vpc@users.noreply.github.com>
2023-02-14 17:18:56 -0300

    filtro altitude
    teste

--- 2023/M_Vinicius/M_Vinicius.ino
+++ 2023/M_Vinicius/M_Vinicius.ino
@@ -1,8 +1,10 @@
 // BPM085 SENSOR ( PRESSÃO / TEMPERATURA / ALTITUDE) 
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
-  int i;
+  int i, w;
   float alt_in = 0;
+  float atura_atual = 0;
+  float vetor[10]
   
   void setup() {
 
@@ -15,21 +17,21 @@ Adafruit_BMP085 bmp;
   Serial.print("Altitude (meters) \t");
   Serial.println("Real altitude (meters)");
 
+  
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+   
   for (i = 0; i<5; i++) {              
       alt_in = alt_in + bmp.readAltitude();
     }
 
     alt_in = alt_in/5;
-   
-  if (!bmp.begin()) {
-    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-    while (1) {}
-   
   }
 }
 
 void loop() {
-      
+
 
   Serial.print(bmp.readTemperature());            
   Serial.print("\t");
@@ -43,6 +45,20 @@ void loop() {
 
   Serial.print(bmp.readAltitude(101500));
   Serial.print("\t");
+    
+    /*for (i = 0; i<10; i++)
+    {
+      vetor[i] = bmp.readAltitude()-alt_in
+      for (w=0; w<9; w++)
+      {
+        if (vetor[w+1]>vetor[w])
+        {
+          Serial.print("0");
+        }        
+      }
+    }*/
+
+    
 
   Serial.println();
     delay(100); 
