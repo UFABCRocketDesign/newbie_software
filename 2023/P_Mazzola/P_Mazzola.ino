a3d7f1ca068f6365b9911bce08fcf21dc5c47a69
pietro <pimazzo19@gmail.com>
2023-02-14 17:54:20 -0300

    exercicio led

--- 2023/P_Mazzola/P_Mazzola.ino
+++ 2023/P_Mazzola/P_Mazzola.ino
@@ -9,44 +9,29 @@ void setup() {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
   }
-  Serial.println('temperatura'); Serial.println( 'pressao'); Serial.println('altitude'); Serial.println('pressao no nvl do mar'); Serial.println ('real altitude');
+  Serial.println("temperatura"); Serial.println( "pressao"); Serial.println("altitude"); Serial.println("pressao no nvl do mar"); Serial.println ("real altitude");
 }
   
 void loop() {
     Serial.print(bmp.readTemperature());
-    digitalWrite(LED_BUILTIN, HIGH); 
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);  
-    delay(1000); 
-    
+    Serial.println("  ");
 
     Serial.print(bmp.readPressure());
-    digitalWrite(LED_BUILTIN, HIGH); 
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);  
-    delay(1000); 
+    Serial.println("  ");
     
 
     Serial.print(bmp.readAltitude());
-    digitalWrite(LED_BUILTIN, HIGH); 
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);  
-    delay(1000); 
+    Serial.println("  ");
 
 
     Serial.print(bmp.readSealevelPressure());
-    digitalWrite(LED_BUILTIN, HIGH); 
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);  
-    delay(1000); 
+    Serial.println("  ");
 
 
 
     Serial.print(bmp.readAltitude(101500));
-    digitalWrite(LED_BUILTIN, HIGH); 
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);  
-    delay(1000); 
+    Serial.println("  ");
+
     
     Serial.println();
     delay(500);
