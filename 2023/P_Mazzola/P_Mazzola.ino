ca64fb9aecb20869e3a6672e50f0dc4929c14bc0
pietro <pimazzo19@gmail.com>
2023-02-28 17:44:44 -0300

    update altura

--- 2023/P_Mazzola/P_Mazzola.ino
+++ 2023/P_Mazzola/P_Mazzola.ino
@@ -2,35 +2,43 @@
 
 
 Adafruit_BMP085 bmp;
-float p0=0;
-int passo =20;
+float p0 = 0;
+int passo = 20;
 int i;
+int contador = 0;
 void setup() {
   Serial.begin(115200);
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
-  Serial.print("temperatura "); Serial.print( "pressao  "); Serial.print("altitude  "); Serial.print("pressao no nvl do mar "); Serial.print ("real altitude  ");
- // definindo condiçoes iniciais 
-  for (i= 1; i<= passo; i++)
-  {
-    p0= bmp.readAltitude()+p0;
+  Serial.print("temperatura ");
+  Serial.print("pressao  ");
+  Serial.print("altitude  ");
+  Serial.print("pressao no nvl do mar ");
+  Serial.print("real altitude  ");
+  // definindo condiçoes iniciais
+  for (i = 1; i <= passo; i++) {
+    p0 = bmp.readAltitude() + p0;
   }
-  p0=p0/passo;
+  p0 = p0 / passo;
 }
-  
+
 void loop() {
-    Serial.print(bmp.readTemperature());
-    Serial.print("  ");
+  Serial.print(bmp.readTemperature());
+  Serial.print("  ");
 
-    Serial.print(bmp.readPressure());
-    Serial.print("  ");
-    
+  Serial.print(bmp.readPressure());
+  Serial.print("  ");
 
-    Serial.print(bmp.readAltitude()-p0);
-    Serial.print("  ");
-    
-    Serial.println();
-    delay(5);
+  float v[10]={};
+  bmp.readAltitude() - p0;
+  contador = contador + 1;
+  float altitude = v[10] / contador;
+  Serial.print(altitude);
+  Serial.print("  ");
+
+  Serial.println();
+
+  delay(5);
 }
