5aa1c499413df830e2952a50b79a2b2840817b27
marcos-vpc <107070597+marcos-vpc@users.noreply.github.com>
2023-02-28 18:03:43 -0300

    altura refinada
    teste

--- 2023/M_Vinicius/M_Vinicius.ino
+++ 2023/M_Vinicius/M_Vinicius.ino
@@ -4,10 +4,14 @@ Adafruit_BMP085 bmp;
 int i, w;
 float altura, alt_in = 0;
 float altura_semRuido = 0;
+float altura_sRuido2 = 0;
 //float vetor[10];
 float filtro[10];
+float filtro2[10];
 int index = 0;
+int indi = 0;
 float total = 0;
+float acum = 0;
 
 
 void setup() {
@@ -20,6 +24,8 @@ void setup() {
   Serial.print("Pressure (Pa) \t");
   Serial.print("Altitude (meters) \t");
   Serial.print("Altitude sem ruido (meters) \t");
+  Serial.print("Altitude s. ruido 2 (meters) \t");
+
 
 
   if (!bmp.begin()) {
@@ -47,6 +53,16 @@ void loop() {
   }
   altura_semRuido = total / 10;
 
+  filtro2[indi] =  altura_semRuido;
+  indi = (indi + 1) % 10;
+  acum = 0;
+  for (int i = 0; i< 10; i++)
+  {
+    acum += filtro2[i];
+  }
+
+  altura_sRuido2 = acum/10;
+
 
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
@@ -61,6 +77,9 @@ void loop() {
 
 
   Serial.print(altura_semRuido);
+  Serial.print("\t");
+
+  Serial.print(altura_sRuido2);
   Serial.print("\t");
 
 
