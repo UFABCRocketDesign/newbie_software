be1f243afb04312f546462d3e9903e33879f5609
Giovana <torres.a@aluno.ufabc.edu.br>
2023-03-03 15:47:24 -0300

    update

--- 2023/G_Torres/G_Torres.ino
+++ 2023/G_Torres/G_Torres.ino
@@ -1,8 +1,14 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
 float alt_inicial;
-float alt; 
+float alt;
 int i;
+
+ #define N 5
+
+
+  float V[N] = {};
+  int indx = 0;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -11,25 +17,36 @@ void setup() {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   }
-  
+
   Serial.println(" Temperature(*C) | Pressure(Pa) | Altitude(meters)");
   Serial.println();
-
-   for(i=0; i<5; i++){
-    alt += bmp.readAltitude();
-  } 
- alt_inicial = alt/5;
+   
+  for(i=0; i<5; i++){
+  alt += bmp.readAltitude();
+  }
+  alt_inicial = alt/5;
 }
-
 void loop() {
+
+float L = bmp.readAltitude() - alt_inicial;
 
     Serial.print(bmp.readTemperature());
     Serial.print("\t");
     Serial.print(bmp.readPressure());
     Serial.print("\t");
-    Serial.print(bmp.readAltitude() - alt_inicial);
+    Serial.print("L");
     Serial.print("\t");
 
+    for(int i = N-1; i>0; i--){
+    V[i] = V[i-1];
+    }
+    V[0] = L;
+    float sum = 0.0;
+    for (int i = 0; i<N; i++){
+      sum += V[i];
+
+  }
+  float media = sum / N;
 
   Serial.println();
   delay(5);
