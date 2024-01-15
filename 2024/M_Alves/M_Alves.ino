f9b641ef7b08285604e0a2b797c2b48c7e75a671
Matheus Alves <aalves.maatheus@gmail.com>
2024-01-15 20:56:26 -0300

    Update M_Alves4
    Update separando os valores com '\t'

--- 2024/M_Alves/M_Alves.ino
+++ 2024/M_Alves/M_Alves.ino
@@ -1,12 +1,8 @@
 #include <Adafruit_BMP085.h>
-#define LED 13
 
 Adafruit_BMP085 bmp;
 
 void setup() {
-  //LED
-  pinMode(LED, OUTPUT);
-
   //BME085
   Serial.begin(9600);
   if (!bmp.begin()) {
@@ -14,30 +10,24 @@ void setup() {
 	while (1) {}
   }
 
-  Serial.print("Temperature (*C); Pressure (Pa); Altitude (m); Pressure at sea level (Pa); Real Altitude (m);");
+  //Cabeçalho
+  Serial.println("Temperature (*C); Pressure (Pa); Altitude (m); Pressure at sea level (Pa); Real Altitude (m);");
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
-  digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(50);                      // wait for a second
-
-  //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%//
-
   //BME085
   Serial.print(bmp.readTemperature());
-  Serial.print("; ");
+  Serial.print('\t');
   Serial.print(bmp.readPressure());
-  Serial.print("; ");
+  Serial.print('\t');
   Serial.print(bmp.readAltitude());
-  Serial.print("; ");
+  Serial.print('\t');
   Serial.print(bmp.readSealevelPressure());
-  Serial.print("; ");
+  Serial.print('\t');
   Serial.print(bmp.readAltitude(101500));
-  Serial.print("; ");
-  Serial.println();
+  Serial.println('\t');
 
-  digitalWrite(LED, LOW);   // turn the LED off by making the voltage LOW
-  delay(3000);
+  delay(50);
 }
