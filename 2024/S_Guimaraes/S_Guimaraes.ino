24febbb5a0f4081e3e691d4d9751b1f9758b63a6
Sarah-G-S-777 <sarahguimaraesdasilva@gmail.com>
2024-01-15 21:09:02 -0300

    WS-Sarahtest2
    Código.

--- 2024/S_Guimaraes/S_Guimaraes.ino
+++ 2024/S_Guimaraes/S_Guimaraes.ino
@@ -1,27 +1,35 @@
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+ #include <Adafruit_BMP085.h>
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(3000);
+Adafruit_BMP085 bmp;
+
+void setup() {
+  // initialize digital 
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  if (!bmp.begin()) 
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) 
+	while (1) {}
+   }
+void loop() {
+digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(3000);
     Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+
     Serial.println(" *C");
     
     Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
     Serial.println(" Pa");
     
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
     Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
     Serial.println(" meters");
 
     Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
     Serial.println(" Pa");
 
   // you can get a more precise measurement of altitude
@@ -29,8 +37,8 @@ void loop() {
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
     Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
     Serial.println(" meters");
-    
     Serial.println();
     delay(500);
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
