e549e0b9f9c91348be82b6bf46c86b9e0a48b73c
Cauã de Souza Oliveira Querino <caua.oliversouza@gmail.com>
2024-01-15 21:17:09 -0300

    commit tabelinha 3 agr vai

--- 2024/C_Querino/C_Querino.ino
+++ 2024/C_Querino/C_Querino.ino
@@ -9,43 +9,28 @@ void setup() {
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
+  
   }
+  Serial.print("Temperatura\tpressão\tAltitude\tpressão em relação ao mar\taltitude real");
 }
 
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);
   delay(501); 
-  //led acima, bpm085 abaixo deste comment.
-  Serial.print("Temperature = ");
+  
     Serial.print(bmp.readTemperature());
-    Serial.print(" *C  ");
-    
-    Serial.print(" Pressure = ");
+    Serial.print("\t");
     Serial.print(bmp.readPressure());
-    Serial.print(" Pa  ");
-    
-    // Calculate altitude assuming 'standard' barometric
-    // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print(" Altitude = ");
+    Serial.print("\t");
     Serial.print(bmp.readAltitude());
-    Serial.print(" meters  ");
-
-    Serial.print(" Pressure at sealevel (calculated) = ");
+    Serial.print("\t");
     Serial.print(bmp.readSealevelPressure());
-    Serial.print(" Pa  ");
-
-  // you can get a more precise measurement of altitude
-  // if you know the current sea level pressure which will
-  // vary with weather and such. If it is 1015 millibars
-  // that is equal to 101500 Pascals.
-    Serial.print(" Real altitude = ");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.print(" meters  ");
+    Serial.print("\t");
+    Serial.print(bmp.readAltitude(101500));//altitude real
     
     Serial.println();
-    delay(500);                     // wait for a second
+    delay(500);                     
 }
