4d3bf5bad4634bb77123367aae0a0fce7a83b8be
Joao Henrique <jjoaohenrique@icloud.com>
2024-01-18 17:29:00 -0300

    Bmp3
    Teste Bmp3

--- 2024/J_Cardozo/J_Cardozo.ino
+++ 2024/J_Cardozo/J_Cardozo.ino
@@ -5,29 +5,29 @@ Adafruit_BMP085 bmp;
 void setup() {
   Serial.begin(9600);
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
-  Serial.print("Temperature(*C) |");
-  Serial.print("Pressure(Pa) |");
-  Serial.print("Altitude(m) |");
-  Serial.print("Pressure at sealevel (calculated)(Pa) |");
-  Serial.print("Real altitude(m)|");
+  Serial.print("Temperature(*C)\t");
+  Serial.print("Pressure(Pa)\t");
+  Serial.print("Altitude(m)\t");
+  Serial.print("Pressure at sealevel (calculated)(Pa)\t");
+  Serial.print("Real altitude(m)\t");
 
   Serial.println("");
 }
 
 void loop() {
   Serial.print(bmp.readTemperature());
-  Serial.print(" |");
+  Serial.print("\t");
   Serial.print(bmp.readPressure());
-  Serial.print(" |");
+  Serial.print("\t");
   Serial.print(bmp.readAltitude());
-  Serial.print(" |");
+  Serial.print("\t");
   Serial.print(bmp.readSealevelPressure());
-  Serial.print(" |");
+  Serial.print("\t");
   Serial.print(bmp.readAltitude(101500));
-  Serial.print(" |");
+  Serial.print("\t");
 
   Serial.println();
   delay(500);      
