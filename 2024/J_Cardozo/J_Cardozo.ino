53664e5ec2549453d38b076f48663925ccb425c4
Joao Henrique <jjoaohenrique@icloud.com>
2024-01-18 17:44:06 -0300

    Bmp4
    Teste Bmp4

--- 2024/J_Cardozo/J_Cardozo.ino
+++ 2024/J_Cardozo/J_Cardozo.ino
@@ -2,17 +2,19 @@
 
 Adafruit_BMP085 bmp;
 
+float pressaoInicial;
+
 void setup() {
   Serial.begin(9600);
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
   }
+  pressaoInicial = bmp.readPressure();
   Serial.print("Temperature(*C)\t");
   Serial.print("Pressure(Pa)\t");
   Serial.print("Altitude(m)\t");
-  Serial.print("Pressure at sealevel (calculated)(Pa)\t");
-  Serial.print("Real altitude(m)\t");
+  
 
   Serial.println("");
 }
@@ -22,13 +24,8 @@ void loop() {
   Serial.print("\t");
   Serial.print(bmp.readPressure());
   Serial.print("\t");
-  Serial.print(bmp.readAltitude());
+  Serial.print(bmp.readAltitude(pressaoInicial)); //Calcula a altura utilizando a pressao calculada quando o setup inicia
   Serial.print("\t");
-  Serial.print(bmp.readSealevelPressure());
-  Serial.print("\t");
-  Serial.print(bmp.readAltitude(101500));
-  Serial.print("\t");
-
-  Serial.println();
-  delay(500);      
+  
+  Serial.println();    
 }
