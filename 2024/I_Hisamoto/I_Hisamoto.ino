f7a8da354fb5acdb4dafb88497faa40245ec6ea5
isahisamoto <isabella.chisamoto@gmail.com>
2024-01-18 17:53:54 -0300

    algo?

--- 2024/I_Hisamoto/I_Hisamoto.ino
+++ 2024/I_Hisamoto/I_Hisamoto.ino
@@ -2,40 +2,24 @@
 Adafruit_BMP085 bmp;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
   }
+  Serial.print("Temperature(C)   Pressure(Pa)   Altitude(meters)   Pressure at sealevel (calculated, Pa)   Real altitude(meters)");
 }
 
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(3000);                      
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(3000);  
-   Serial.print("Temperature = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-    
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-    
-    Serial.print("Altitude = ");
-    Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
 
-    Serial.print("Pressure at sealevel (calculated) = ");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
-
-    Serial.print("Real altitude = ");
+    
+    Serial.print(bmp.readTemperature(),"   |");
+    Serial.print(bmp.readPressure(),"   |");
+    Serial.print(bmp.readAltitude(),"   |");
+    Serial.print(bmp.readSealevelPressure(),"   |   ");
     Serial.print(bmp.readAltitude(101500));
-    Serial.println(" meters");
-    
+    Serial.print("/n");
+
     Serial.println();
-    delay(500);
 }
