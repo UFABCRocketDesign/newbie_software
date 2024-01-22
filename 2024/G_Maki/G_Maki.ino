2c1cb51b80602f245f340bce96ba3b6b11877e8b
Gabriel Maki <107128221+GabrielMaki@users.noreply.github.com>
2024-01-22 20:00:58 -0300

    continuação sensores
    continuação sensores

--- 2024/G_Maki/G_Maki.ino
+++ 2024/G_Maki/G_Maki.ino
@@ -1,5 +1,7 @@
 #include <Adafruit_BMP085.h>
+
 Adafruit_BMP085 bmp;
+
 float AltIni = 0;
 int nLeitIni = 15;
 float somaAltIni = 0;
@@ -7,20 +9,29 @@ float altitudes[10];
 int frente = 0;
 int nLeitFila = 10;
 float rawAltitude = 0;
+
 void setup() {
+
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+
   if (!bmp.begin()) {
-	  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	  while (1) {}
-  }  Serial.println("Temperature (*C) \t Pressure (Pa) \t Altitude (m) \t Pressure at sea level (Pa) \t Raw Altitude \t Real Altitude (m)");
+
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+
+    while (1) {}
+  }
+  Serial.println("Temperature (*C) \t Pressure (Pa) \t Altitude (m) \t Pressure at sea level (Pa) \t Raw Altitude \t Real Altitude (m)");
   for (int i = 0; i < nLeitIni; i++) {
     somaAltIni += bmp.readAltitude();
-  }  AltIni = somaAltIni / nLeitIni;
+  }
+  AltIni = somaAltIni / nLeitIni;
   for (int i = 0; i < nLeitFila; i++) {
     altitudes[i] = bmp.readAltitude();
-  }}
+  }
+}
 void loop() {
+
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
   Serial.print(bmp.readPressure());
@@ -29,13 +40,18 @@ void loop() {
   Serial.print("\t");
   Serial.print(bmp.readSealevelPressure());
   Serial.print("\t");
-   rawAltitude = bmp.readAltitude() - AltIni;
+
+  rawAltitude = bmp.readAltitude() - AltIni;
   Serial.print(rawAltitude);
   Serial.print('\t');
+
   altitudes[frente] = rawAltitude;
   frente = (frente + 1) % nLeitFila;
+
   float somaAltRelativa = 0;
+
   for (int i = 0; i < nLeitFila; i++) {
     somaAltRelativa += altitudes[i];
   }
+  Serial.println();
 }
