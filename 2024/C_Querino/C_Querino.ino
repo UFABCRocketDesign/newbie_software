b29cb7135d3622cf60fd924f2a52c87e1325a1d6
Cauã de Souza Oliveira Querino <caua.oliversouza@gmail.com>
2024-01-22 20:45:29 -0300

    Commit altura5.0

--- 2024/C_Querino/C_Querino.ino
+++ 2024/C_Querino/C_Querino.ino
@@ -5,40 +5,41 @@ float alturainicial;
 float altura=0;
 int l;
 float altufinal;
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
   
   }
-  alturainicial = bmp.readAltitude(101500);
+  alturainicial = bmp.readAltitude();
 
   Serial.print("Temperatura\tpressão\tAltitude\tpressão em relação ao mar\taltitude real");
 }
 
 
-void loop() {
-  for (l=1;l<=5;l++)
+void loop(){
+ /* for (l=1;l<=10;l++)
   
   {
-    altura = altura+bmp.readAltitude(101500);
+    altura = altura + bmp.readAltitude();
   }
-    altura = altura/5;
-
-    altufinal=altura-alturainicial;
-
+    altura = (altura/10)-alturainicial*/
+    
     Serial.print(bmp.readTemperature());
     Serial.print("\t");
+
     Serial.print(bmp.readPressure());
     Serial.print("\t");
-    Serial.print(altufinal);
+
+    Serial.print(bmp.readAltitude()-alturainicial);
     Serial.print("\t");
+
     Serial.print(bmp.readSealevelPressure());
     Serial.print("\t");
+
     Serial.print(bmp.readAltitude(101500));
-    
     Serial.println();
     }
 
