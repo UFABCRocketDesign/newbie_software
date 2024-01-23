9ad6125067ffec2555f304ff9f5e5df3caed162c
Star-BJS <134816653+Star-BJS@users.noreply.github.com>
2024-01-23 17:08:49 -0300

    Update B_Santos.ino

--- 2024/B_Santos/B_Santos.ino
+++ 2024/B_Santos/B_Santos.ino
@@ -1,31 +1,14 @@
-/*
-  Blink
+#include <Adafruit_BMP085.h>
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
+Adafruit_BMP085 bmp;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
 }
 
 // the loop function runs over and over again forever
@@ -34,4 +17,22 @@ void loop() {
   delay(2000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(8000);                      // wait for a second
+
+  Serial.print(bmp.readTemperature());
+    Serial.print(" *C");
+    
+    Serial.print(bmp.readPressure());
+    Serial.print(" Pa");
+    
+    Serial.print(bmp.readAltitude());
+    Serial.print(" meters");
+
+    Serial.print(bmp.readSealevelPressure());
+    Serial.print(" Pa");
+
+    Serial.print(bmp.readAltitude(101500));
+    Serial.print(" meters");
+    
+    Serial.println();
+    delay(500);
 }
