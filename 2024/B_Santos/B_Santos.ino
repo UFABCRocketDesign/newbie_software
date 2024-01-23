ba4f71de2175414a4626d2c4be2effacea75ad32
Star-BJS <134816653+Star-BJS@users.noreply.github.com>
2024-01-23 17:39:12 -0300

    Update B_Santos.ino

--- 2024/B_Santos/B_Santos.ino
+++ 2024/B_Santos/B_Santos.ino
@@ -1,38 +1,57 @@
 #include <Adafruit_BMP085.h>
 
 Adafruit_BMP085 bmp;
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
-  if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
-  }
+
+  void setup() {
+// initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    if (!bmp.begin()) {
+	  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	  while (1) {}
+    }
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(2000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(8000);                      // wait for a second
+ // the loop function runs over and over again forever
+  void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(2000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(8000);                      // wait for a second
 
-  Serial.print(bmp.readTemperature());
-    Serial.print(" *C");
-    
+ //cabeçalho
+    Serial.print("Temp");
+    Serial.print("/t");
+
+    Serial.print("Pressão");
+    Serial.print("/t");
+
+    Serial.print("Altitude");
+    Serial.print("/t");
+  
+    Serial.print("P. Nível do Mar");
+    Serial.print("/t");
+
+    Serial.print("Alt. Nível do mar");
+    Serial.print("/t");
+
+ //Leitura de dados
+
+    Serial.print(bmp.readTemperature());
+    Serial.print("/t")
+  
     Serial.print(bmp.readPressure());
-    Serial.print(" Pa");
-    
+    Serial.print("/t");
+  
     Serial.print(bmp.readAltitude());
-    Serial.print(" meters");
-
-    Serial.print(bmp.readSealevelPressure());
-    Serial.print(" Pa");
-
-    Serial.print(bmp.readAltitude(101500));
-    Serial.print(" meters");
+    Serial.print("/t");
     
+    Serial.print(bmp.readSealevelPressure());
+    Serial.print("/t");
+  
+    Serial.print(bmp.readAltitude(101500));
+    Serial.print("/t");
+   
     Serial.println();
     delay(500);
 }
