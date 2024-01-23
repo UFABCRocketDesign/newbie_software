18b8eed88ed3c810c14542f1b8e46e3b94cd02f7
isahisamoto <isabella.chisamoto@gmail.com>
2024-01-22 21:18:30 -0300

    Update I_Hisamoto.ino

--- 2024/I_Hisamoto/I_Hisamoto.ino
+++ 2024/I_Hisamoto/I_Hisamoto.ino
@@ -7,24 +7,27 @@ int SomaAltitude;
 int MediaAltitudes;
 
 void setup() {
+
   Serial.begin(9600);
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
   }
+
   Serial.print("Temperature(C)\t Pressure(Pa)\t High(meters)\t Pressure at sealevel (calculated, Pa)\t Real altitude(meters)");
-}
-
-void loop() {
-
-  ListaAltitude[0]= bmp.readAltitude(); //armazena valores de altitude
-
+  
   SomaAltitude=0; //para dar partida inicial a soma
   for(int posicaoListaAltitude=0; posicaoListaAltitude<10; posicaoListaAltitude++){
+    ListaAltitude[posicaoListaAltitude]= bmp.readAltitude();
     SomaAltitude += ListaAltitude[posicaoListaAltitude]; //soma das 10 primeiras leituras
   }
 
   MediaAltitudes= SomaAltitude/10; //media das 10 primeiras leituras
+
+}
+
+void loop() {
+
   float Altura= bmp.readAltitude()- MediaAltitudes; 
 
   Serial.print(bmp.readTemperature());
