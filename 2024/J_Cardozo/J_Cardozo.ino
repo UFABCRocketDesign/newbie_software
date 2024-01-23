d636ab360cc3ec79006f5d42606a82c742106131
Joao Henrique <jjoaohenrique@icloud.com>
2024-01-23 18:17:33 -0300

    Teste bmp

--- 2024/J_Cardozo/J_Cardozo.ino
+++ 2024/J_Cardozo/J_Cardozo.ino
@@ -4,11 +4,15 @@ Adafruit_BMP085 bmp;
 
 float var;
 float alturaInicial;
-const int numLeituras = 10;
+const int numLeituras = 20;
 float leituras[numLeituras];
+float medias[numLeituras]; 
 int indiceLeitura = 0;
+int indiceMedia = 0; 
 float soma = 0;
+float somaMedias = 0; 
 float media = 0;
+float mediaDasMedias = 0; 
 
 void setup() {
   Serial.begin(115200);
@@ -29,11 +33,13 @@ void setup() {
  for (int i = 0; i < numLeituras; i++) {
     leituras[i] = bmp.readAltitude() - alturaInicial;
     soma += leituras[i];
+    medias[i] = 0; // Inicializa o array de medias
   }
 
   Serial.print("Temperature(*C)\t");
   Serial.print("Pressure(Pa)\t");
-  Serial.print("Altitude com filtro(m)\t");
+  Serial.print("Altitude com primeiro filtro(m)\t");
+  Serial.print("Altitude com segundo filtro(m)\t");
   Serial.print("Altitude sem filtro(m)\t");
 
   Serial.println("");
@@ -58,11 +64,27 @@ void loop() {
   // Calcula a média
   media = soma / numLeituras;
 
+  // Armazena a média no array de medias e atualiza a soma de medias
+  somaMedias -= medias[indiceMedia];
+  medias[indiceMedia] = media;
+  somaMedias += medias[indiceMedia];
+
+  // Atualiza o índice para o array de medias
+  indiceMedia++;
+  if (indiceMedia >= numLeituras) {
+    indiceMedia = 0;
+  }
+
+  // Calcula a média das médias
+  mediaDasMedias = somaMedias / numLeituras;
+
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
   Serial.print(bmp.readPressure());
   Serial.print("\t");
   Serial.print(media);
+  Serial.print("\t");
+  Serial.print(mediaDasMedias); 
   Serial.print("\t");
   Serial.print(var);
   Serial.print("\t");
