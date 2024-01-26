33fde8d0b644881c00292bac0f53cabf8259ca34
Matheus Alves <aalves.maatheus@gmail.com>
2024-01-26 10:21:08 -0300

    Update M_Alves.ino

--- 2024/M_Alves/M_Alves.ino
+++ 2024/M_Alves/M_Alves.ino
@@ -57,8 +57,8 @@ void setup() {
 
   //Cabeçalho
   String dadosString = "";
-  dadosString += "Temperature (*C) \t Pressure (Pa) \t Raw Altitude (m) \t First Filter (m) \t Second Filter (m) \t Estado (0 ou 1)";
-  Serial.println("Temperature (*C) \t Pressure (Pa) \t Raw Altitude (m) \t First Filter (m) \t Second Filter (m) \t Estado (0 ou 1) ");
+  dadosString += "Temperature (*C)\tPressure (Pa)\tRaw Altitude (m)\tFirst Filter (m)\tSecond Filter (m)\tEstado (0 ou 1)";
+  Serial.println("Temperature (*C)\tPressure (Pa)\tRaw Altitude (m)\tFirst Filter (m)\tSecond Filter (m)\tEstado (0 ou 1) ");
 
   File dadosFile = SD.open("M_Alves.txt", FILE_WRITE);
   if (dadosFile) {
