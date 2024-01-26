3cc98abf5191567328e994e450fee3e18aaf6aba
isahisamoto <isabella.chisamoto@gmail.com>
2024-01-26 17:48:54 -0300

    Update I_Hisamoto.ino

--- 2024/I_Hisamoto/I_Hisamoto.ino
+++ 2024/I_Hisamoto/I_Hisamoto.ino
@@ -7,6 +7,18 @@ float ListaAltitude_0[10];
 float SomaListaAltitude_0;
 float FiltroAltitude_0[10];
 float SomaFiltroAltitude_0;
+float MediaFiltroAltitude_0;
+
+
+float filtro_0(){
+  SomaListaAltitude_0=0;
+  for(int i=0;i<10;i++){
+    SomaListaAltitude_0 += bmp.readAltitude()- AltitudeInicial;
+  }
+  MediaFiltroAltitude_0 = SomaListaAltitude_0/10;
+  return MediaFiltroAltitude_0;
+}
+
 
 void setup() {
 
@@ -17,8 +29,7 @@ void setup() {
   }
 
   Serial.print("Temperature(C)\t Pressure(Pa)\t High(meters)\t Pressure at sealevel (calculated, Pa)\t Real altitude(meters)");
-  
-  //determina uma altitude inicial baseada na media das 10 primeiras leituras
+
   SomaAltitude=0; 
   for(int posicaoListaAltitude=0; posicaoListaAltitude<10; posicaoListaAltitude++){
     SomaAltitude += bmp.readAltitude(); 
@@ -30,18 +41,7 @@ void setup() {
 
 void loop() {
 
-  //cria uma lista para as leituras da altitude e outra lista para a soma dessas leituras. 
-  SomaListaAltitude_0=0;
-  for(int i=0; i<10; i++){ 
-    ListaAltitude_0[i]= bmp.readAltitude()- AltitudeInicial;
-    SomaListaAltitude_0 += ListaAltitude_0[i];
-    FiltroAltitude_0[i]= ListaAltitude_0[i];
-    SomaFiltroAltitude_0 += FiltroAltitude_0[i];
-  }
-
-  // determina a altura a partir da divisão da leitura atual pela media das ultimas 10 leituras.
-  float MediaFiltroAltitude_0= SomaFiltroAltitude_0/10;
-  float Altura= bmp.readAltitude()/MediaFiltroAltitude_0;
+  float Altura= bmp.readAltitude()/filtro_0();
 
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
