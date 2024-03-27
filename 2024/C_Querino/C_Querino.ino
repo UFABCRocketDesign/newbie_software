d265b16918eb54ebb33932eff8fd2a13580c3c8b
Cauã de Souza Oliveira Querino <caua.oliversouza@gmail.com>
2024-03-27 15:34:28 -0300

    filtroA

--- 2024/C_Querino/C_Querino.ino
+++ 2024/C_Querino/C_Querino.ino
@@ -3,27 +3,29 @@
 Adafruit_BMP085 bmp;
 float alturainicial;
 float altura=0;
-int l;
+float height[19];
 float altufinal;
-
+float somaH;
+float filtroA;
 void setup() {
-  Serial.begin(9600);
-  if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
-  
-  }
- for (l=1;l<=20;l++)
-  
-  {
+  Serial.begin(115200);
+  if (!bmp.begin()){
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");}
+  for (int i=0;i<20;i++){ 
+    height[i] = bmp.readAltitude();
+    somaH = height[i]+somaH;
     alturainicial = alturainicial + bmp.readAltitude();
   }
-    altura = (alturainicial/20);
+  filtroA = somaH/20;
+  altura = (alturainicial/20);
   Serial.print("Temperatura\tpressão\tAltitude\tpressão em relação ao mar\taltitude real");
 }
-
 void loop(){
-   
+    for (int j=0;j<20;j++){ 
+      height[j] = bmp.readAltitude();
+      somaH = height[j]+somaH;
+      filtroA = somaH/20;}
+    
     Serial.print(bmp.readTemperature());
     Serial.print("\t");
 
@@ -38,7 +40,9 @@ void loop(){
 
     Serial.print(bmp.readAltitude(101500));
     Serial.println();
+    
+    Serial.print(filtroA);
+    Serial.println("filtro");
+    
+    somaH=0;
     }
-
-
-
