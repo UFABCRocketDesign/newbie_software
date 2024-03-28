367d82af4cb653c51d457b631a25304761bce984
Felymir <153313872+Felymir@users.noreply.github.com>
2024-03-28 12:11:14 -0300

    Agora foi
    Agora certamente foi.

--- 2024/F_Jesus/F_Jesus.ino
+++ 2024/F_Jesus/F_Jesus.ino
@@ -1,11 +1,44 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+
 void setup() {
- 
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+  
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+  }
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(666);                     
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(666);                      
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(666);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(666);
+  
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+  
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+  
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+  
+  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
+  
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.println(" meters");
+  
+  Serial.println();
+  delay(500);
 }
