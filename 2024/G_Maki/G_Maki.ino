5375bac2834bbc5427c61a604372271bf2568aa3
Gabriel Maki <107128221+GabrielMaki@users.noreply.github.com>
2024-04-02 15:33:06 -0300

    Update G_Maki.ino

--- 2024/G_Maki/G_Maki.ino
+++ 2024/G_Maki/G_Maki.ino
@@ -10,10 +10,18 @@ int frente = 0;
 int nLeitFila = 10;
 float rawAltitude = 0;
 
+//variáveis para a média simples:
+const int numLeituras = 25; //leituras para a média
+float leituras[numLeituras]; //leituras do sensor de altitude
+int indiceLeitura = 0; //leitura atual
+float somaLeituras = 0; //soma das leituras
+float mediaAltitude = 0; //média das leituras
+
+
 void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   if (!bmp.begin()) {
 
@@ -21,30 +29,49 @@ void setup() {
 
     while (1) {}
   }
-  Serial.println("Temperature (*C) \t Pressure (Pa) \t Altitude (m) \t Pressure at sea level (Pa) \t Raw Altitude \t Real Altitude (m)");
+  Serial.println("Temperature (*C) \t Pressure (Pa) \t Altitude (m) \t Pressure at sea level (Pa) \t Raw Altitude \t Real Altitude (m)"); //cabeçalho
+
   for (int i = 0; i < nLeitIni; i++) {
-    somaAltIni += bmp.readAltitude();
+    somaAltIni += bmp.readAltitude(); //leituras inciais
   }
-  AltIni = somaAltIni / nLeitIni;
-  for (int i = 0; i < nLeitFila; i++) {
-    altitudes[i] = bmp.readAltitude();
-  }
+
+  AltIni = somaAltIni / nLeitIni; //média leituras
+
+  for (int i = 0; i < numLeituras; i++) {
+  altitudes[i] = bmp.readAltitude() - AltIni;
+  somaLeituras += altitudes[i];
+ }
 
 }
 void loop() {
 
-  Serial.print(bmp.readTemperature());
+  Serial.print(bmp.readTemperature()); //lendo temperatura
   Serial.print("\t");
-  Serial.print(bmp.readPressure());
+  Serial.print(bmp.readPressure()); //lendo pressão
   Serial.print("\t");
-  Serial.print(bmp.readAltitude());
+  Serial.print(bmp.readAltitude()); //lendo altitude
   Serial.print("\t");
-  Serial.print(bmp.readSealevelPressure());
+  Serial.print(bmp.readSealevelPressure()); //lendo altitude no nível do mar
   Serial.print("\t");
 
-  rawAltitude = bmp.readAltitude() - AltIni;
-  Serial.print(rawAltitude);
+  float rawAltitude = bmp.readAltitude() - AltIni; //calculando a altura
+  Serial.print(rawAltitude); //altura sem filtro
   Serial.print('\t');
-  
+
+  //filtro:
+  somaLeituras = somaLeituras - altitudes[indiceLeitura];
+  altitudes[indiceLeitura] = rawAltitude;
+  somaLeituras = somaLeituras + altitudes[indiceLeitura];
+  indiceLeitura++;
+  if (indiceLeitura >= numLeituras) { //quando chegar no último vetor - loop
+    indiceLeitura = 0;
+  }
+
+  //Média
+  mediaAltitude = somaLeituras / numLeituras;
+  Serial.println(mediaAltitude);
+
   Serial.println();
+
+
 }
