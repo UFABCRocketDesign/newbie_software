57f15ca7d33ca9eb95413743177bb5b466e421c7
isahisamoto <isabella.chisamoto@gmail.com>
2024-04-03 15:48:26 -0300

    Update I_Hisamoto.ino

--- 2024/I_Hisamoto/I_Hisamoto.ino
+++ 2024/I_Hisamoto/I_Hisamoto.ino
@@ -3,26 +3,27 @@ Adafruit_BMP085 bmp;
 
 float SomaAltitude;
 float AltitudeInicial;
-float SomaListaAltitude_0;
+float SomaListaAltura_0;
 float MediaFiltroAltitude_0;
-float ListaAltitude[10];
+float ListaAltura[10];
 
-float filtro_0(){
+float filtro_0() {
 
-  for(int i = 9; i > 0; i--){
-    ListaAltitude[i] = ListaAltitude[i-1];
+  for (int i = 9; i > 0; i--) {
+    ListaAltura[i] = ListaAltura[i - 1];
   }
 
-  ListaAltitude[0] = bmp.readAltitude()- AltitudeInicial;
-  
-  SomaListaAltitude_0=0;
-  for(int i = 0; i < 10; i++){
-    SomaListaAltitude_0 += ListaAltitude[i];
+  ListaAltura[0] = bmp.readAltitude() - AltitudeInicial;
+
+  SomaListaAltura_0 = 0;
+  for (int i = 0; i < 10; i++) {
+    SomaListaAltura_0 += ListaAltura[i];
   }
 
-  MediaFiltroAltitude_0 = SomaListaAltitude_0/10;
+  MediaFiltroAltura_0 = SomaListaAltura_0 / 10;
+
+
   return MediaFiltroAltitude_0;
-
 }
 
 
@@ -30,24 +31,23 @@ void setup() {
 
   Serial.begin(115200);
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
 
   Serial.print("Temperature(C)\t Pressure(Pa)\t High(meters)\t Pressure at sealevel (calculated, Pa)\t Real altitude(meters)");
 
-  SomaAltitude=0; 
-  for(int posicaoListaAltitude=0; posicaoListaAltitude<10; posicaoListaAltitude++){
-    SomaAltitude += bmp.readAltitude(); 
+  SomaAltitude = 0;
+  for (int posicaoListaAltitude = 0; posicaoListaAltitude < 10; posicaoListaAltitude++) {
+    SomaAltitude += bmp.readAltitude();
   }
 
-  AltitudeInicial= SomaAltitude/10; 
-
+  AltitudeInicial = SomaAltitude / 10;
 }
 
 void loop() {
-
-  float Altura= (bmp.readAltitude()- AltitudeInicial;)/filtro_0();
+  
+  float Altura = filtro_0();
 
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
@@ -58,6 +58,6 @@ void loop() {
   Serial.print(bmp.readSealevelPressure());
   Serial.print("\t");
   Serial.print(bmp.readAltitude(101500));
-    
+
   Serial.println();
 }
