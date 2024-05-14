86ce47e3a31a52ce225a313777cecf653c5fa8f1
isahisamoto <isabella.chisamoto@gmail.com>
2024-05-14 15:24:18 -0300

    Update I_Hisamoto.ino

--- 2024/I_Hisamoto/I_Hisamoto.ino
+++ 2024/I_Hisamoto/I_Hisamoto.ino
@@ -1,40 +1,40 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
 
-float AltitudeInicial;
-float SomaAltitude;
-float ListaSuavizarCurva_0[10];
-float ListaSuavizarCurva_1[5];
-float ListaDeteccaoQueda[2];
+float altitudeInicial;
+float somaAltitude;
+float listaSuavizarCurva_0[10];
+float listaSuavizarCurva_1[5];
+float listaDeteccaoQueda[2];
 int contador = 0;
 
 
 float filtroSuavizarCurva_0(float dadosCurva_0) {
 
   for (int i = 9; i > 0; i--) {
-    ListaSuavizarCurva_0[i] = ListaSuavizarCurva_0[i - 1];
+    listaSuavizarCurva_0[i] = listaSuavizarCurva_0[i - 1];
   }
-  ListaSuavizarCurva_0[0] = dadosCurva_0;
-  float SomaLista = 0;
+  listaSuavizarCurva_0[0] = dadosCurva_0;
+  float somaLista = 0;
   for (int i = 0; i < 10; i++) {
-    SomaLista += ListaSuavizarCurva_0[i];
+    somaLista += listaSuavizarCurva_0[i];
   }
-  float MediaFiltro = SomaLista / 10;
-  return MediaFiltro;
+  float mediaFiltro = somaLista / 10;
+  return mediaFiltro;
 }
 
 float filtroSuavizarCurva_1(float dadosCurva_1) {
 
   for (int i = 4; i > 0; i--) {
-    ListaSuavizarCurva_1[i] = ListaSuavizarCurva_1[i - 1];
+    listaSuavizarCurva_1[i] = listaSuavizarCurva_1[i - 1];
   }
-  ListaSuavizarCurva_1[0] = dadosCurva_1;
-  float SomaLista = 0;
+  listaSuavizarCurva_1[0] = dadosCurva_1;
+  float somaLista = 0;
   for (int i = 0; i < 5; i++) {
-    SomaLista += ListaSuavizarCurva_1[i];
+    somaLista += listaSuavizarCurva_1[i];
   }
-  float MediaFiltro = SomaLista / 5;
-  return MediaFiltro;
+  float mediaFiltro = somaLista / 5;
+  return mediaFiltro;
 }
 
 
@@ -48,45 +48,47 @@ void setup() {
 
   Serial.print("Temperature(C)\t Pressure(Pa)\t High(meters)\t Fallen(1)/ Not fallen (0)\t Contador de Queda");
 
-  SomaAltitude = 0;
+  somaAltitude = 0;
   for (int posicaoListaAltitude = 0; posicaoListaAltitude < 10; posicaoListaAltitude++) {
-    SomaAltitude += bmp.readAltitude();
+    somaAltitude += bmp.readAltitude();
   }
 
-  AltitudeInicial = SomaAltitude / 10;
+  altitudeInicial = somaAltitude / 10;
 }
 
 void loop() {
-  
-  float Altura = bmp.readAltitude() - AltitudeInicial;
-  float Altura_Filtrada_0 = filtroSuavizarCurva_0(Altura);
-  float Altura_Filtrada_1 = filtroSuavizarCurva_1(Altura_Filtrada_0);
+
+  float altura = bmp.readAltitude() - altitudeInicial;
+  float alturaFiltrada_0 = filtroSuavizarCurva_0(altura);
+  float alturaFiltrada_1 = filtroSuavizarCurva_1(alturaFiltrada_0);
+  float alturaAnterior;
   int fallenCondition = 0;
 
-  for (int i = 1; i <= 2; i++){
-    if (ListaDeteccaoQueda[i - 1] < ListaDeteccaoQueda[i]) {
-      contador++;
-    } else{ 
-      contador = 0;
-    }
-    ListaDeteccaoQueda[0] = Altura_Filtrada_1;
-    if (contador >= 5) {
-      fallenCondition = 1;
-    } else {
-      fallenCondition = 0;
-    }
-  
+
+  for (int i = 1; i < 2; i++) {
+    listaDeteccaoQueda[0] = alturaFiltrada_1;
+    listaDeteccaoQueda[1] = alturaAnterior;
+  }
+  if (alturaFiltrada_1 < alturaAnterior) {
+    contador++;
+  } else {
+    contador = 0;
+  }
+  if (contador >= 5) {
+    fallenCondition = 1;
+  } else {
+    fallenCondition = 0;
   }
 
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
   Serial.print(bmp.readPressure());
   Serial.print("\t");
-  Serial.print(Altura);
+  Serial.print(altura);
   Serial.print("\t");
-  Serial.print(Altura_Filtrada_0);
+  Serial.print(alturaFiltrada_0);
   Serial.print("\t");
-  Serial.print(Altura_Filtrada_1);
+  Serial.print(alturaFiltrada_1);
   Serial.print("\t");
   Serial.print(fallenCondition);
   Serial.print("\t");
