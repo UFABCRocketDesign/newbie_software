717ea988da604852777a66c3a77cf3f70da7633d
joao <jjoaohenrique@icloud.com>
2024-06-04 17:05:46 -0300

    biblioteca acelerometro

--- 2024/J_Cardozo/src/lib/Acelerometro/Acel.cpp
+++ 2024/J_Cardozo/src/lib/Acelerometro/Acel.cpp
@@ -1,2 +1,68 @@
 #include "Acel.h"
 
+bool Acel::begin() {
+  Wire.beginTransmission(ADXAddress);
+  Wire.write(Reg_2D);
+  Wire.write(8);
+  return (Wire.endTransmission() == 0);
+}
+
+void Acel::lerTudo() {
+  //X
+  Wire.beginTransmission(ADXAddress);
+  Wire.write(Reg_X0);
+  Wire.write(Reg_X1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    X0 = Wire.read();
+    X1 = Wire.read(); 
+    X1=X1<<8;
+    X_out=X0+X1;   
+  }
+
+  //Y
+  Wire.beginTransmission(ADXAddress);
+  Wire.write(Register_Y0);
+  Wire.write(Register_Y1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    Y0 = Wire.read();
+    Y1 = Wire.read(); 
+    Y1=Y1<<8;
+    Y_out=Y0+Y1;
+  }
+
+  //Z
+  Wire.beginTransmission(ADXAddress);
+  Wire.write(Register_Z0);
+  Wire.write(Register_Z1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    Z0 = Wire.read();
+    Z1 = Wire.read(); 
+    Z1=Z1<<8;
+    Z_out=Z0+Z1;
+  }
+
+  Xg=X_out/256.0;
+  Yg=Y_out/256.0;
+  Zg=Z_out/256.0;
+}
+
+float Acel::getX() {
+  return Xg;
+}
+
+float Acel::getY() {
+  return Yg;
+}
+
+float Acel::getZ() {
+  return Zg;
+}
--- 2024/J_Cardozo/src/lib/Acelerometro/Acel.h
+++ 2024/J_Cardozo/src/lib/Acelerometro/Acel.h
@@ -1,19 +1,33 @@
 #ifndef ACEL_H
 #define ACEL_H
 
-class Apogeu {
-  const int historicoTamanho;
-  const float fatorDescida;
-  float* historico;
-  int indice = 0;
-  bool estaDescendo;
+#include <Wire.h>
+
+#define Reg_ID 0
+#define Reg_2D 0x2D
+#define Reg_X0 0x32
+#define Reg_X1 0x33
+#define Reg_Y0 0x34
+#define Reg_Y1 0x35
+#define Reg_Z0 0x36
+#define Reg_Z1 0x37
+
+class Acel {
+private:
+  int ADXAddress = 0x53;  
+  int reading = 0;
+  int val=0;
+  int X0,X1,X_out;
+  int Y0,Y1,Y_out;
+  int Z1,Z0,Z_out;
+  double Xg,Yg,Zg;
 
 public:
-  Apogeu(int tamanho = 20, float fator = 0.85f);
-  ~Apogeu();
-
-  bool deteccaoApogeu(float entrada);
-  bool getEstaDescendo() const;
+  bool begin();
+  void lerTudo();
+  float getX();
+  float getY();
+  float getZ();
 };
 
 #endif
