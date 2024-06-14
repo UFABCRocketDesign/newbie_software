eb10c324dba7b04137ffc00de541d20309d9c256
Heitor Rodrigues <heitor.rodrigues.savegnago@outlook.com>
2024-06-14 16:15:19 -0300

    Update pinos.h

--- pinos.h
+++ pinos.h
@@ -8,3 +8,11 @@
 #define IGN_2 61	/*act2*/
 #define IGN_3 46	/*act3*/
 #define IGN_4 55	/*act4*/
+
+#define HEAL_1 68	/*health 1*/
+#define HEAL_2 62	/*health 2*/
+#define HEAL_3 56	/*health 3*/
+#define HEAL_4 58	/*health 4*/
+
+#define BUZZ_PIN A0
+#define BUZZ_CMD LOW
