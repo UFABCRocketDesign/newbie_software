829e569daa317e912dd26686d2842b51f90e7b57
Matheus Alves <aalves.maatheus@gmail.com>
2024-07-02 22:28:56 -0300

    Update Barometro.h

--- 2024/M_Alves/src/lib/Barometro/Barometro.h
+++ 2024/M_Alves/src/lib/Barometro/Barometro.h
@@ -17,9 +17,9 @@ class Barometro {
 
   private:
     const unsigned char OSS = 0;  
-    int ac1, ac2, ac3, b1, b2, mb, mc, md;
+    int ac1, ac2, ac3, mb, mc, md;
     unsigned int ac4, ac5, ac6;
-    long b5; 
+    long b1, b2, b5; 
     float temperatura;
     long pressao;
     float altitude;
