54d7dbb3014f57943abbd2ababfb4ec18f590a9e
leoSartori1 <leosartori25@gmail.com>
2025-02-21 17:30:39 -0300

    Update L_Sartori.ino

--- 2025/L_Sartori/L_Sartori.ino
+++ 2025/L_Sartori/L_Sartori.ino
@@ -1,27 +1,5 @@
 #include <Adafruit_BMP085.h>
 
-/*************************************************** 
-This is an example for the BMP085 Barometric Pressure & Temp Sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout 
-  ----> https://www.adafruit.com/products/391
-
-  These pressure and temperature sensors use I2C to communicate, 2 pins
-  are required to interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
-// Connect GND to Ground
-// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
-// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
-// EOC is not used, it signifies an end of conversion
-// XCLR is a reset pin, also not used here
 
 Adafruit_BMP085 bmp;
   
@@ -32,30 +10,30 @@ void setup() {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   }
-  Serial.println("Temperature | Pressure | Altitude |Pressure | Altitude");
+  Serial.println("Temperature\tPressure\tAltitude\tPressure\tAltitude\t");
 }
   
 void loop() {
     Serial.print(bmp.readTemperature());
-    Serial.print(";  ");
+    Serial.print("\t");
     
     Serial.print(bmp.readPressure());
-    Serial.print(";  ");
+    Serial.print("\t");
     
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
     Serial.print(bmp.readAltitude());
-    Serial.print(";  ");
+    Serial.print("\t");
 
     Serial.print(bmp.readSealevelPressure());
-    Serial.print(";  ");
+    Serial.print("\t");
 
   // you can get a more precise measurement of altitude
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
     Serial.print(bmp.readAltitude(101500));
-    Serial.println(";");
+    Serial.println("\t");
 
     delay(300);
 }
