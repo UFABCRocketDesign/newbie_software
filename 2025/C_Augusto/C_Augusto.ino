f7c191b5ed9689dfcee9b6a23410bd9237a8c1ba
cesarafcm <87275058+cesarafcm@users.noreply.github.com>
2025-02-21 16:06:23 -0300

    Update C_Augusto.ino

--- 2025/C_Augusto/C_Augusto.ino
+++ 2025/C_Augusto/C_Augusto.ino
@@ -7,9 +7,12 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_PISCA, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(180);                       // wait for a second
   digitalWrite(LED_PISCA, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
-  
+  delay(180);
+   digitalWrite(LED_PISCA, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);
+  digitalWrite(LED_PISCA, LOW);    // turn the LED off by making the voltage LOW
+  delay(500); 
   //comentario super original feito por cesar
 }
