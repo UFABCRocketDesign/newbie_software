bb95133f051e492ad180341604386135fcbcc33a
cesarafcm <87275058+cesarafcm@users.noreply.github.com>
2025-02-21 15:55:44 -0300

    Update C_Augusto.ino

---
+++ 2025/C_Augusto/C_Augusto.ino
@@ -0,0 +1,15 @@
+#define LED_PISCA 13
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_PISCA, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_PISCA, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_PISCA, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
+  
+  //comentario super original feito por cesar
+}
