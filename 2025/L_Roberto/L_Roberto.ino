8d30ea1b48d19e0f214857c8316db7937a1c9e14
Luis <roberto4luis1@gmail.com>
2025-02-21 16:39:50 -0300

    BMP e Blink

--- 2025/L_Roberto/L_Roberto.ino
+++ 2025/L_Roberto/L_Roberto.ino
@@ -1,9 +1,41 @@
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+
 void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+  }
 }
 void loop(){
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(10);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(10);
+
+  Serial.print("Temperatura = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressão = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.print("Pressão no nível do mar (calculada) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Altitude real = ");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.println(" meters");
+
+  Serial.println();
+  delay(500);
 }
