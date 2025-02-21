e61c9ec044213c7aba1c5f9e34ad4f02f5bce28a
rrzampieri <rrzampieri@hotmail.com>
2025-02-21 16:55:19 -0300

    Otimização BMP085

--- 2025/R_Zampieri/R_Zampieri.ino
+++ 2025/R_Zampieri/R_Zampieri.ino
@@ -3,45 +3,21 @@
 Adafruit_BMP085 bmp;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
   }
+  Serial.println("Temperatura, Pressão, Altitude, Pressão no Nível do Mar, Altitude Real.");
 }
 
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(200);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(200);
-
-  Serial.print("Temperatura = ");
   Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
-
-  Serial.print("Pressão = ");
   Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
-
-  Serial.print("Altitude = ");
   Serial.print(bmp.readAltitude());
-  Serial.println(" metros");
-
-  Serial.print("Pressão no nível do mar (calculada) = ");
   Serial.print(bmp.readSealevelPressure());
-  Serial.println(" Pa");
-
-  Serial.print("Altitude real = ");
   Serial.print(bmp.readAltitude(101500));
-  Serial.println(" metros");
 
   Serial.println();
-  delay(500);
 }
