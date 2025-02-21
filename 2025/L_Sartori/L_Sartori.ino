918228b1c0d9a8a0463c96c1a46d0974fb8e3e3f
leoSartori1 <leosartori25@gmail.com>
2025-02-21 17:04:25 -0300

    Update L_Sartori.ino

--- 2025/L_Sartori/L_Sartori.ino
+++ 2025/L_Sartori/L_Sartori.ino
@@ -32,30 +32,30 @@ void setup() {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   }
+  Serial.println("Temperature | Pressure | Altitude |Pressure | Altitude");
 }
   
 void loop() {
-    Serial.println("Temperature | Pressure | Altitude |Pressure | Altitude");
     Serial.print(bmp.readTemperature());
-    Serial.print(" *C;    ");
+    Serial.print(";  ");
     
     Serial.print(bmp.readPressure());
-    Serial.print(" Pa;    ");
+    Serial.print(";  ");
     
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
     Serial.print(bmp.readAltitude());
-    Serial.print(" m;    ");
+    Serial.print(";  ");
 
     Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa;    ");
+    Serial.println(";  ");
 
   // you can get a more precise measurement of altitude
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
     Serial.print(bmp.readAltitude(101500));
-    Serial.println(" m;");
+    Serial.println(";");
 
     Serial.println();
     delay(300);
