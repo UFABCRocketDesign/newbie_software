6d7ddb8349966e43d904dcd1beed46ca102c7d94
rrzampieri <rrzampieri@hotmail.com>
2025-02-21 17:27:52 -0300

    Correção 2 BMP085

--- 2025/R_Zampieri/R_Zampieri.ino
+++ 2025/R_Zampieri/R_Zampieri.ino
@@ -8,26 +8,26 @@ void setup() {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
   }
-  Serial.print("Temperatura/t");
-  Serial.print("Pressão/t");
-  Serial.print("Altitude/t");
-  Serial.print("Pressão no Nível do Mar/t");
-  Serial.print("Altitude Real/t");
+  Serial.print("Temperatura\t");
+  Serial.print("Pressão\t");
+  Serial.print("Altitude\t");
+  Serial.print("Pressão no Nível do Mar\t");
+  Serial.print("Altitude Real\t");
   Serial.println();
 }
 
 
 void loop() {
   Serial.print(bmp.readTemperature());
-  Serial.print("/t");
+  Serial.print("\t");
   Serial.print(bmp.readPressure());
-  Serial.print("/t");
+  Serial.print("\t");
   Serial.print(bmp.readAltitude());
-  Serial.print("/t");
+  Serial.print("\t");
   Serial.print(bmp.readSealevelPressure());
-  Serial.print("/t");
+  Serial.print("\t");
   Serial.print(bmp.readAltitude(101500));
-  Serial.print("/t");
+  Serial.print("\t");
 
   Serial.println();
 }
