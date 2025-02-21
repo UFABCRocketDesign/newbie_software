d2530b96b0453a95016d0dea35088609c33ec3e5
leoSartori1 <leosartori25@gmail.com>
2025-02-21 16:58:07 -0300

    Update L_Sartori.ino

--- 2025/L_Sartori/L_Sartori.ino
+++ 2025/L_Sartori/L_Sartori.ino
@@ -35,42 +35,28 @@ void setup() {
 }
   
 void loop() {
-    Serial.print("Temperature = ");
+    Serial.println("Temperature | Pressure | Altitude |Pressure | Altitude");
     Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
+    Serial.print(" *C;    ");
     
-    Serial.print("Pressure = ");
     Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
+    Serial.print(" Pa;    ");
     
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print("Altitude = ");
     Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
+    Serial.print(" m;    ");
 
-    Serial.print("Pressure at sealevel (calculated) = ");
     Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
+    Serial.println(" Pa;    ");
 
   // you can get a more precise measurement of altitude
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
-    Serial.print("Real altitude = ");
     Serial.print(bmp.readAltitude(101500));
-    Serial.println(" meters");
+    Serial.println(" m;");
 
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1500);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(500);     // wait for a second
-
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(500);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(1500);     // wait for a second
-    
     Serial.println();
-    delay(500);
+    delay(300);
 }
