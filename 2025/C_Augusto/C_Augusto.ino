3e737e723d885cb672994929b45058c956a27a71
cesarafcm <87275058+cesarafcm@users.noreply.github.com>
2025-02-25 15:53:56 -0300

    Update C_Augusto.ino

--- 2025/C_Augusto/C_Augusto.ino
+++ 2025/C_Augusto/C_Augusto.ino
@@ -1,38 +1,42 @@
-#include <Adafruit_BMP085.h>
+  #include <Adafruit_BMP085.h>
 
-Adafruit_BMP085 bmp;
-float altitudeTarada = 0;  
+  Adafruit_BMP085 bmp;
+  float altitudeTarada = 0;  
+  float suavizador = 0.1;
 
-void setup() {
-  Serial.begin(115200);
-  if (!bmp.begin()) {
-    Serial.println("Nao foi possivel encontrar o bmp085, verificar as conexoes!");
-    while (1) {}
-  }  
+  void setup() {
+    Serial.begin(115200);
+    if (!bmp.begin()) {
+      Serial.println("Nao foi possivel encontrar o bmp085, verificar as conexoes!");
+      while (1) {}
+    }  
 
-  for (int i = 0; i < 10; i++) { 
-    altitudeTarada += bmp.readAltitude();
+    for (int i = 0; i < 10; i++) { 
+      altitudeTarada += bmp.readAltitude();
+
+    }
+    altitudeTarada /= 10; 
+
+    Serial.println("Temperatura \t Pressao \t Altitude \t Nivel do mar \t Altitude Relativa");
+  }
+
+  void loop() {
+      
+      altitudeTarada = ((altitudeTarada*(1-suavizador))+(bmp.readAltitude()*suavizador));
+      
+      Serial.print(bmp.readTemperature());
+      Serial.print("\t");
+
+      Serial.print(bmp.readPressure());
+      Serial.print("\t");
+
+      Serial.print(bmp.readAltitude());
+      Serial.print("\t");
+
+      Serial.print(bmp.readSealevelPressure());
+      Serial.print("\t");
+
+      Serial.print(bmp.readAltitude() - altitudeTarada);
+      Serial.println();
 
   }
-  altitudeTarada /= 10; 
-
-  Serial.println("Temperatura \t Pressao \t Altitude \t Nivel do mar \t Altitude Relativa");
-}
-
-void loop() {
-    Serial.print(bmp.readTemperature());
-    Serial.print("\t");
-
-    Serial.print(bmp.readPressure());
-    Serial.print("\t");
-
-    Serial.print(bmp.readAltitude());
-    Serial.print("\t");
-
-    Serial.print(bmp.readSealevelPressure());
-    Serial.print("\t");
-
-    Serial.print(bmp.readAltitude() - altitudeTarada);
-    Serial.println();
-
-}
