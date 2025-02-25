c43a2d255c166f05a318b953e04d348fabf13e2f
cesarafcm <87275058+cesarafcm@users.noreply.github.com>
2025-02-25 14:31:23 -0300

    Update C_Augusto.ino

--- 2025/C_Augusto/C_Augusto.ino
+++ 2025/C_Augusto/C_Augusto.ino
@@ -2,20 +2,25 @@
 
 
 Adafruit_BMP085 bmp;
-  
+float pressaoTara;
+
+
 void setup() {
   Serial.begin(115200);
   if (!bmp.begin()) {
 	Serial.println("Nao foi possivel encontrar o bmp085, verificar as conexoes!");
 	while (1) {}
   }
+pressaoTara = bmp.readPressure();  
+
 Serial.println("Temperatura \t Pressao \t Altitude \t Nivel do mar \t Altitude Real");
 }
   
+
 void loop() {
     Serial.print( bmp.readTemperature());
     Serial.print("\t");
-    Serial.print(bmp.readAltitude());
+    Serial.print(bmp.readAltitude(pressaoTara));
     Serial.print("\t");
     Serial.print(bmp.readPressure());
     Serial.print("\t");
