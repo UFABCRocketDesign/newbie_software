7d634ca2e22c16af3f496c074abf73523dc74260
leoSartori1 <leosartori25@gmail.com>
2025-02-25 15:57:09 -0300

    Update L_Sartori.ino

--- 2025/L_Sartori/L_Sartori.ino
+++ 2025/L_Sartori/L_Sartori.ino
@@ -3,7 +3,8 @@
 
 Adafruit_BMP085 bmp;
 float med_alt = 0; 
-float alt5;
+float c[5];
+int k =0;
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -15,31 +16,35 @@ void setup() {
   for(int i =0;i<10;i++){
     med_alt += bmp.readAltitude();
   }
-  med_alt /=10;
-  alt5 = 5 * med_alt; 
+  med_alt /=10; 
+  for(int i=0;i<5;i++){
+    c[i]= bmp.readAltitude()- med_alt; 
+  }
 }
 
 void loop() {
-    float a = (bmp.readTemperature()+bmp.readTemperature()+bmp.readTemperature()+bmp.readTemperature()+bmp.readTemperature())/5;
-    Serial.print(a);
+    Serial.print(bmp.readTemperature());
     Serial.print("\t");
-    float b =(bmp.readPressure()+bmp.readPressure()+bmp.readPressure()+bmp.readPressure()+bmp.readPressure())/5;
-    Serial.print(b);
+   
+    Serial.print(bmp.readPressure());
     Serial.print("\t");
-    float c = (bmp.readAltitude()+bmp.readAltitude()+bmp.readAltitude()+bmp.readAltitude()+bmp.readAltitude()-alt5)/5;
+    c[k] = bmp.readAltitude()- med_alt;
+    k +=1;
+    k%=5;
+    
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print(c);
+    Serial.print((c[0]+c[1]+c[2]+c[3]+c[4])/5);
     Serial.print("\t");
-    float d = (bmp.readSealevelPressure()+bmp.readSealevelPressure()+bmp.readSealevelPressure()+bmp.readSealevelPressure()+bmp.readSealevelPressure())/5;
-    Serial.print(d);
+    
+    Serial.print(bmp.readSealevelPressure());
     Serial.print("\t");
-    float e = ((bmp.readAltitude(101500))+(bmp.readAltitude(101500))+(bmp.readAltitude(101500))+(bmp.readAltitude(101500))+(bmp.readAltitude(101500)))/5;
+    
   // you can get a more precise measurement of altitude
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
-    Serial.print(e);
+    Serial.print((bmp.readAltitude(101500)));
     Serial.println("\t");
 
 
