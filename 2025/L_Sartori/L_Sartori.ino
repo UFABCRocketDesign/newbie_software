19bbd1b7f50474ff5648c2f7e34814de516e3f25
leoSartori1 <leosartori25@gmail.com>
2025-02-28 19:05:14 -0300

    Update L_Sartori.ino

--- 2025/L_Sartori/L_Sartori.ino
+++ 2025/L_Sartori/L_Sartori.ino
@@ -4,29 +4,31 @@
 Adafruit_BMP085 bmp;
 bool h = false;
 float med_alt = 0; 
-float c[5][2];
-float ss1;
-int j =1;
-int jr =0;
-float s1;
+float c[2][5];
+float f1;
+float ff1;
 int k;
+int i;
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+ 
   if (!bmp.begin()) {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  
   while (1) {}
   }
   Serial.println("Temperature\tPressure\tAltitude\tPressure\tAltitude\t");
+  
   for(int i =0;i<10;i++){
     med_alt += bmp.readAltitude();
   }
+ 
   med_alt /=10; 
+  
   for(int i=0;i<5;i++){
-    c[i][0]= bmp.readAltitude()- med_alt; 
-    c[i][1]= 0;
+    c[0][i]=0;
+    c[1][i]=0; 
   }
-  s1 = (c[0][0]+c[1][0]+c[2][0]+c[3][0]+c[4][0])/5;
 }
 
 void loop() {
@@ -37,31 +39,33 @@ void loop() {
     Serial.print("\t");
 
     
+    for (i =0 ; i<5;i++){
+      c[0][i]=  bmp.readAltitude()- med_alt;
+      for(int j =0;j<5;j++){
+        c[1][i]+= c[0][j];
+      }
+      c[0][i]/=5;
+      for(int j =0;j<5;j++){
+        ff1+= c[1][j];
+      }
+      ff1/=5;
+    }
     
-    c[j][0] = bmp.readAltitude()- med_alt;
-    s1 = (c[0][0]+c[1][0]+c[2][0]+c[3][0]+c[4][0])/5;
-    c[j][1] = s1;
-    ss1 = (c[0][1]+c[1][1]+c[2][1]+c[3][1]+c[4][1])/5;
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print(c[j][0]);
+    Serial.print(c[0][i]);
     Serial.print("\t");
     
-    Serial.print(c[j][1]);
+    Serial.print(ff1);
     Serial.print("\t");
-    
-    j +=1;
-    j%=5;
-    jr +=1;
-    jr%=5;    
+   
     
     Serial.print(bmp.readSealevelPressure());
     Serial.print("\t");
-      
-    if (c[jr][1]>c[j][1]){
-      h = true;
+    if (i==0){  
+      h = c[1][5]>c[1][i];
     } else {
-      h = false; 
+      h = c[1][i-1]>c[1][i];
     }
     Serial.print(h);
     Serial.print("\t"); 
