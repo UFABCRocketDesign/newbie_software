b325ffa55c1f1b7043b8e896703fecccbad623a5
rrzampieri <rrzampieri@hotmail.com>
2025-02-28 15:49:55 -0300

    Segunda tentativa de filtro

--- 2025/R_Zampieri/R_Zampieri.ino
+++ 2025/R_Zampieri/R_Zampieri.ino
@@ -1,20 +1,34 @@
 #include <Adafruit_BMP085.h>
 
 Adafruit_BMP085 bmp;
+//Declarando variáveis e array
+float tara = 0;
+float vetor[5];
+int guia = 0;
+//
 
-float x = 0;
-float filtro = 0;
-float filtro_final = 0;
 void setup() {
+  //Inicialização do sensor
   Serial.begin(115200);
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
   }
+  //
+
+  //Reset de altitude
   for (int i = 1; i <= 10; ++i) {
-    x = x + bmp.readAltitude();
-    }
-  x = x/10;
+    tara += bmp.readAltitude();
+  }
+  tara /= 10;
+  //
+
+  //Vetor inicial de suavização
+  for (int i = 0; i < 5; i = i + 1) {
+    vetor[i] = bmp.readAltitude() - tara;
+  }
+  //
+
   Serial.print("Temperatura\t");
   Serial.print("Pressão\t");
   Serial.print("Altitude\t");
@@ -24,18 +38,24 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 1; i <= 5; ++i) {filtro = filtro + bmp.readAltitude();}
-  filtro_final = filtro*5;
+  //print dos valores medidos
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
   Serial.print(bmp.readPressure());
   Serial.print("\t");
-  Serial.print(filtro_final-x);
+  Serial.print((vetor[0] + vetor[1] + vetor[2] + vetor[3] + vetor[4]) / 5);
   Serial.print("\t");
   Serial.print(bmp.readSealevelPressure());
   Serial.print("\t");
   Serial.print(bmp.readAltitude(101500));
   Serial.print("\t");
-
   Serial.println();
+
+  //atualização dos valores gravados
+  vetor[guia] = bmp.readAltitude() - tara;
+  if (guia < 5) {
+    guia += 1;
+  } else {
+    guia = 0;
+  }
 }
