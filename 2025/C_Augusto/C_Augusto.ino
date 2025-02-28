3bf7df68347d269b77751722a8c4be8dcd582f9f
cesarafcm <87275058+cesarafcm@users.noreply.github.com>
2025-02-28 14:23:10 -0300

    Update C_Augusto.ino

--- 2025/C_Augusto/C_Augusto.ino
+++ 2025/C_Augusto/C_Augusto.ino
@@ -1,43 +1,37 @@
-    #include <Adafruit_BMP085.h>
+#include <Adafruit_BMP085.h>
 
-    Adafruit_BMP085 bmp;
-    float altitudeTarada = 0;  
-    float suavizador = 0.01;
+Adafruit_BMP085 bmp;
+#define AMOSTRAS 20
+float leituras[AMOSTRAS];
+float soma = 0;
+int indice = 0;
 
-    void setup() {
-      Serial.begin(115200);
-      if (!bmp.begin()) {
-        Serial.println("Nao foi possivel encontrar o bmp085, verificar as conexoes!");
-        while (1) {}
-      }  
+void setup() {
+  Serial.begin(115200);
+  if (!bmp.begin()) {
+    while (1) {}
+  }
+  for (int i = 0; i < AMOSTRAS; i++) {
+    float valor = bmp.readAltitude();
+    leituras[i] = valor;
+    soma += valor;
+  }
+  Serial.println("Temperatura \t Pressao \t Altitude \t Nivel do mar \t Altitude Filtrada");
+}
 
-      for (int i = 0; i < 10; i++) { 
-        altitudeTarada += bmp.readAltitude();
-
-      }
-      altitudeTarada /= 10; 
-      float altitudeNova = bmp.readAltitude();
-
-      Serial.println("Temperatura \t Pressao \t Altitude \t Nivel do mar \t Altitude Relativa");
-    }
-
-    void loop() {
-        
-      float altitudeNova = ((altitudeNova*(1-suavizador))+(bmp.readAltitude()*suavizador));
-        
-        Serial.print(bmp.readTemperature());
-        Serial.print("\t");
-
-        Serial.print(bmp.readPressure());
-        Serial.print("\t");
-
-        Serial.print(bmp.readAltitude());
-        Serial.print("\t");
-
-        Serial.print(bmp.readSealevelPressure());
-        Serial.print("\t");
-
-        Serial.print(altitudeNova);
-        Serial.println();
-
-    }
+void loop() {
+  float valor = bmp.readAltitude();
+  soma = soma - leituras[indice] + valor;
+  leituras[indice] = valor;
+  indice = (indice + 1) % AMOSTRAS;
+  float altitudeFiltrada = soma / AMOSTRAS;
+  Serial.print(bmp.readTemperature());
+  Serial.print("\t");
+  Serial.print(bmp.readPressure());
+  Serial.print("\t");
+  Serial.print(bmp.readAltitude());
+  Serial.print("\t");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.print("\t");
+  Serial.println(altitudeFiltrada);
+}
