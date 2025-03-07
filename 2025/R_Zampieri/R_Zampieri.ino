fdc02cadeeca550c5d230dd57bbe8479be5efcd9
rrzampieri <rrzampieri@hotmail.com>
2025-03-07 14:42:40 -0300

    Correção 2 de variável altitude filtrada

--- 2025/R_Zampieri/R_Zampieri.ino
+++ 2025/R_Zampieri/R_Zampieri.ino
@@ -32,8 +32,6 @@ void setup() {
   Serial.println();
 }
 
-float altitude_filtrada = (vetor[0] + vetor[1] + vetor[2] + vetor[3] + vetor[4]) / 5;
-
 void loop() {
 
   //atualização dos valores gravados
@@ -43,7 +41,7 @@ void loop() {
   } else {
     guia = 0;
   }
-  
+
   float altitude_filtrada = (vetor[0] + vetor[1] + vetor[2] + vetor[3] + vetor[4]) / 5;
 
   //print dos valores medidos
