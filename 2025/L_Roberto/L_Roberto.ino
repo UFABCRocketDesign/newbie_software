11b5abfadf07d782e9c4055d79755432dd518fd3
Luis <roberto4luis1@gmail.com>
2025-03-07 14:59:46 -0300

    a

--- 2025/L_Roberto/L_Roberto.ino
+++ 2025/L_Roberto/L_Roberto.ino
@@ -1,8 +1,11 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
+
 float alt = 0;
-float leituras[5] = {0};
+float leituras[10] = {0};
 int index = 0;
+float filteredAltitude(const float);
+
 void setup(){  
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
@@ -15,30 +18,39 @@ void setup(){
     alt += bmp.readAltitude();
   alt = alt/100 ;
 }
-float filteredAltitude(float altura_atual){
-  if (index == 5) {
+
+void loop(){
+ float altura_atual = bmp.readAltitude() - alt;
+
+  Serial.print(bmp.readTemperature());
+    Serial.print("\t");
+
+  Serial.print(bmp.readPressure());
+    Serial.print("\t");
+
+  Serial.print(altura_atual);
+  Serial.print("\t");
+
+  Serial.print(filteredAltitude(altura_atual));
+    Serial.print("\t");
+
+  Serial.print(bmp.readSealevelPressure());
+    Serial.print("\t");
+
+  Serial.print(bmp.readAltitude(101500));
+    Serial.print("\n");
+}
+
+
+float filteredAltitude(const float altura_atual){
+  if (index == 10) {
   index = 0;
   }
   leituras[index] = altura_atual;
   index++;
-  float num = (leituras[0]+leituras[1]+leituras[2]+leituras[3]+leituras[4])/5;
-  return num;
-}
-void loop(){
- float altura_atual = bmp.readAltitude();
-
-  Serial.print(bmp.readTemperature());
-    Serial.print("\t");
-  Serial.print(bmp.readPressure());
-    Serial.print("\t");
-  Serial.print(altura_atual - alt);
-  Serial.print("\t");
-  Serial.print(filteredAltitude(altura_atual) - alt);
-    Serial.print("\t");
-  Serial.print(bmp.readSealevelPressure());
-    Serial.print("\t");
-  Serial.print(bmp.readAltitude(101500));
-    Serial.print("\n");
-  
-
+  float num = 0;
+  for (int i = 0; i < index; i++) {
+  num += leituras[i];
+  }
+  return num/index;
 }
