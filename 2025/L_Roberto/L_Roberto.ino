c96ecf751635f2507883be9605e1852e8fff72ae
Luis <roberto4luis1@gmail.com>
2025-03-07 14:43:31 -0300

    a

--- 2025/L_Roberto/L_Roberto.ino
+++ 2025/L_Roberto/L_Roberto.ino
@@ -1,21 +1,21 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
-float alt;
+float alt = 0;
 float leituras[5] = {0};
 int index = 0;
 void setup(){  
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-    Serial.println("\nTemperatura | Pressão | Altitude | Altitude Filtrada | Pressão Nivel do Mar | Altitude Real.");
+    Serial.println("\nTemperatura | Pressão | Altura | Altura Filtrada | Pressão Nivel do Mar | Altitude Real.");
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
   }
-  alt = 0;
   for (int i = 0; i < 100; i++) 
     alt += bmp.readAltitude();
   alt = alt/100 ;
 }
+
 float filteredAltitude(){
   if (index == 5) {
   index = 0;
@@ -23,16 +23,17 @@ float filteredAltitude(){
   leituras[index] = bmp.readAltitude();
   index++;
   float num = (leituras[0]+leituras[1]+leituras[2]+leituras[3]+leituras[4])/5;
-  return num - alt;
+  return num;
 }
 void loop(){
+ 
   Serial.print(bmp.readTemperature());
     Serial.print("\t");
   Serial.print(bmp.readPressure());
     Serial.print("\t");
-  Serial.print(leituras[index-1]);
+  Serial.print(leituras[index-1] - alt);
   Serial.print("\t");
-  Serial.print(filteredAltitude());
+  Serial.print(filteredAltitude() - alt);
     Serial.print("\t");
   Serial.print(bmp.readSealevelPressure());
     Serial.print("\t");
