c610b1276c01fbddc6e802a296e41406c12276e1
cesarafcm <87275058+cesarafcm@users.noreply.github.com>
2025-03-07 14:49:26 -0300

    Update C_Augusto.ino

--- 2025/C_Augusto/C_Augusto.ino
+++ 2025/C_Augusto/C_Augusto.ino
@@ -1,8 +1,12 @@
 #include <Adafruit_BMP085.h>
+#include <SPI.h>
+#include <SD.h>
 
 Adafruit_BMP085 bmp;
 
 #define AMOSTRAS 20
+
+const int chipSelect = 10;
 float leituras[AMOSTRAS];
 float leituras2[AMOSTRAS];
 float soma = 0;
@@ -23,13 +27,27 @@ void setup() {
   }
   altitudeTarada /= 10.0;
 
-
   Serial.println("Temperatura\tPressao\tAltitude\tNivel do mar\tAltitude Filtrada\tAltura");
+
+  while (!Serial);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("1. is a card inserted?");
+    Serial.println("2. is your wiring correct?");
+    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
+    Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
+    while (true);
+  }
+
+  Serial.println("initialization done.");
 }
 
 void loop() {
   float altitude = bmp.readAltitude();
-  float altura = altitude - altitudeTarada ;
+  float altura = altitude - altitudeTarada;
   soma = soma - leituras[indice] + altura;
   leituras[indice] = altura;
   indice = (indice + 1) % AMOSTRAS;
@@ -39,7 +57,7 @@ void loop() {
   soma2 = soma2 - leituras2[indice1] + altitudeFiltrada;
   leituras2[indice1] = altitudeFiltrada;
   indice1 = (indice1 + 1) % AMOSTRAS;
-  float altitudeFiltrada2 = soma2/AMOSTRAS;
+  float altitudeFiltrada2 = soma2 / AMOSTRAS;
 
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
@@ -54,4 +72,21 @@ void loop() {
   Serial.print(altitudeFiltrada);
   Serial.print("\t");
   Serial.println(altura);
+
+  String dataString = "";
+  dataString += String(bmp.readTemperature()) + "\t";
+  dataString += String(bmp.readPressure()) + "\t";
+  dataString += String(altitude) + "\t";
+  dataString += String(bmp.readSealevelPressure()) + "\t";
+  dataString += String(altitudeFiltrada2) + "\t";
+  dataString += String(altitudeFiltrada) + "\t";
+  dataString += String(altura);
+
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+  } else {
+    Serial.println("error opening datalog.txt");
+  }
 }
