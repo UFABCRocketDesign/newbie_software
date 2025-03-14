0526db7c7b4dab4dcbc8a4b94b6d967918089bdc
rrzampieri <rrzampieri@hotmail.com>
2025-03-14 16:04:57 -0300

    Detector de Queda V2.4 (Final)
    Atualização de limpeza e organização da versão V2.3

--- 2025/R_Zampieri/R_Zampieri.ino
+++ 2025/R_Zampieri/R_Zampieri.ino
@@ -3,11 +3,11 @@
 #define tamanho 10
 
 Adafruit_BMP085 bmp;
-//Declarando variáveis e array
+
+//Declarando variáveis e arrays
 float tara = 0;
 float vetor[tamanho];
 float vetor2[tamanho];
-//float vetorqueda[2];
 int guia = 0;
 int guia2 = 0;
 int detectorqueda = 0;
@@ -33,88 +33,73 @@ void setup() {
   //
 
   Serial.print("alturapassada\t");
-  //Serial.print("Temperatura\t");
-  //Serial.print("Pressão\t");
   Serial.print("Altitude Sem Filtro\t");
   Serial.print("Altitude Filtrada\t");
   Serial.print("Altitude Filtrada 2\t");
-  //Serial.print("Pressão no Nível do Mar\t");
-  //Serial.print("Altitude Real\t");
   Serial.print("Detector de Queda\t");
+  Serial.print("Temperatura\t");
+  Serial.print("Pressão\t");
+  Serial.print("Pressão no Nível do Mar\t");
+  Serial.print("Altitude Real\t");
   Serial.println();
 }
 
 void loop() {
 
-  //atualização dos valores gravados
-  vetor[guia] = bmp.readAltitude() - tara;
+  //FILTROS
+  vetor[guia] = bmp.readAltitude() - tara;  //setup do filtro 1
   if (guia < tamanho - 1) {
     guia += 1;
   } else {
     guia = 0;
   }
 
-  altitude_filtrada = 0;
-  for (int i = 0; i < tamanho; i += 1) {
+  altitude_filtrada = 0;                  //reset da altitude pra usar no filtro 1
+  for (int i = 0; i < tamanho; i += 1) {  //cálculo do filtro 1
     altitude_filtrada += vetor[i];
   }
 
-  altitude_filtrada /= tamanho;
+  altitude_filtrada /= tamanho;  //output do filtro 1
 
-  vetor2[guia2] = altitude_filtrada;
+  vetor2[guia2] = altitude_filtrada;  //setup do filtro 2
   if (guia2 < tamanho - 1) {
     guia2 += 1;
   } else {
     guia2 = 0;
   }
 
-  altitude_filtrada2 = 0;
-  for (int i = 0; i < tamanho; i += 1) {
+  altitude_filtrada2 = 0;                 //reset da altitude pra usar no filtro 2
+  for (int i = 0; i < tamanho; i += 1) {  //cálculo do filtro 2
     altitude_filtrada2 += vetor2[i];
   }
-  altitude_filtrada2 /= tamanho;
+  altitude_filtrada2 /= tamanho;  //output do filtro 2
 
-  //SEGUNDA TENTATIVA DE DETECTOR DE QUEDA AQUI
-  if (altitude_filtrada2 < alturapassada) {  //AQUI ELE COMPARA A ALTITUDE ATUAL COM A ANTERIOR
+  //DETECTOR DE QUEDA
+  if (altitude_filtrada2 < alturapassada) {  //Comparação da altitude atual pós-filtros com a altitude anterior ("alturapassada")
     detectorqueda = 1;
   } else {
     detectorqueda = 0;
   }
-  alturapassada = altitude_filtrada2;  //AQUI ELE ARMAZENA A ALTITUDE ATUAL PRA USAR NO PRÓXIMO LOOP COMO alturapassada
-
-  /*
-  vetorqueda[alturapassada] = altitude_filtrada2;
-  if (alturapassada < 1) {
-    alturapassada += 1;
-  } else {
-    alturapassada = 0;
-  }
-
-  if (vetorqueda[0] < vetorqueda[1]) {
-    detectorqueda = 1;
-  } else {
-    detectorqueda = 0;
-  }
-*/
+  alturapassada = altitude_filtrada2;  //Armazenamento da altitude atual para usar como "alturapassada" no próximo loop
 
   //print dos valores medidos
-  //Serial.print(bmp.readTemperature());
-  //Serial.print("\t");
-  //Serial.print(bmp.readPressure());
-  //Serial.print("\t");
   Serial.print(alturapassada);
-  Serial.print("\t");  
+  Serial.print("\t");
   Serial.print(vetor[guia]);
   Serial.print("\t");
   Serial.print(altitude_filtrada);
   Serial.print("\t");
   Serial.print(altitude_filtrada2);
   Serial.print("\t");
-  //Serial.print(bmp.readSealevelPressure());
-  //Serial.print("\t");
-  //Serial.print(bmp.readAltitude(101500));
-  //Serial.print("\t");
   Serial.print(detectorqueda);
+  Serial.print("\t");
+  Serial.print(bmp.readTemperature());
+  Serial.print("\t");
+  Serial.print(bmp.readPressure());
+  Serial.print("\t");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.print("\t");
+  Serial.print(bmp.readAltitude(101500));
   Serial.print("\t");
   Serial.println();
 }
