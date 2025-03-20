c269c875462896f24c227f47253a93358400b815
leoSartori1 <leosartori25@gmail.com>
2025-03-20 16:04:16 -0300

    Update L_Sartori.ino

--- 2025/L_Sartori/L_Sartori.ino
+++ 2025/L_Sartori/L_Sartori.ino
@@ -1,6 +1,17 @@
 #include <Adafruit_BMP085.h>
 #include <SPI.h>
-#include <SD.h>
+#include <SD.h>]
+#ifdef ARDUINO_AVR_MEGA2560
+#define SD_CS_PIN 53
+#else
+#define SD_CS_PIN 10
+#endif // ARDUINO_AVR_MEGA2560
+
+#define IGN_1 36  /*act1*/
+#define IGN_2 61  /*act2*/
+#define IGN_3 46  /*act3*/
+#define IGN_4 55  /*act4*/
+
 
 Adafruit_BMP085 bmp;
 #define N 3
