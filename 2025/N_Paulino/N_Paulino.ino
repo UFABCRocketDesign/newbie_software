fc4274bc46671a70c8078718835084ad935772ed
Natanael Paulino <natanael.silva27ns@gmail.com>
2025-07-10 17:50:00 -0300

    Update N_Paulino.ino

--- 2025/N_Paulino/N_Paulino.ino
+++ 2025/N_Paulino/N_Paulino.ino
@@ -1,6 +1,6 @@
 
 #include <Adafruit_BMP085.h>
-
+Adafruit_BMP085 bmp;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -17,7 +17,7 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(10);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(10); 
+  delay(2000); 
   Serial.print("Temperature = ");
     Serial.print(bmp.readTemperature());
     Serial.println(" *C");
