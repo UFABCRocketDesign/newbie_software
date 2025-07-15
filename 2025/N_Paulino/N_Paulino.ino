8dd55af6322af77d6ec8f7d80032541340b498d1
Natanael Paulino <natanael.silva27ns@gmail.com>
2025-07-15 19:24:55 -0300

    Update N_Paulino.ino

--- 2025/N_Paulino/N_Paulino.ino
+++ 2025/N_Paulino/N_Paulino.ino
@@ -1,34 +1,31 @@
-
 #include <Adafruit_BMP085.h>
+
 Adafruit_BMP085 bmp;
-float alt_zero = 0
-// the setup function runs once when you press reset or power the board
+float alt_zero = 0.0;              
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(115200);
-  if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
-   float soma = 0;
-  for (int i=0 ; i>10 ; i++) { soma += bmp.readAltitude();}
-  alt_zero = soma/10};
+  Serial.begin(115200);
+  if (!bmp.begin()) {           
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) { }               
+  }
+  float soma = 0;
+  for (int i = 0; i < 10; i++) {
+    soma += bmp.readAltitude();
+                      
+  }
+  alt_zero = soma/10.0;
 }
 
-// the loop function runs over and over again forever
 void loop() {
+  Serial.print(bmp.readTemperature());       
+  Serial.print('\t');
+  Serial.print(bmp.readPressure());          
+  Serial.print('\t');
+  Serial.print(bmp.readSealevelPressure());  
+  Serial.print('\t');
+  Serial.print(bmp.readAltitude(101500) - alt_zero);
+  Serial.println();
 
-    Serial.print(bmp.readTemperature());
-    Serial.print("\t");
-    Serial.print(bmp.readPressure());
-    Serial.print("\t");
-    Serial.print(bmp.readAltitude());
-    Serial.print("\t");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.print("\t");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.print("\t");
-    Serial.println();
-
-                     // wait for a second
 }
