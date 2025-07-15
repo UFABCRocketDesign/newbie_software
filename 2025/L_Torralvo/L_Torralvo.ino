b2fa35ae1521666a1a453df0a47129a81cb69b9c
dionesz <159745596+dionesz@users.noreply.github.com>
2025-07-15 19:32:42 -0300

    Update L_Torralvo.ino

--- 2025/L_Torralvo/L_Torralvo.ino
+++ 2025/L_Torralvo/L_Torralvo.ino
@@ -7,25 +7,17 @@ void setup() {
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
+  /*Coluna*/
+  Serial.println("Temperature\tPressure\tAltitude\tPressure at sealevel\tReal altitude");
   }
 }
 
 void loop() {
-
-    /*cabeçalho!*/
-    Serial.println("Temperature\tPressure\tAltitude\tPressure at sealevel\tReal altitude");
-
-    /*Valores*/
-    Serial.print(bmp.readTemperature()); Serial.print("\t");
-    
-    Serial.print(bmp.readPressure()); Serial.print("\t");
-
-    Serial.print(bmp.readAltitude()); Serial.print("\t");
-
-    Serial.print(bmp.readSealevelPressure()); Serial.print("\t");
-
-    Serial.println(bmp.readAltitude(101500)); Serial.print("\t");
-    
-    Serial.println();
-    delay(500);                
+  /*Valores*/
+  Serial.print(bmp.readTemperature()); Serial.print("\t");
+  Serial.print(bmp.readPressure()); Serial.print("\t");
+  Serial.print(bmp.readAltitude()); Serial.print("\t");
+  Serial.print(bmp.readSealevelPressure()); Serial.print("\t");
+  Serial.print(bmp.readAltitude(101500)); Serial.print("\t");
+  Serial.println();               
 }
