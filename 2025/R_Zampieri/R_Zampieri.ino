b293ef42b426c2bd29693e4680129bdffe014697
rrzampieri <rrzampieri@hotmail.com>
2025-07-17 16:29:53 -0300

    Reorganização do Código

--- 2025/R_Zampieri/R_Zampieri.ino
+++ 2025/R_Zampieri/R_Zampieri.ino
@@ -7,19 +7,20 @@
 
 #define tamanho 10  //Usado pro filtro de dados do BMP
 
+//Pinos de Ignitores dos Paraquedas
 #define IGN_1 36 /*act1*/
 #define IGN_2 61 /*act2*/
 #define IGN_3 46 /*act3*/
 #define IGN_4 55 /*act4*/
+
+#include <SPI.h>  //Usado no DataLogger
+#include <SD.h>   //Usado no DataLogger
 
 Adafruit_BMP085 bmp;  //Sensor BMP
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(54321);
 L3G gyro;
-
-#include <SPI.h>  //Usado no DataLogger
-#include <SD.h>   //Usado no DataLogger
 
 //Declarando variáveis e arrays pro BMP
 float tara = 0;
@@ -32,8 +33,12 @@ float alturapassada = 0;
 float altura_filtrada = 0;
 float altura_filtrada2 = 0;
 
+//Declaração pro detector de queda
 bool queda = false;
+
+//Declaração pro relógio (millis)
 float tempo = 0;
+
 //Declarações pro SD DataLogger
 const int chipSelect = 53;
 int lognumber = 0;
@@ -42,27 +47,24 @@ String nomelog = "ZAMP";
 String zerospacetext;
 int zerospacelength;
 
-//Declarações pro Paraquedas
+/*------DECLARAÇÕES PRO PARAQUEDAS------*/
+//Estado de cada paraquedas
 int paraquedas1armado = 0;
 int paraquedas2armado = 0;
 int paraquedas3armado = 0;
 int paraquedas4armado = 0;
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillisPRQ1 = 0;  // will store last time LED was updated
+//Tempo de ativação de cada paraquedas
+unsigned long previousMillisPRQ1 = 0; 
 unsigned long previousMillisPRQ2 = 0;
 unsigned long previousMillisPRQ3 = 0;
 unsigned long previousMillisPRQ4 = 0;
-
-// constants won't change:
-const long intervalIGN = 5000;  // interval at which to blink (milliseconds)
+//Intervalo entre os paraquedas
+const long intervalIGN = 5000;
 const long intervalPRQ2 = 2000;
 const long intervalPRQ4 = 2000;
 
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 void setup() {
-  //COMEÇO DO SETUP DATALOGGER
+  /*------SETUP DATALOGGER------*/
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
   // wait for Serial Monitor to connect. Needed for native USB port boards only:
@@ -98,10 +100,9 @@ void setup() {
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   File dataFile = SD.open(nomearquivo, FILE_WRITE);
-
   //FIM DO SETUP DATALOGGER
 
-  //COMEÇO DO SETUP BMP
+  /*------SETUP BMP------*/
   //Inicialização do sensor
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
@@ -115,6 +116,7 @@ void setup() {
   }
   tara /= 10;
   //
+
   Serial.print("tempo\t");
   Serial.print("alturapassada\t");
   Serial.print("Altura Sem Filtro\t");
@@ -136,6 +138,7 @@ void setup() {
   Serial.println();
   //FIM DO SETUP BMP
 
+  /*------SETUP ARMAZENAMENTO NO ARQUIVO------*/
   if (dataFile) {
     dataFile.print("tempo\t");
     dataFile.print("alturapassada\t");
@@ -166,12 +169,13 @@ void setup() {
     Serial.println("Erro ao abrir" + nomearquivo);
   }
 
+  /*------PINOS DO PARAQUEDAS------*/
   pinMode(IGN_1, OUTPUT);
   pinMode(IGN_2, OUTPUT);
   pinMode(IGN_3, OUTPUT);
   pinMode(IGN_4, OUTPUT);
 
-  /* Initialise the sensor */
+  /*------INICIALIZAÇÃO ACELERÔMETRO------*/
   if (!accel.begin()) {
     /* There was a problem detecting the ADXL345 ... check your connections */
     Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
@@ -182,6 +186,7 @@ void setup() {
 
   Wire.begin();
 
+  /*------INICIALIZAÇÃO GIROSCÓPIO------*/
   if (!gyro.init()) {
     Serial.println("Failed to autodetect gyro type!");
     while (1)
@@ -190,7 +195,7 @@ void setup() {
 
   gyro.enableDefault();
 
-  /* Initialise the sensor */
+  /*------INICIALIZAÇÃO MAGNETÔMETRO------*/
   if (!mag.begin()) {
     /* There was a problem detecting the HMC5883 ... check your connections */
     Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
@@ -198,16 +203,27 @@ void setup() {
       ;
   }
 }
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 void loop() {
+
+  /*------COLETA DE DADOS------*/
   unsigned long currentMillis = millis();
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open(nomearquivo, FILE_WRITE);
 
   tempo = currentMillis / 1000.0;
 
-  //COMEÇO DA SEÇÃO DO SENSOR BMP
+  // Get a new sensor event (Acelerômetro)
+  sensors_event_t event;
+  accel.getEvent(&event);
+
+  gyro.read();
+
+  // Get a new sensor event (Magnetômetro)
+  sensors_event_t eventmag;
+  mag.getEvent(&eventmag);
+
+  //*------SENSOR BMP------*/
   //FILTROS
   vetor[guia] = bmp.readAltitude() - tara;  //setup do filtro 1
   if (guia < tamanho - 1) {
@@ -236,7 +252,7 @@ void loop() {
   }
   altura_filtrada2 /= tamanho;  //output do filtro 2
 
-  //DETECTOR DE QUEDA
+  /*------DETECÇÃO DE QUEDA------*/
   if (altura_filtrada2 < alturapassada) {  //Comparação da altura atual pós-filtros com a altura anterior ("alturapassada")
     detectorqueda += 1;
   } else {
@@ -247,46 +263,7 @@ void loop() {
   }
   alturapassada = altura_filtrada2;  //Armazenamento da altura atual para usar como "alturapassada" no próximo loop
 
-  /* Get a new sensor event */
-  sensors_event_t event;
-  accel.getEvent(&event);
-
-  gyro.read();
-
-  /* Get a new sensor event */
-  sensors_event_t eventmag;
-  mag.getEvent(&eventmag);
-
-  // Armazenamento dos valores na dataString
-  String dataString = "";
-  dataString += String(tempo) + "\t";
-  dataString += String(alturapassada) + "\t";
-  dataString += String(vetor[guia]) + "\t";
-  dataString += String(altura_filtrada) + "\t";
-  dataString += String(altura_filtrada2) + "\t";
-  dataString += String(detectorqueda) + "\t";
-  dataString += String(bmp.readTemperature()) + "\t";
-  dataString += String(bmp.readPressure()) + "\t";
-  dataString += String(paraquedas1armado) + "\t";
-  dataString += String(paraquedas2armado) + "\t";
-  dataString += String(paraquedas3armado) + "\t";
-  dataString += String(paraquedas4armado) + "\t";
-  dataString += String(event.acceleration.x) + "\t";
-  dataString += String(event.acceleration.y) + "\t";
-  dataString += String(event.acceleration.z) + "\t";
-  dataString += String((int)gyro.g.x) + "\t";
-  dataString += String((int)gyro.g.y) + "\t";
-  dataString += String((int)gyro.g.z) + "\t";
-  dataString += String(eventmag.magnetic.x) + "\t";
-  dataString += String(eventmag.magnetic.y) + "\t";
-  dataString += String(eventmag.magnetic.z) + "\t";
-  //Print da dataString
-  Serial.println(dataString);
-
-  //FIM DA SEÇÃO DO SENSOR BMP
-
-  //COMEÇO DA SEÇÃO DO PARAQUEDAS
-
+  /*------ACIONAMENTO DE PARAQUEDAS------*/
   // Paraquedas 1
   if ((queda) && (paraquedas1armado == 0)) {
     digitalWrite(IGN_1, HIGH);
@@ -344,10 +321,37 @@ void loop() {
     digitalWrite(IGN_4, LOW);
     paraquedas4armado = 2;
   }
-  //FIM DA SEÇÃO DO PARAQUEDAS
 
-  //COMEÇO DA SEÇÃO DO DATALOGGER
+  /*------ARMAZENAMENTO DE DADOS------*/
+  //Abre o arquivo
+  File dataFile = SD.open(nomearquivo, FILE_WRITE);
+  // Armazenamento dos valores na dataString
+  String dataString = "";
+  dataString += String(tempo) + "\t";
+  dataString += String(alturapassada) + "\t";
+  dataString += String(vetor[guia]) + "\t";
+  dataString += String(altura_filtrada) + "\t";
+  dataString += String(altura_filtrada2) + "\t";
+  dataString += String(detectorqueda) + "\t";
+  dataString += String(bmp.readTemperature()) + "\t";
+  dataString += String(bmp.readPressure()) + "\t";
+  dataString += String(paraquedas1armado) + "\t";
+  dataString += String(paraquedas2armado) + "\t";
+  dataString += String(paraquedas3armado) + "\t";
+  dataString += String(paraquedas4armado) + "\t";
+  dataString += String(event.acceleration.x) + "\t";
+  dataString += String(event.acceleration.y) + "\t";
+  dataString += String(event.acceleration.z) + "\t";
+  dataString += String((int)gyro.g.x) + "\t";
+  dataString += String((int)gyro.g.y) + "\t";
+  dataString += String((int)gyro.g.z) + "\t";
+  dataString += String(eventmag.magnetic.x) + "\t";
+  dataString += String(eventmag.magnetic.y) + "\t";
+  dataString += String(eventmag.magnetic.z) + "\t";
+  //Print da dataString
+  Serial.println(dataString);
 
+  /*------SALVA NO ARQUIVO------*/
   // if the file is available, write to it:
   if (dataFile) {
     dataFile.println(dataString);
@@ -357,5 +361,4 @@ void loop() {
   else {
     Serial.println("Erro ao abrir" + nomearquivo);
   }
-  //FIM DA SEÇÃO DO DATALOGGER
 }
