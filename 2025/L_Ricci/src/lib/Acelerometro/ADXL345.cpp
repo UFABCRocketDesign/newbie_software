d8513bfb634b7adeeb987f2f79db55c2552855f9
Lucas Ricci <llricci@protonmail.com>
2025-07-22 18:32:18 -0300

    Biblioteca ADXL345

--- 2025/L_Ricci/src/lib/Acelerometro/ADXL345.cpp
+++ 2025/L_Ricci/src/lib/Acelerometro/ADXL345.cpp
@@ -13,7 +13,7 @@ ADXL345::ADXL345(uint8_t range) {
     scale = 64.0;
     break;
   default:
-    scale = 128.0;
+    scale = 256.0;
   }
 }
 
@@ -32,16 +32,21 @@ bool ADXL345::readAll() {
 
   Wire.requestFrom(0x53, 6);
   if (Wire.available() == 6) {
-    X0 = Wire.read() | Wire.read() << 8;
+    X0 = Wire.read();
     X1 = Wire.read();
-    Y0 = Wire.read() | Wire.read() << 8;
+    Y0 = Wire.read();
     Y1 = Wire.read();
-    Z0 = Wire.read() | Wire.read() << 8;
+    Z0 = Wire.read();
     Z1 = Wire.read();
   }
 
+  X1 = X1 << 8;
   X_out = X0 + X1;
+
+  Y1 = Y1 << 8;
   Y_out = Y0 + Y1;
+
+  Z1 = Z1 << 8;
   Z_out = Z0 + Z1;
 
   x = X_out / scale;
