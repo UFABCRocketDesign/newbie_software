2fb7f3e74616a129fac33cc7e3b230407d8ec144
Natanael Paulino <natanael.silva27ns@gmail.com>
2025-08-14 16:55:16 -0300

    Update N_Paulino.ino

--- 2025/N_Paulino/N_Paulino.ino
+++ 2025/N_Paulino/N_Paulino.ino
@@ -2,7 +2,8 @@
 
 Adafruit_BMP085 bmp;
 float alt_zero = 0;              
-
+float vetor_num [10];
+int ind_vetor = 0;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
@@ -19,12 +20,25 @@ void setup() {
 }
 
 void loop() {
+  float real_alt = bmp.readAltitude() - alt_zero;
+  
+  vetornum [ind_vetor] = real_alt;
+  ind_vetor ++;
+  if (ind_vetor >= 10) ind_vetor = 0;
+    
+  float valor_vet = 0;
+  for (int i = 0; i < 10 ; i++) {
+   valor_vet += vetor_num [i];
+  }
+  valor_flt = valor_vet / 10;
+
   Serial.print(bmp.readTemperature());       
   Serial.print('\t');
   Serial.print(bmp.readPressure());          
   Serial.print('\t');
-  Serial.print(bmp.readAltitude() - alt_zero);
-  Serial.print(bmp.readSealevelPressure());  
+  Serial.print(real_alt);
+  Serial.print('\t');
+  Serial.print(valor_flt);  
   Serial.print('\t');
   Serial.println();
 
