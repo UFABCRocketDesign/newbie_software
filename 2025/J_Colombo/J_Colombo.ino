49b845e409641822ef290974f611771447cc1174
JulColombo <101673490+JulColombo@users.noreply.github.com>
2025-08-25 18:37:16 -0300

    0_tabula

--- 2025/J_Colombo/J_Colombo.ino
+++ 2025/J_Colombo/J_Colombo.ino
@@ -13,19 +13,28 @@ void setup() {
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
+
+  Serial.print("Temperature (*C)  ");
+  Serial.print("/t");
+  Serial.print("Pressure (Pa)  ");
+  Serial.print("/t");
+  Serial.print("Altitude (m)  ");
+  Serial.print("/t");
+  Serial.print("Pressure at sealevel (calculated in Pa) = ");
+  Serial.print("/t");
+  Serial.println("Real altitude (m)  ");
+
   }
 }
   
 void loop() {
-    Serial.print("Temperature (*C)  ");
-    Serial.print("Pressure (Pa)  ");
-    Serial.print("Altitude (m)  ");
-    Serial.print("Pressure at sealevel (calculated in Pa) = ");
-    Serial.println("Real altitude (m)  ");
-
     Serial.print(bmp.readTemperature());
+    Serial.print("/t");
     Serial.print(bmp.readPressure());
+    Serial.print("/t");
     Serial.print(bmp.readAltitude());
+    Serial.print("/t");
     Serial.print(bmp.readSealevelPressure());
+    Serial.print("/t");
     Serial.println(bmp.readAltitude(101500));
 }
