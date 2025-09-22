b423ae330d24509759577d5f44684ec60fc6caff
RaphaFDAS <playraphael@gmail.com>
2025-09-22 16:59:56 -0300

    porque erro????

--- 2025/R_Santos/R_Santos.ino
+++ 2025/R_Santos/R_Santos.ino
@@ -3,15 +3,17 @@
 Adafruit_BMP085 bmp;
   
 void setup() {
+  float solo = bmp.readAltitude();
+  Serial.println("Temperature(C)|Pressure(PA)|Altitude(Pa)|Pressure at Sea Level(PA)|Real Altitude(m)|Altura Atual(m)");
   Serial.begin(115200);
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
-  Serial.println("Temperature(C)|Pressure(PA)|Altitude(Pa)|Pressure at Sea Level(PA)|Real Altitude(m)");
   }
 }
   
 void loop() {
+    float altitude = bmp.readAltitude() - solo;
     Serial.print(bmp.readTemperature());
     Serial.print("\t");
     Serial.print(bmp.readPressure());
@@ -21,8 +23,8 @@ void loop() {
     Serial.print(bmp.readSealevelPressure());
     Serial.print("\t");
     Serial.print(bmp.readAltitude(101500));
-    
-    Serial.println();
-    delay(500);
+    Serial.print("\t");
+    Serial.println(altitude);
+
 }
 
