95bbe6af6b85352c41d53f0f52ff6125b5ca1a40
RaphaFDAS <playraphael@gmail.com>
2025-09-22 17:10:38 -0300

    att

--- 2025/R_Santos/R_Santos.ino
+++ 2025/R_Santos/R_Santos.ino
@@ -2,33 +2,29 @@
 
 Adafruit_BMP085 bmp;
 
-float solo = 0; ///variaveis
+float solo = 0;  ///variaveis
 
-void setup() { ///inicialização
+void setup() {  ///inicialização
   Serial.begin(115200);
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
-
-  solo = bmp.readAltitude(); /// cabeçario e leitura de nivel de solo
-  Serial.println("Temperature(C)|Pressure(PA)|Altitude(Pa)|Pressure at Sea Level(PA)|Real Altitude(m)|Altura Atual(m)");
-
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
-}
-  
-void loop() { ///leituras
-    float altitude = bmp.readAltitude() - solo;
-    Serial.print(bmp.readTemperature());
-    Serial.print("\t");
-    Serial.print(bmp.readPressure());
-    Serial.print("\t");
-    Serial.print(bmp.readAltitude());
-    Serial.print("\t");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.print("\t");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.print("\t");
-    Serial.println(altitude);
-
+    solo = bmp.readAltitude();  /// cabeçario e leitura de nivel de solo
+    Serial.println("Temperature(C)|Pressure(PA)|Altitude(Pa)|Pressure at Sea Level(PA)|Real Altitude(m)|Altura Atual(m)");
 }
 
+void loop() {  ///leituras
+  float altitude = bmp.readAltitude() - solo;
+  Serial.print(bmp.readTemperature());
+  Serial.print("\t");
+  Serial.print(bmp.readPressure());
+  Serial.print("\t");
+  Serial.print(bmp.readAltitude());
+  Serial.print("\t");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.print("\t");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.print("\t");
+  Serial.println(altitude);
+}
