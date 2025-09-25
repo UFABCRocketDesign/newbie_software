60f018eeb5d31d14792ba73184ba99f9ee4e87a5
RaphaFDAS <playraphael@gmail.com>
2025-09-25 16:22:08 -0300

    atualização de filtro de altitude

--- 2025/R_Santos/R_Santos.ino
+++ 2025/R_Santos/R_Santos.ino
@@ -1,9 +1,14 @@
 #include <Adafruit_BMP085.h>
+#include <stdio.h>
 
 Adafruit_BMP085 bmp;
 
 float solo = 0;
-float mediaA = 0;
+int k = 0;
+int K = 0;
+float altitudeArray[10];
+float mediaAlt = 0;
+float somaAlt = 0;
 
 void setup() {  ///inicialização
   Serial.begin(115200);
@@ -12,7 +17,7 @@ void setup() {  ///inicialização
     while (1) {}
   }
   for(int i= 0; i < 10; i++){
-    solo = bmp.readAltitude() + solo;
+    solo += bmp.readAltitude();
   }
   solo = solo/10;
 
@@ -20,13 +25,14 @@ void setup() {  ///inicialização
   Serial.println("Temperature(C)|Pressure(PA)|Altitude(Pa)|Pressure at Sea Level(PA)|Real Altitude(m)|Altura Atual(m)");
 }
 
-void loop() {  ///leituras
-  mediaA = 0;
-  for(int i= 0; i < 20; i++){
-    float altitude = bmp.readAltitude() - solo;
-    mediaA = altitude + mediaA;
-  }
-  mediaA = mediaA/20;
+void loop() {
+  float altitude = bmp.readAltitude() - solo;   //Essa parte do codigo vai tirar apartir da 11 interação o valor k = 0| k++ da array e colocar um novo
+  somaAlt -= altitudeArray[k];              //Retira o ultimo valor usado
+  somaAlt += altitude;                      //soma que vai ser feita todo ciclo para poder fazer a media
+  altitudeArray[k] += altitude;
+  if(K < 10) K++;                           //contador que vai parar em 10 -> limite da array [10]
+  mediaAlt = somaAlt / K;       
+  k = k + 1 % 10;                           //contador circular de interações no array (k)
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
   Serial.print(bmp.readPressure());
@@ -37,5 +43,5 @@ void loop() {  ///leituras
   Serial.print("\t");
   Serial.print(bmp.readAltitude(101500));
   Serial.print("\t");
-  Serial.println(mediaA);
+  Serial.println(mediaAlt);
 }
