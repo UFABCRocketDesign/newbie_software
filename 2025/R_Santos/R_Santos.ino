6a89e64c427df1ba69d547bfd02b57864c48c9b7
RaphaFDAS <playraphael@gmail.com>
2025-10-03 19:05:02 -0300

    Update R_Santos.ino

--- 2025/R_Santos/R_Santos.ino
+++ 2025/R_Santos/R_Santos.ino
@@ -4,10 +4,10 @@
 Adafruit_BMP085 bmp;
 
 float solo = 0;
-int k = 0;
-float altitudeArray[10];
-float mediaAlt = 0;
-float somaAlt = 0;
+int idx[3] = {};
+float altitudeArray[3][10];
+float mediaAlt[3] = {};
+float somaAlt[3] = {};
 
 void setup() {  ///inicialização
   Serial.begin(115200);
@@ -25,12 +25,12 @@ void setup() {  ///inicialização
 }
 
 void loop() {
-  float altitude = bmp.readAltitude() - solo;   //Essa parte do codigo vai tirar apartir da 11 interação o valor k = 0| k++ da array e colocar um novo
-  somaAlt -= altitudeArray[k];              //Retira o ultimo valor usado
-  somaAlt += altitude;                      //soma que vai ser feita todo ciclo para poder fazer a media
-  altitudeArray[k] = altitude;
-  mediaAlt = somaAlt / 10;       
-  k = (k + 1) % 10;                           //contador circular de interações no array (k)
+  float altitude = bmp.readAltitude() - solo;   //Essa parte do codigo vai tirar apartir da 11 interação o valor idx = 0| idx++ da array e colocar um novo
+  somaAlt[0] -= altitudeArray[0][idx[0]];              //Retira o ultimo valor usado
+  somaAlt[0] += altitude;                      //soma que vai ser feita todo ciclo para poder fazer a media
+  altitudeArray[0][idx[0]] = altitude;
+  mediaAlt[0] = somaAlt[0] / 10;       
+  idx[0] = (idx[0] + 1) % 10;               //contador circular de interações no array (idx)
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
   Serial.print(bmp.readPressure());
@@ -41,7 +41,7 @@ void loop() {
   Serial.print("\t");
   Serial.print(bmp.readAltitude(101500));
   Serial.print("\t");
-  Serial.print(mediaAlt);
+  Serial.print(mediaAlt[0]);
   Serial.print('\t');
   Serial.println(altitude);
 }
