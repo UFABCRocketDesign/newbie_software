b297badba6e78e89b01274d9f4a90b2d849854a0
RaphaFDAS <playraphael@gmail.com>
2025-10-03 19:22:44 -0300

    Update R_Santos.ino

--- 2025/R_Santos/R_Santos.ino
+++ 2025/R_Santos/R_Santos.ino
@@ -4,9 +4,10 @@
 Adafruit_BMP085 bmp;
 
 float solo = 0;
+int i = 0;
 int idx[3] = {};
-float altitudeArray[3][10];
-float mediaAlt[3] = {};
+float alturaArray[3][10];
+float mediaAlt[3+1] = {}; //+1 porque ele amazena a altura entrando
 float somaAlt[3] = {};
 
 void setup() {  ///inicialização
@@ -21,27 +22,30 @@ void setup() {  ///inicialização
   solo = solo/10;
 
 /// cabeçario
-  Serial.println("Temperature(C)|Pressure(PA)|Altitude(Pa)|Pressure at Sea Level(PA)|Real Altitude(m)|Altura Atual(FILTRADA)(m)|Altura Atual(raw)(m)");
+  Serial.println("Temperature(C)|Pressure(PA)|Real Altitude(m)|Altura Atual(FILTRO 1)(m)| Altura Atual(FILTRO 2)(m)|Altura Atual(FILTRO 3)(m)|Altura Atual(raw)(m)");
 }
 
 void loop() {
-  float altitude = bmp.readAltitude() - solo;   //Essa parte do codigo vai tirar apartir da 11 interação o valor idx = 0| idx++ da array e colocar um novo
-  somaAlt[0] -= altitudeArray[0][idx[0]];              //Retira o ultimo valor usado
-  somaAlt[0] += altitude;                      //soma que vai ser feita todo ciclo para poder fazer a media
-  altitudeArray[0][idx[0]] = altitude;
-  mediaAlt[0] = somaAlt[0] / 10;       
-  idx[0] = (idx[0] + 1) % 10;               //contador circular de interações no array (idx)
+  float altura = bmp.readAltitude() - solo;   //Essa parte do codigo vai tirar apartir da 11 interação o valor idx = 0| idx++ da array e colocar um novo
+  mediaAlt[0] = altura;
+  for(int i = 0; i<3; i++){
+    somaAlt[i] -= alturaArray[i][idx[i]];              //Retira o ultimo valor usado
+    somaAlt[i] += mediaAlt[i];                      //soma que vai ser feita todo ciclo para poder fazer a media
+    alturaArray[i][idx[i]] = altura;
+    mediaAlt[i+1] = somaAlt[i] / 10;       
+    idx[i] = (idx[i] + 1) % 10;               //contador circular de interações no array (idx)
+  }
   Serial.print(bmp.readTemperature());
   Serial.print("\t");
   Serial.print(bmp.readPressure());
   Serial.print("\t");
   Serial.print(bmp.readAltitude());
   Serial.print("\t");
-  Serial.print(bmp.readSealevelPressure());
-  Serial.print("\t");
-  Serial.print(bmp.readAltitude(101500));
-  Serial.print("\t");
-  Serial.print(mediaAlt[0]);
+  Serial.print(mediaAlt[1]);
   Serial.print('\t');
-  Serial.println(altitude);
+  Serial.print(mediaAlt[2]);
+  Serial.print('\t');
+  Serial.print(mediaAlt[3]);
+  Serial.print('\t');
+  Serial.println(altura);
 }
