45dd6a7ab36be8739365c5cd9fff9b10b0991ddb
Victor Themoteo Mordachini <mordachini.v@gmail.com>
2026-03-16 16:50:13 -0300

    pisca_led_victor_v1
    altera delay entre  high / low

---
+++ 2026/V_Mordachini/V_Mordachini.ino
@@ -0,0 +1,17 @@
+// pisca_led Victor :)
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1500);                      
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(500);   
+  
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);      
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(500);         
+}
