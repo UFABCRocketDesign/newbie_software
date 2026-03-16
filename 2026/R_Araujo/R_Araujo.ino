2a2b07e17a3fa2595999a6c8d7d97cff720d3c11
XrodrigoZw <jrsdoliveira30@gmail.com>
2026-03-16 17:55:12 -0300

    R_led_bmp_2

--- 2026/R_Araujo/R_Araujo.ino
+++ 2026/R_Araujo/R_Araujo.ino
@@ -16,25 +16,23 @@ void loop() {
   delay(500);           
     Serial.print("Temperature = ");
     Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
+    Serial.print(" *C/ ");
     
     Serial.print("Pressure = ");
     Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
+    Serial.print(" Pa/ ");
     
     Serial.print("Altitude = ");
     Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
+    Serial.print(" meters/ ");
 
     Serial.print("Pressure at sealevel (calculated) = ");
     Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
-
+    Serial.print(" Pa/ ");
 
     Serial.print("Real altitude = ");
     Serial.print(bmp.readAltitude(101500));
     Serial.println(" meters");
     
-    Serial.println();
     delay(500);         
 }
