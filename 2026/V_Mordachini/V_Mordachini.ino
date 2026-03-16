80b4fe847b7d6364b3e0a6a8e309b45be69c86e2
Victor Themoteo Mordachini <mordachini.v@gmail.com>
2026-03-16 17:11:24 -0300

    pisca_led_bmp_v2
    implementa BMP085

--- 2026/V_Mordachini/V_Mordachini.ino
+++ 2026/V_Mordachini/V_Mordachini.ino
@@ -1,17 +1,45 @@
 // pisca_led Victor :)
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  
+
+  Serial.print("Temperatura: ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressao: ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Altitude: ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" m");
+  Serial.println("--------------------");
+  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  Serial.print("LED LIGADO :)"); 
   delay(1500);                      
-  digitalWrite(LED_BUILTIN, LOW);   
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("LED DESLIGADO :)");    
   delay(500);   
   
-  digitalWrite(LED_BUILTIN, HIGH);   
+  digitalWrite(LED_BUILTIN, HIGH);  
+  Serial.print("LED LIGADO :)");  
   delay(500);      
   digitalWrite(LED_BUILTIN, LOW); 
+  Serial.print("LED DESLIGADO :)"); 
   delay(500);         
 }
