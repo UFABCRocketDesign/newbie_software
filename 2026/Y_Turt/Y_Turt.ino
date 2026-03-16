263c620bada267de3f20a9fb4bcdce3f5d9d778e
YuBuTu <yuribturt@gmail.com>
2026-03-16 18:12:02 -0300

    Update Y_Turt.ino

--- 2026/Y_Turt/Y_Turt.ino
+++ 2026/Y_Turt/Y_Turt.ino
@@ -1,27 +1,26 @@
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
   }
+  Serial.println(" *C / Pa / meters / Pa / meters ");
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  Serial.print(" *C / Pa / meters / Pa / meters ");
-    Serial.print(bmp.readTemperature());
-    
-    Serial.print(bmp.readPressure());
-    
-    Serial.print(bmp.readAltitude());
-
-    Serial.print(bmp.readSealevelPressure());
-
-    Serial.print(bmp.readAltitude(101500));
-    
-    Serial.println();
+  Serial.print(bmp.readTemperature());
+  Serial.print("  ");
+  Serial.print(bmp.readPressure());
+  Serial.print("  ");
+  Serial.print(bmp.readAltitude());
+  Serial.print("  ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.print("  ");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.print("  ");
+  Serial.println();
 }
