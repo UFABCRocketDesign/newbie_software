3d999fb0e37c0f088a71a60f09d72d019fe5e934
rrzampieri <rrzampieri@hotmail.com>
2026-03-16 18:33:36 -0300

    Adição de GPS, tentando descobrir o que tá acontecendo

--- 2025/R_Zampieri/R_Zampieri.ino
+++ 2025/R_Zampieri/R_Zampieri.ino
@@ -152,7 +152,15 @@ TinyGPSPlus gps;
 
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
-unsigned long dados_totais = gps.passedChecksum() + gps.failedChecksum();
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
 #endif //GPS
 
 void setup() {
@@ -609,9 +617,9 @@ void loop() {
 
 //------------GPS------------
 #if USANDO_GPS
+smartDelay(1000);
 dataString += String(gps.location.lat(), 6) + "\t";
 dataString += String(gps.location.lng(), 6) + "\t";
-dataString += String(dados_totais) + "\t";
 #endif //GPS
 
   //Print da dataString
