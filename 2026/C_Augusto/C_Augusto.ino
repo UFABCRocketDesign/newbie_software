a3c2f5c0905b565de31bbac925a9dd5d9926979a
cesarafcm <87275058+cesarafcm@users.noreply.github.com>
2026-03-19 18:55:13 -0300

    Update C_Augusto.ino

--- 2026/C_Augusto/C_Augusto.ino
+++ 2026/C_Augusto/C_Augusto.ino
@@ -1,12 +1,45 @@
-void setup() {
-  // inicializa a funcao do pino do led 13
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+#include <Adafruit_BMP085.h>
 
-// loopzao pra repetir
+Adafruit_BMP085 bmp;
+  
+void setup() {
+  Serial.begin(9600);
+   // inicializa a funcao do pino do led 13
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  if (!bmp.begin()) {
+	Serial.println("bmp nao encontrado, verifique as conexoes");
+	while (1) {}
+  }
+}
+  
 void loop() {
+  
   digitalWrite(LED_BUILTIN, HIGH);  // muda o pino pra alta tensao
   delay(500);                      // segura 0.5 segundos
   digitalWrite(LED_BUILTIN, LOW);   // muda o pino pra baixa tensao
-  delay(500);                      // segura 0.5 segundos
+  delay(500);                      // segura 0.5 segundos  
+
+    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+    
+    Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+    
+    Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+
+    Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.println(" meters");
+    
+    Serial.println();
+    delay(500);
 }
