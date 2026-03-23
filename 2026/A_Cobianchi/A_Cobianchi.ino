080aae6da0098aa480b40d64fc33e9960739aeff
AlvaroCobianchi <alvarocobianchi1@gmail.com>
2026-03-23 18:19:37 -0300

    ARDUINO
    Removi a parte do LED pro codigo rodar mais rapido, alterei a parte do TAB pra um só pois no ultimo codigo eu tinha colocado dois sem querer e também adicionei uma parte no codigo de calculo de altitude.

--- 2026/A_Cobianchi/A_Cobianchi.ino
+++ 2026/A_Cobianchi/A_Cobianchi.ino
@@ -1,40 +1,45 @@
 #include <Adafruit_BMP085.h>
 
 Adafruit_BMP085 bmp;
+
+// Guarda a altitude inicial (ponto zero)
+float altitudeReferencia; 
   
 void setup() {
   Serial.begin(115200);
   
+  // Verifica se o sensor tá conectado
   if (!bmp.begin()) {
     Serial.println("Erro: Sensor BMP085 nao encontrado!");
     while (1) {} 
   }
 
-  pinMode(LED_BUILTIN, OUTPUT); 
+  // Salva a altitude do momento exato em que a placa liga como referência
+  altitudeReferencia = bmp.readAltitude();
 
-  // --- CABEÇALHO ---
-  Serial.println("Temp (*C)\tPressao (Pa)\tAlt (m)\t\tP. Mar (Pa)\tAlt Real (m)");
+  // Imprime o cabeçalho
+  Serial.println("Temp(*C)\tPressao(Pa)\tAlt_Absoluta(m)\tAlt_Relativa(m)");
 }
   
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  
+  // Lê a altitude do momento
+  float altitudeAtual = bmp.readAltitude();
+  
+  // Calcula o quanto subiu ou desceu em relação ao início
+  float altitudeRelativa = altitudeAtual - altitudeReferencia;
 
+  // Envia os dados pra tela no formato de tabela
   Serial.print(bmp.readTemperature());
-  Serial.print("\t\t"); 
+  Serial.print("\t"); 
   
   Serial.print(bmp.readPressure());
-  Serial.print("\t\t");
+  Serial.print("\t");
   
-  Serial.print(bmp.readAltitude());
-  Serial.print("\t\t");
+  Serial.print(altitudeAtual);
+  Serial.print("\t");
 
-  Serial.print(bmp.readSealevelPressure());
-  Serial.print("\t\t");
+  Serial.println(altitudeRelativa);
 
-  Serial.println(bmp.readAltitude(101500)); 
-
-  delay(500);
-
-  digitalWrite(LED_BUILTIN, LOW);  
-  delay(500); 
+  // Pausa rápida pra não bugar o Monitor Serial
+  delay(50); 
 }
