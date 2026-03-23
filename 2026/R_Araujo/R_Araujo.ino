1afd478c874ec0a623230630ec3bda861d5b66cd
XrodrigoZw <jrsdoliveira30@gmail.com>
2026-03-23 17:53:59 -0300

    Update R_Araujo.ino

--- 2026/R_Araujo/R_Araujo.ino
+++ 2026/R_Araujo/R_Araujo.ino
@@ -3,11 +3,15 @@
 Adafruit_BMP085 bmp;
 float t0;
 float t1;
+float t2;
+float t3;
+float t4;
+float t5;
 float si=0;
 int ci=0;
 float mi=0;
 float s=0;
-int c=0;
+int c=1;
 float m=0;
 void setup() {
   Serial.begin(115200);
@@ -30,10 +34,37 @@ void setup() {
 void loop() {
   t1=bmp.readAltitude();
   s+= t1;
-  c+= 1;
-  if (c>=5) {
-    m=s/5;
-    
+    if(c==1){
+      s+=t1;
+      m=s/c;
+    }
+    else if (c==2) {
+      t2=t1;
+      s+=t2;
+      m=s/c;
+    }
+    else if (c==3) {
+      t3=t1;
+      s+=t3;
+      m=s/c;
+    }
+    else if (c==4) {
+      t4=t1;
+      s+=t4;
+      m=s/c;
+    }
+    else if (c==5) {
+      t5=t1;
+      s+=t5;
+      m=s/c;
+    }
+    else {
+      t4=t5;
+      t3=t4;
+      t2=t3;
+      t5=t1;
+      m=(t4+t3+t2+t1)/4;
+    }
     Serial.print(bmp.readTemperature());
     Serial.print('\t');
     Serial.print(bmp.readPressure());
@@ -43,7 +74,5 @@ void loop() {
     Serial.print(bmp.readSealevelPressure());
     Serial.print('\t');
     Serial.println(bmp.readAltitude(101500));
-  c=0;
-  s=0;
-  }
+  c+= 1;
 }
