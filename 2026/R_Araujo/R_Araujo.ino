ee5bc1bebd698b37b56ab73390b1eee4c8fd937b
XrodrigoZw <jrsdoliveira30@gmail.com>
2026-03-23 18:19:14 -0300

    Update R_Araujo.ino

--- 2026/R_Araujo/R_Araujo.ino
+++ 2026/R_Araujo/R_Araujo.ino
@@ -7,6 +7,8 @@ float t2;
 float t3;
 float t4;
 float t5;
+float t6;
+float t7;
 float som_i=0;
 int contador_i=0;
 float med_i=0;
@@ -45,12 +47,20 @@ void loop() {
     else if (contador==5) {
       t5=t1;
     }
+    else if (contador==6) {
+      t6=t1;
+    }
+    else if (contador==7) {
+      t7=t1;
+    }
     else {
+      t6=t7;
+      t5=t6;
       t4=t5;
       t3=t4;
       t2=t3;
-      t5=t1;
-      med=(t4+t3+t2+t1)/4;
+      t7=t1;
+      med=(t6+t5+t4+t3+t2+t1)/6;
     }
     Serial.print(bmp.readTemperature());
     Serial.print('\t');
