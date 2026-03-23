2699608b28e95ab967c2c6d183e7bc05035d5dc9
AlvaroCobianchi <alvarocobianchi1@gmail.com>
2026-03-23 18:02:09 -0300

    LED
    alterei o delay do LED novamente para 500 (o tempo em que ele vai ficar apagado) e ajustei para aparecer em formato de tabela os dados.

--- 2026/A_Cobianchi/A_Cobianchi.ino
+++ 2026/A_Cobianchi/A_Cobianchi.ino
@@ -3,49 +3,38 @@
 Adafruit_BMP085 bmp;
   
 void setup() {
-  // --- PARTE 1: Configuração do Sensor ---
-  Serial.begin(115200); // Inicia a comunicação com o PC
+  Serial.begin(115200);
   
   if (!bmp.begin()) {
-    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-    while (1) {} // Se o sensor não for achado, o código trava aqui
+    Serial.println("Erro: Sensor BMP085 nao encontrado!");
+    while (1) {} 
   }
 
-  // --- PARTE 2: Configuração do LED ---
-  pinMode(LED_BUILTIN, OUTPUT); // Avisa que o pino do LED vai enviar energia (saída)
+  pinMode(LED_BUILTIN, OUTPUT); 
+
+  // --- CABEÇALHO ---
+  Serial.println("Temp (*C)\tPressao (Pa)\tAlt (m)\t\tP. Mar (Pa)\tAlt Real (m)");
 }
   
 void loop() {
-  // 1. Liga o LED para indicar que está fazendo a leitura
   digitalWrite(LED_BUILTIN, HIGH);  
 
-  // 2. Faz as leituras do sensor e joga na tela (Monitor Serial)
-  Serial.print("Temperature = ");
   Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
+  Serial.print("\t\t"); 
   
-  Serial.print("Pressure = ");
   Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
+  Serial.print("\t\t");
   
-  Serial.print("Altitude = ");
   Serial.print(bmp.readAltitude());
-  Serial.println(" meters");
+  Serial.print("\t\t");
 
-  Serial.print("Pressure at sealevel (calculated) = ");
   Serial.print(bmp.readSealevelPressure());
-  Serial.println(" Pa");
+  Serial.print("\t\t");
 
-  Serial.print("Real altitude = ");
-  Serial.print(bmp.readAltitude(101500));
-  Serial.println(" meters");
-  
-  Serial.println(); // Pula uma linha no monitor serial pra ficar fácil de ler
+  Serial.println(bmp.readAltitude(101500)); 
 
-  // 3. Mantém o LED aceso e espera 500 milissegundos (meio segundo)
   delay(500);
 
-  // 4. Apaga o LED e espera mais 500 milissegundos antes de recomeçar
   digitalWrite(LED_BUILTIN, LOW);  
-  delay(100); 
+  delay(500); 
 }
