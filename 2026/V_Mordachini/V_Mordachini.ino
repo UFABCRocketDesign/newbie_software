4a311959462b063976d0968c6fd3be178b308b09
Victor Themoteo Mordachini <mordachini.v@gmail.com>
2026-03-26 19:16:46 -0300

    Update V_Mordachini.ino

--- 2026/V_Mordachini/V_Mordachini.ino
+++ 2026/V_Mordachini/V_Mordachini.ino
@@ -3,32 +3,32 @@
 
 Adafruit_BMP085 bmp;
 
-//variaveis de altitude
+// variaveis de altitude
 float altitudeInicial;
 const int medicoes = 30; // numero de leituras para calibragem
 
-
-//config filtro SMA 1 
+// config filtro SMA 1 
 const int tamanhoJanela = 10; // quantidade de leituras para media
 float leituras[tamanhoJanela];
 int indice = 0; // posicão atual no vetor leituras
 float somaSMA = 0;
 float alturaFiltradaSMA = 0;
 
-//config filtro SMA 2 (cascata - refinamento)
+// config filtro SMA 2 (cascata - refinamento)
 const int tamanhoJanela2 = 10;
 float leituras2[tamanhoJanela2];
 int indice2 = 0;
 float somaSMA2 = 0;
 float alturaFinal = 0;
 
-//variaveis para deteccao de queda
+// variaveis para deteccao de queda
 float alturaMaxima = 0.0;
 bool quedaDetectada = false; // muda para true ao detectar queda
-int contadorQueda = 0;
 const float margemQueda = 1.0; // o foguete precisa cair 1 metro para confirmar
-const int confirmacoesNecessarias = 5; // precisa confirmar a queda 5 vezes seguidas
 
+float alturaAnterior = 0.0;
+int contadorQuedaPares = 0;
+const int confirmacoesNecessarias = 10; // precisa confirmar a queda 5 vezes seguidas
 
 void setup() {
   Serial.begin(115200);
@@ -38,7 +38,7 @@ void setup() {
     while (1) {}
   }
 
-  //LOGICA DE CALIBRACAO
+  // LOGICA DE CALIBRACAO
   float soma = 0;
   for (int i = 0; i < medicoes; i++) {
     soma += bmp.readAltitude();
@@ -49,8 +49,7 @@ void setup() {
   for (int i = 0; i < tamanhoJanela; i++) leituras[i] = 0;
   for (int i = 0; i < tamanhoJanela2; i++) leituras2[i] = 0;
 
-
-  //CABECALHO
+  // CABECALHO
   Serial.println("Temp(C)\tPressao(Pa)\tBruta(m)\tSMA1(m)\tFinal(m)\tMax(m)\tEstado");
 }
 
@@ -59,7 +58,7 @@ void loop() {
   float altitudeAtual = bmp.readAltitude();
   float alturaBruta = altitudeAtual - altitudeInicial;
 
-  //filtro SMA 1 (media movel simples) 
+  // filtro SMA 1 (media movel simples) 
   somaSMA -= leituras[indice];
   leituras[indice] = alturaBruta;
   somaSMA += leituras[indice]; 
@@ -67,7 +66,7 @@ void loop() {
   if (indice >= tamanhoJanela) indice = 0;
   alturaFiltradaSMA = somaSMA / tamanhoJanela;
 
-  //segundo filtro SMA (refinamento)
+  // segundo filtro SMA (refinamento)
   somaSMA2 -= leituras2[indice2];
   leituras2[indice2] = alturaFiltradaSMA;
   somaSMA2 += leituras2[indice2]; 
@@ -77,33 +76,40 @@ void loop() {
 
   // LOGICA PARA DETECCAO DE QUEDA
 
-  //1. atualiza record de altura
+  // 1. atualiza record de altura
   if (alturaFinal > alturaMaxima) {
     alturaMaxima = alturaFinal;
-    contadorQueda = 0; //zera contador pois ainda está subindo
   }
 
   // 2. verifica de o foguete comecou a cair (caso queda nao detectada)
   if (!quedaDetectada) {
     // se altura menor do que altura max - margem de seguranca estipulada
     if (alturaFinal < (alturaMaxima - margemQueda)) {
-      contadorQueda++; // confirma uma leitura de queda
 
-      // 3. caso confirmacoes suficientes
-      if (contadorQueda >= confirmacoesNecessarias) {
-        quedaDetectada = true;
+      // verifica tendencia de queda (analise de pares)
+      if (alturaFinal < alturaAnterior) {
+        contadorQuedaPares++;
+
+        // 3. caso confirmacoes suficientes
+        if (contadorQuedaPares >= confirmacoesNecessarias) {
+          quedaDetectada = true;
         }
-    } else {
-      // zera o contador caso oscile para cima
-      contadorQueda = 0; 
+
+      } else {
+        // zera o contador caso a altura se mantenha ou suba
+        contadorQuedaPares = 0;
+      }
+
+      // salva a altura de agora para comparar no prox ciclo
+      alturaAnterior = alturaFinal;
     }
   }
 
-  //SAIDA DE DADOS
-  Serial.print(bmp.readTemperature()); //temperatura
+  // SAIDA DE DADOS
+  Serial.print(bmp.readTemperature()); // temperatura
   Serial.print("\t");
 
-  Serial.print(bmp.readPressure()); //pressao
+  Serial.print(bmp.readPressure()); // pressao
   Serial.print("\t");
 
   Serial.print(alturaBruta);
@@ -118,6 +124,5 @@ void loop() {
   Serial.print(alturaMaxima);
   Serial.print("\t");
 
-  Serial.print(quedaDetectada);
-  Serial.println("");
+  Serial.println(quedaDetectada ? 1 : 0);
 }
