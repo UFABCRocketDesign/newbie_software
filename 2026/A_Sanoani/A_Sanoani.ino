6dd77666f743adc101f4867523dafdd42dd50d7c
sanoaniarthur-cloud <sanoani.arthur@gmail.com>
2026-04-02 19:53:10 -0300

    print2.5

--- 2026/A_Sanoani/A_Sanoani.ino
+++ 2026/A_Sanoani/A_Sanoani.ino
@@ -1,35 +1,41 @@
 #include <Adafruit_BMP085.h>
 
 Adafruit_BMP085 bmp;
+
 float altitudeInicial;
+float soma = 0;
+int amostra = 5;
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1) {}
   }
-  
-  altitudeInicial = bmp.readAltitude();
-  
-  Serial.println("Temperature(*C), Pressure(Pa), Altitude(meters), Pressure at sealevel (calculated)(Pa), Real altitude(meters)");
-}
+  for (int i = 0; i < amostra; i++) {
+    soma += bmp.readAltitude();
+    altitudeInicial = soma / amostra;
+  }
+  Serial.println("Temperature(*C)\tPressure(Pa)\tAltitude(meters)\tPressure at sealevel (calculated)(Pa)\tReal altitude(meters)");
+  }
 
 
-void loop() {
- 
-  Serial.print(bmp.readTemperature());
-  Serial.print("\t");
+  void loop() {
 
-  Serial.print(bmp.readPressure());
-  Serial.print("\t");
+    Serial.print(bmp.readTemperature());
+    Serial.print("\t");
 
-  Serial.print(bmp.readAltitude() - altitudeInicial);
-  Serial.print("\t");
+    Serial.print(bmp.readPressure());
+    Serial.print("\t");
 
-  Serial.print(bmp.readSealevelPressure());
-  Serial.print("\t");
+    Serial.print(bmp.readAltitude() - altitudeInicial);
+    Serial.print("\t");
 
-  Serial.println(bmp.readAltitude(101500));
-}
+    Serial.print(bmp.readSealevelPressure());
+    Serial.print("\t");
+
+    Serial.println(bmp.readAltitude(101500));
+  }
