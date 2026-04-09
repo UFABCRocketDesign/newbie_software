27785a0ea06d3da72a5dcda36e2222b6eb692e52
sanoaniarthur-cloud <sanoani.arthur@gmail.com>
2026-04-09 19:44:03 -0300

    filtro_altura_1.4.1

--- 2026/A_Sanoani/A_Sanoani.ino
+++ 2026/A_Sanoani/A_Sanoani.ino
@@ -4,10 +4,15 @@ Adafruit_BMP085 bmp;
 
 float altitudeInicial;
 float soma = 0;
-float amostra = 200;
-float PotData = 0; 
+int amostra = 100;
+float altura_bruta = 0;
+const int numReadings = 10;  // Number of readings for averaging
+float readings[numReadings];
 float filteredValue = 0;  // Initialize filtered value
-float alpha = 0.01;  // Filter coefficient (0 < alpha < 1)
+float alpha = 0.5;        // Filter coefficient (0 < alpha < 1)
+float average = 0;
+float total = 0;
+int readIndex = 0;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -21,29 +26,51 @@ void setup() {
   for (int i = 0; i < amostra; i++) {
     soma += bmp.readAltitude();
   }
+  for (int i = 0; i < numReadings; i++) {
+    readings[i] = 0;
+  }
   altitudeInicial = soma / amostra;
   Serial.println("Temperature(*C)\tPressure(Pa)\tAltitude(meters)\tPressure at sealevel (calculated)(Pa)\tReal altitude(meters)");
+}
+
+
+void loop() {
+  altura_bruta = bmp.readAltitude() - altitudeInicial;
+  total = total - readings[readIndex];
+
+  // Read the sensor
+  readings[readIndex] = altura_bruta;
+
+  // Add the reading to the total
+  total = total + readings[readIndex];
+
+  // Advance to the next position in the array
+  readIndex = readIndex + 1;
+
+  // If we're at the end of the array, wrap around to the beginning
+  if (readIndex >= numReadings) {
+    readIndex = 0;
   }
 
+  // Calculate the average
+  average = total / numReadings;
 
-  void loop() {
-    PotData = bmp.readAltitude() - altitudeInicial;
-    filteredValue = (alpha * PotData + (1 - alpha) * filteredValue);
+  filteredValue = (alpha * average + (1 - alpha) * filteredValue);
 
-    Serial.print(bmp.readTemperature());
-    Serial.print("\t");
+  Serial.print(bmp.readTemperature());
+  Serial.print("\t");
 
-    Serial.print(bmp.readPressure());
-    Serial.print("\t");
+  Serial.print(bmp.readPressure());
+  Serial.print("\t");
 
-    Serial.print(PotData);
-    Serial.print("\t");
+  Serial.print(altura_bruta);
+  Serial.print("\t");
 
-    Serial.print(filteredValue);
-    Serial.print("\t");
+  Serial.print(filteredValue);
+  Serial.print("\t");
 
-    Serial.print(bmp.readSealevelPressure());
-    Serial.print("\t");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.print("\t");
 
-    Serial.println(bmp.readAltitude(101500));
-  }
+  Serial.println(bmp.readAltitude(101500));
+}
