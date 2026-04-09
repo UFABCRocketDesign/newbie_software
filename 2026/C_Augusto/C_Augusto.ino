1b92cd69de49c217055e9bdbe23ba817aa41bb4e
cesarafcm <87275058+cesarafcm@users.noreply.github.com>
2026-04-09 18:42:18 -0300

    Update C_Augusto.ino

--- 2026/C_Augusto/C_Augusto.ino
+++ 2026/C_Augusto/C_Augusto.ino
@@ -8,7 +8,7 @@ float c = 1.0; //confiança do filtro
 float g = 0;//ganho do filtro
 float filtrocalman = 0;
 float lowpass = 0;
-float lowpass_suavizacao = 0.2;
+float lowpass_suavizacao = 0.5;
 float apogeu = 0;
 
 
